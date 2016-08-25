89a73b9dd951aeb44b14b53609cb0dec43c2c82f
Marc Johnson <marc@k-int.co.uk>
2016-08-25 11:51:16 +0100

    Use webpack hot module replacement for development

--- demo/ui/webpack.config.js
+++ demo/ui/webpack.config.js
@@ -1,15 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: [
+        'webpack-dev-server/client?http://localhost:8080', // Setting the URL for the hot reload
+        'webpack/hot/only-dev-server', // Reload only the dev server
         './src/index.js'
     ],
-    // module: {
-    //     loaders: [{
-    //         test: /\.jsx?$/,
-    //         exclude: /node_modules/,
-    //     }]
-    // },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'react-hot' // Include the react-hot loader
+        }]
+    },
     resolve: {
-        extensions: ['', '.js']
+        extensions: ['', '.js', '.jsx']
     },
     output: {
         path: __dirname + '/dist',
@@ -17,6 +22,10 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist'
-    }
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin() // Wire in the hot loading plugin
+    ]
 };
