c2a0420ec29c8ea92544d8c3503c9cea8a138eb8
Marc Johnson <marc@k-int.co.uk>
2016-09-08 17:23:59 +0100

    Refine the stop scripts to work on linux and mac

--- catalogue-core/stop.sh
+++ catalogue-core/stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kill `ps | grep "[c]atalogue-core.jar" | awk '{print $1}'`
+kill `ps -f | grep "[c]atalogue-core.jar" | awk '{print $2}'`
 
--- knowledge-base-core/stop.sh
+++ knowledge-base-core/stop.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-kill `ps | grep "[k]nowledge-base-core.jar" | awk '{print $1}'`
+kill `ps -f | grep "[k]nowledge-base-core.jar" | awk '{print $2}'`
+
 
