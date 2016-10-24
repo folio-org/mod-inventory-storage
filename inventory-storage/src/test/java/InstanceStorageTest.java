170f14233199f986b0d6128844b712d092102223
Marc Johnson <marc@k-int.co.uk>
2016-10-24 15:46:58 +0100

    Initial RAML API interface for instance storage

---
+++ inventory-storage/src/test/java/InstanceStorageTest.java
@@ -0,0 +1,40 @@
+package org.folio.rest;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@RunWith(VertxUnitRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class InstanceStorageTest {
+
+  private static Vertx vertx;
+
+  @BeforeClass
+  public static void before() throws InterruptedException, ExecutionException, TimeoutException {
+
+  }
+
+  @AfterClass
+  public static void after() {
+
+  }
+
+  @Test
+  public void exampleTest(TestContext context) {
+    context.assertTrue(true);
+  }
+}
