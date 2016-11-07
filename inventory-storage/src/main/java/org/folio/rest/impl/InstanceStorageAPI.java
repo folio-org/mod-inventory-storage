fb76d1353f7346ff001ee5923c2c5ef588e0987d
Thomas Pickin <thomas.pickin@k-int.co.uk>
2016-11-07 11:39:51 +0000

    Comment out mongo lines from InstanceStorageAPI.java

--- inventory-storage/src/main/java/org/folio/rest/impl/InstanceStorageAPI.java
+++ inventory-storage/src/main/java/org/folio/rest/impl/InstanceStorageAPI.java
@@ -15,8 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import io.vertx.groovy.ext.mongo.MongoClient
-
 
 public class InstanceStorageAPI implements InstanceStorageResource {
 
@@ -48,13 +46,13 @@ public class InstanceStorageAPI implements InstanceStorageResource {
                                           Handler<AsyncResult<Response>> asyncResultHandler, 
                                           Context vertxContext) throws Exception {
 
-    java.util.Map<java.lang.String, java.lang.Object> mongo_config = new java.util.HashMap<java.lang.String, java.lang.Object>()
-    MongoClient client = MongoClient.createShared(vertxContext.owner(), config, "instance-storage-pool")
+      //    java.util.Map<java.lang.String, java.lang.Object> mongo_config = new java.util.HashMap<java.lang.String, java.lang.Object>()
+	//    MongoClient client = MongoClient.createShared(vertxContext.owner(), config, "instance-storage-pool")
     
 
     // This is sample code from the Virt.x async mongoClient -- it's groovy code tho, so will not work here, suspect without it this would be very verbose
-    JsonObject document = new JsonObject().put("title", "The Hobbit");
-
+      //    JsonObject document = new JsonObject().put("title", "The Hobbit");
+    /*
     mongoClient.save("books", document, { res ->
       if (res.succeeded()) {
         String id = res.result()
@@ -63,12 +61,12 @@ public class InstanceStorageAPI implements InstanceStorageResource {
         res.cause().printStackTrace()
       }
     })
-
+    */
+    
     asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetInstanceStorageInstanceResponse.withPlainInternalServerError("Not implemented")));
   }
 
   @Override
-
   public void getInstanceStorageInstanceByInstanceId(@DefaultValue("en") @Pattern(regexp = "[a-zA-Z]{2}") java.lang.String lang,
                                                      java.lang.String instanceId,
                                                      java.util.Map<java.lang.String,java.lang.String> okapiHeaders,
