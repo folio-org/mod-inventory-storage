a8420faf3697e840a5a7156f58a8033c971bc074
Marc Johnson <marc@k-int.co.uk>
2016-11-25 15:31:55 +0000

    Start inventory polls until the web server responds to requests

--- inventory/ingest-demo.sh
+++ inventory/ingest-demo.sh
@@ -2,15 +2,6 @@
 
 ./start.sh
 
-printf 'Waiting for inventory module to start\n'
-
-until $(curl --output /dev/null --silent --get --fail http://localhost:9403/inventory/items); do
-    printf '.'
-    sleep 1
-done
-
-printf '\n'
-
 cd sample-data
 
 ./example-ingest.sh
