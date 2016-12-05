391897859873505463bc5b5e9793f75104154922
Marc Johnson <marc@k-int.co.uk>
2016-12-05 14:21:57 +0000

    Use version 3 of raml2md and raml2html as version 4 only supports RAML 1.0

---
+++ doc/api/generate-documentation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+npm install
+
+rm -rf generated
+
+mkdir generated
+mkdir generated/html
+mkdir generated/md
+
+node_modules/raml2html/bin/raml2html -i knowledgebase.raml -o generated/html/knowledgebase.html
+
+node_modules/raml2html/bin/raml2html -i catalogue.raml -o generated/html/catalogue.html
+
+node_modules/raml2md/bin/raml2md -i catalogue.raml -o generated/md/catalogue.md
+
+node_modules/raml2md/bin/raml2md -i knowledgebase.raml -o generated/md/knowledgebase.md
--- generate-documentation.sh
+++ generate-documentation.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
-npm install -g raml2html
-npm install -g raml2md
-
 cd doc/api
 
-rm -rf generated
+./generate-documentation.sh
 
-mkdir generated
-mkdir generated/html
-mkdir generated/md
+cd ../../
 
-raml2html -i knowledgebase.raml -o generated/html/knowledgebase.html
-raml2html -i catalogue.raml -o generated/html/catalogue.html
-
-raml2md -i catalogue.raml -o generated/md/catalogue.md
-raml2md -i knowledgebase.raml -o generated/md/knowledgebase.md
