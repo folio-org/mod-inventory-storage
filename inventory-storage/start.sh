1ccb6e20bc2402ef817a7ec393e1359f2c1b2a20
Marc Johnson <marc@k-int.co.uk>
2016-12-10 14:17:29 +0000

    Run tests as part of build before starting inventory storage module

--- inventory-storage/start.sh
+++ inventory-storage/start.sh
@@ -2,19 +2,23 @@
 
 port=${1:-9407}
 config_path=${2:-"$(pwd)/postgres-conf.json"}
+tenant_id=${3:-demo_tenant}
 
 rm output.log
 
 echo "Building"
 
-# Temporarily skip tests until using tenant specific databases 
-mvn clean install -DskipTests
+./setup-test-db.sh
+
+mvn clean install
+
+./setup-demo-db.sh
 
 java -jar target/inventory-storage-fat.jar db_connection=${config_path} -Dhttp.port=${port} 1>output.log 2>output.log &
 
 echo 'Waiting for inventory storage module to start'
 
-until $(curl --output /dev/null --silent --get --fail -H "X-Okapi-Tenant: demo_tenant"  http://localhost:${port}/item-storage/items); do
+until $(curl --output /dev/null --silent --get --fail -H "X-Okapi-Tenant: ${tenant_id}"  http://localhost:${port}/item-storage/items); do
     printf '.'
     sleep 1
 done
