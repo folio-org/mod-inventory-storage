dffe3a12b51354226f052216d210ce5e0c76adb4
Marc Johnson <marc@k-int.co.uk>
2016-12-13 12:17:07 +0000

    Full test includes inventory storage tests (not via Okapi yet)

--- full-test.sh
+++ full-test.sh
@@ -10,7 +10,7 @@ inventory_storage_direct_address=${4:-http://localhost:9407}
 knowledgebase_instance_id=${5:-localhost-9601}
 catalogue_instance_id=${6:-localhost-9602}
 inventory_instance_id=${7:-localhost-9603}
-inventory_storage_instance_id=${8:-localhost-9407} #Not currently included
+inventory_storage_instance_id=${8:-localhost-9407}
 tenant_id=${9:-test_tenant}
 
 ./create-tenant.sh ${tenant_id}
@@ -25,6 +25,15 @@ tenant_id=${9:-test_tenant}
   ${inventory_instance_id} \
   ${inventory_storage_instance_id} \
   ${tenant_id}
+
+cd inventory-storage
+
+./setup-test-db.sh
+
+#Does not perform tests via Okapi yet
+mvn clean test
+
+cd ..
 
 gradle -Dokapi.address="${okapi_address}" clean test testApiViaOkapi
 
