20dbe9759ab495c73ffccd59c25f40351e237460
Marc Johnson <marc@k-int.co.uk>
2016-12-15 14:45:31 +0000

    RAML and JSON.Schema documentation for inventory API

--- doc/api/generate-documentation.sh
+++ doc/api/generate-documentation.sh
@@ -15,6 +15,10 @@ node_modules/raml2html/bin/raml2html -i knowledgebase.raml -o generated/html/kno
 
 node_modules/raml2html/bin/raml2html -i catalogue.raml -o generated/html/catalogue.html
 
-node_modules/raml2md/bin/raml2md -i catalogue.raml -o generated/md/catalogue.md
+node_modules/raml2html/bin/raml2html -i inventory.raml -o generated/html/inventory.html
 
 node_modules/raml2md/bin/raml2md -i knowledgebase.raml -o generated/md/knowledgebase.md
+
+node_modules/raml2md/bin/raml2md -i catalogue.raml -o generated/md/catalogue.md
+
+node_modules/raml2md/bin/raml2md -i inventory.raml -o generated/md/inventory.md
