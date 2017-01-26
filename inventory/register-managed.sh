5417b692e031cd84f0132a705ef7452816114f43
Marc Johnson <marc@k-int.co.uk>
2017-01-26 21:04:24 +0000

    First attempt at managed deployment for inventory module

---
+++ inventory/register-managed.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+tenant_id=${1:-demo_tenant}
+okapi_proxy_address=${2:-http://localhost:9130}
+module_id=${3:-inventory}
+
+../okapi-registration/managed-deployment/register.sh \
+  ${module_id} \
+  ${okapi_proxy_address} \
+  ${tenant_id}
+
---
+++ inventory/unregister-managed.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+instance_id=${1:-}
+tenant_id=${2:-demo_tenant}
+okapi_proxy_address=${3:-http://localhost:9130}
+module_id=${4:-inventory}
+
+../okapi-registration/managed-deployment/unregister.sh \
+  ${instance_id} \
+  ${module_id} \
+  ${okapi_proxy_address} \
+  ${tenant_id}
+
