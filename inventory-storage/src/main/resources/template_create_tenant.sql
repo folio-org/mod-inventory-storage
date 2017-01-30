faf00bef53d40efbd7411f595f3bee50fa23ad77
Marc Johnson <marc@k-int.co.uk>
2017-01-30 13:25:20 +0000

    Use UUID for primary key in inventory storage tables

--- inventory-storage/src/main/resources/template_create_tenant.sql
+++ inventory-storage/src/main/resources/template_create_tenant.sql
@@ -2,12 +2,20 @@ CREATE ROLE myuniversity_mymodule PASSWORD 'myuniversity' NOSUPERUSER NOCREATEDB
 
 CREATE SCHEMA myuniversity_mymodule AUTHORIZATION myuniversity_mymodule;
 
-CREATE TABLE myuniversity_mymodule.item (_id SERIAL PRIMARY KEY, jsonb JSONB NOT NULL);
+-- for uuid generator -> gen_random_uuid()
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE myuniversity_mymodule.item (
+  _id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  jsonb JSONB NOT NULL
+);
 
 GRANT ALL ON myuniversity_mymodule.item TO myuniversity_mymodule;
-GRANT ALL ON myuniversity_mymodule.item__id_seq TO myuniversity_mymodule;
 
-CREATE TABLE myuniversity_mymodule.instance (_id SERIAL PRIMARY KEY, jsonb JSONB NOT NULL);
+CREATE TABLE myuniversity_mymodule.instance (
+  _id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  jsonb JSONB NOT NULL
+);
 
 GRANT ALL ON myuniversity_mymodule.instance TO myuniversity_mymodule;
-GRANT ALL ON myuniversity_mymodule.instance__id_seq TO myuniversity_mymodule;
+
