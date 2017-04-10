26a64dab51b8ee7e352a0b4dbb6f2eb53d522206
Marc Johnson <marc@k-int.co.uk>
2017-04-10 11:54:01 +0100

    Reverse order of sample data importing to ease material type dependencies

--- import-sample-data.sh
+++ import-sample-data.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-cd inventory/sample-data
-
-./example-ingest.sh http://localhost:9130/inventory
-
-cd ../..
-
 cd inventory-storage/sample-data
 
 ./import.sh
+
+cd ../..
+
+cd inventory/sample-data
+
+./example-ingest.sh http://localhost:9130/inventory
 
 cd ../..
