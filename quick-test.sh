212801a47ba1ffea9374abb22b0b64e8b6a6cec8
Marc Johnson <marc@k-int.co.uk>
2017-05-11 13:38:37 +0100

    Improve feedback regarding success or failure from quick test run

--- quick-test.sh
+++ quick-test.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+./destroy-test-db.sh
+
 ./setup-test-db.sh
 
-mvn clean test \
-    -Dorg.folio.inventory.storage.test.database=external
+mvn -q clean test -Dorg.folio.inventory.storage.test.database=external
 
+test_results=$?
 
+if [ $test_results != 0 ]; then
+  echo '--------------------------------------'
+  echo 'BUILD FAILED'
+  echo '--------------------------------------'
+  exit 1;
+else
+  ./destroy-test-db.sh
+
+  echo '--------------------------------------'
+  echo 'BUILD SUCCEEDED'
+  echo '--------------------------------------'
+fi
