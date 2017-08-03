dd9165856280dd788108fdd172fe0ee3eb9d0507
Marc Johnson <marc@k-int.co.uk>
2017-08-03 15:49:10 +0100

    Release v5.1.0

--- register-managed.sh
+++ register-managed.sh
@@ -3,7 +3,7 @@
 deployment_descriptor=${1:-DeploymentDescriptor-environment.json}
 tenant_id=${2:-demo_tenant}
 okapi_proxy_address=${3:-http://localhost:9130}
-module_id=${4:-inventory-storage-5.1.0-SNAPSHOT}
+module_id=${4:-inventory-storage-5.1.0}
 
 ./okapi-registration/managed-deployment/register.sh \
   ${module_id} \
