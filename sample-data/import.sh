789ebdcc2ef0badfd0cccd25f56fbe9bad441f78
Marc Johnson <marc@k-int.co.uk>
2017-10-22 15:13:16 +0100

    Include shelf locations in sample data MODINVSTOR-6

--- sample-data/import.sh
+++ sample-data/import.sh
@@ -3,8 +3,9 @@
 tenant=${1:-demo_tenant}
 item_storage_address=http://localhost:9130/item-storage/items
 instance_storage_address=http://localhost:9130/instance-storage/instances
-loan_type_storage_address=http://localhost:9130/loan-types
 material_type_storage_address=http://localhost:9130/material-types
+loan_type_storage_address=http://localhost:9130/loan-types
+shelf_location_storage_address=http://localhost:9130/shelf-locations
 
 for f in ./mtypes/*.json; do
     curl -w '\n' -X POST -D - \
@@ -12,7 +13,6 @@ for f in ./mtypes/*.json; do
          -H "X-Okapi-Tenant: ${tenant}" \
          -d @$f \
          "${material_type_storage_address}"
-
 done
 
 for f in ./loantypes/*.json; do
@@ -21,6 +21,14 @@ for f in ./loantypes/*.json; do
          -H "X-Okapi-Tenant: ${tenant}" \
          -d @$f \
          "${loan_type_storage_address}"
+done
+
+for f in ./shelf-locations/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -d @$f \
+         "${shelf_location_storage_address}"
 done
 
 for f in ./instances/*.json; do
