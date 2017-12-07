895ea9a2a32f440ca997a707567cf9bf7abb5054
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-12-07 13:07:50 +0100

    Add Platform reference table. MODINVSTOR-35.
      Contains platforms providing electronic resources

---
+++ src/main/java/org/folio/rest/impl/PlatformAPI.java
@@ -0,0 +1,312 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import javax.ws.rs.core.Response;
+import org.folio.rest.annotations.Validate;
+import org.folio.rest.jaxrs.resource.PlatformsResource;
+import org.folio.rest.jaxrs.model.Platform;
+import org.folio.rest.jaxrs.model.Platforms;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
+import org.folio.rest.persist.DatabaseExceptionUtils;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.persist.cql.CQLWrapper;
+import org.folio.rest.tools.messages.MessageConsts;
+import org.folio.rest.tools.messages.Messages;
+import org.folio.rest.tools.utils.OutStream;
+import org.folio.rest.tools.utils.TenantTool;
+import org.z3950.zing.cql.CQLParseException;
+import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
+import org.z3950.zing.cql.cql2pgjson.FieldException;
+
+/**
+ *
+ * @author ne
+ */
+public class PlatformAPI implements PlatformsResource {
+  public static final String PLATFORM_TABLE   = "platform";
+
+  private static final String LOCATION_PREFIX       = "/platforms/";
+  private static final Logger log                 = LoggerFactory.getLogger(PlatformAPI.class);
+  private final Messages messages                 = Messages.getInstance();
+  private String idFieldName                      = "_id";
+
+
+  public PlatformAPI(Vertx vertx, String tenantId) {
+    PostgresClient.getInstance(vertx, tenantId).setIdField(idFieldName);
+  }
+
+  private CQLWrapper getCQL(String query, int limit, int offset) throws FieldException {
+    CQL2PgJSON cql2pgJson = new CQL2PgJSON(PLATFORM_TABLE+".jsonb");
+    return new CQLWrapper(cql2pgJson, query).setLimit(new Limit(limit)).setOffset(new Offset(offset));
+  }
+
+  @Validate
+  @Override
+  public void getPlatforms(String query, int offset, int limit, String lang,
+      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) throws Exception {
+    /**
+     * http://host:port/platforms
+     */
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+        CQLWrapper cql = getCQL(query, limit, offset);
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).get(PLATFORM_TABLE, Platform.class,
+            new String[]{"*"}, cql, true, true,
+            reply -> {
+              try {
+                if (reply.succeeded()) {
+                  Platforms instanceTypes = new Platforms();
+                  @SuppressWarnings("unchecked")
+                  List<Platform> instanceType = (List<Platform>) reply.result().getResults();
+                  instanceTypes.setPlatforms(instanceType);
+                  instanceTypes.setTotalRecords(reply.result().getResultInfo().getTotalRecords());
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.GetPlatformsResponse.withJsonOK(
+                      instanceTypes)));
+                }
+                else{
+                  log.error(reply.cause().getMessage(), reply.cause());
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.GetPlatformsResponse
+                      .withPlainBadRequest(reply.cause().getMessage())));
+                }
+              } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.GetPlatformsResponse
+                    .withPlainInternalServerError(messages.getMessage(
+                        lang, MessageConsts.InternalServerError))));
+              }
+            });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        String message = messages.getMessage(lang, MessageConsts.InternalServerError);
+        if (e.getCause() instanceof CQLParseException) {
+          message = " CQL parse error " + e.getLocalizedMessage();
+        }
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.GetPlatformsResponse
+            .withPlainInternalServerError(message)));
+      }
+    });
+  }
+
+  private void internalServerErrorDuringPost(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(PlatformsResource.PostPlatformsResponse
+        .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+
+  @Validate
+  @Override
+  public void postPlatforms(String lang, Platform entity, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) throws Exception {
+
+    vertxContext.runOnContext(v -> {
+      try {
+        String id = entity.getId();
+        if (id == null) {
+          id = UUID.randomUUID().toString();
+          entity.setId(id);
+        }
+
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).save(
+            PLATFORM_TABLE, id, entity,
+            reply -> {
+              try {
+                if (reply.succeeded()) {
+                  Object ret = reply.result();
+                  entity.setId((String) ret);
+                  OutStream stream = new OutStream();
+                  stream.setData(entity);
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.PostPlatformsResponse.withJsonCreated(
+                      LOCATION_PREFIX + ret, stream)));
+                } else {
+                  String msg = DatabaseExceptionUtils.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringPost(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(PlatformsResource.PostPlatformsResponse
+                      .withPlainBadRequest(msg)));
+                }
+              } catch (Exception e) {
+                internalServerErrorDuringPost(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringPost(e, lang, asyncResultHandler);
+      }
+    });
+  }
+
+  private void internalServerErrorDuringGetById(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(PlatformsResource.GetPlatformsByPlatformIdResponse
+        .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+
+  @Validate
+  @Override
+  public void getPlatformsByPlatformId(String instanceTypeId, String lang,
+      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) throws Exception {
+
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+
+        Criterion c = new Criterion(
+            new Criteria().addField(idFieldName).setJSONB(false).setOperation("=").setValue("'"+instanceTypeId+"'"));
+
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).get(PLATFORM_TABLE, Platform.class, c, true,
+            reply -> {
+              try {
+                if (reply.failed()) {
+                  String msg = DatabaseExceptionUtils.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringGetById(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(PlatformsResource.GetPlatformsByPlatformIdResponse.
+                      withPlainNotFound(msg)));
+                  return;
+                }
+                @SuppressWarnings("unchecked")
+                List<Platform> instanceType = (List<Platform>) reply.result().getResults();
+                if (instanceType.isEmpty()) {
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.GetPlatformsByPlatformIdResponse
+                      .withPlainNotFound(instanceTypeId)));
+                }
+                else{
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.GetPlatformsByPlatformIdResponse
+                      .withJsonOK(instanceType.get(0))));
+                }
+              } catch (Exception e) {
+                internalServerErrorDuringGetById(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringGetById(e, lang, asyncResultHandler);
+      }
+    });
+  }
+
+  private void internalServerErrorDuringDelete(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(PlatformsResource.DeletePlatformsByPlatformIdResponse
+        .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+
+  @Validate
+  @Override
+  public void deletePlatformsByPlatformId(String instanceTypeId, String lang,
+      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) throws Exception {
+
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+        PostgresClient postgres = PostgresClient.getInstance(vertxContext.owner(), tenantId);
+        postgres.delete(PLATFORM_TABLE, instanceTypeId,
+            reply -> {
+              try {
+                if (reply.failed()) {
+                  String msg = DatabaseExceptionUtils.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringDelete(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(PlatformsResource.DeletePlatformsByPlatformIdResponse
+                      .withPlainBadRequest(msg)));
+                  return;
+                }
+                int updated = reply.result().getUpdated();
+                if (updated != 1) {
+                  String msg = messages.getMessage(lang, MessageConsts.DeletedCountError, 1, updated);
+                  log.error(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(PlatformsResource.DeletePlatformsByPlatformIdResponse
+                      .withPlainNotFound(msg)));
+                  return;
+                }
+                asyncResultHandler.handle(Future.succeededFuture(PlatformsResource.DeletePlatformsByPlatformIdResponse
+                        .withNoContent()));
+              } catch (Exception e) {
+                internalServerErrorDuringDelete(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringDelete(e, lang, asyncResultHandler);
+      }
+    });
+  }
+
+  private void internalServerErrorDuringPut(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(PlatformsResource.PutPlatformsByPlatformIdResponse
+        .withPlainInternalServerError(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+
+  @Validate
+  @Override
+  public void putPlatformsByPlatformId(String instanceTypeId, String lang, Platform entity,
+      Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) throws Exception {
+
+    vertxContext.runOnContext(v -> {
+      String tenantId = TenantTool.tenantId(okapiHeaders);
+      try {
+        if (entity.getId() == null) {
+          entity.setId(instanceTypeId);
+        }
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).update(
+            PLATFORM_TABLE, entity, instanceTypeId,
+            reply -> {
+              try {
+                if (reply.succeeded()) {
+                  if (reply.result().getUpdated() == 0) {
+                    asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.PutPlatformsByPlatformIdResponse
+                        .withPlainNotFound(messages.getMessage(lang, MessageConsts.NoRecordsUpdated))));
+                  } else{
+                    asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PlatformsResource.PutPlatformsByPlatformIdResponse
+                        .withNoContent()));
+                  }
+                } else {
+                  String msg = DatabaseExceptionUtils.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringPut(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(PlatformsResource.PutPlatformsByPlatformIdResponse
+                      .withPlainBadRequest(msg)));
+                }
+              } catch (Exception e) {
+                internalServerErrorDuringPut(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringPut(e, lang, asyncResultHandler);      }
+    });
+  }
+  
+}
