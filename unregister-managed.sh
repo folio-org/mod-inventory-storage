e301e11239e5a0ff9857045112c2b09ac0939964
Marc Johnson <marc@k-int.co.uk>
2017-12-20 15:54:47 +0000

    Use 6.0.1-SNAPSHOT for stopping managed demo

--- unregister-managed.sh
+++ unregister-managed.sh
@@ -3,7 +3,7 @@
 instance_id=${1:-}
 tenant_id=${2:-demo_tenant}
 okapi_proxy_address=${3:-http://localhost:9130}
-module_id=${4:-mod-inventory-storage-6.0.0-SNAPSHOT}
+module_id=${4:-mod-inventory-storage-6.0.1-SNAPSHOT}
 
 ./okapi-registration/managed-deployment/unregister.sh \
   ${instance_id} \
