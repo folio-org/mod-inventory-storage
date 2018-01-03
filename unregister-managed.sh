7fed6dfe7b0dc56b1ed13212e7dc62aecf9d9d4b
Marc Johnson <marc@k-int.co.uk>
2018-01-03 11:00:48 +0000

    No longer need to define module ID separately during un-registration

--- unregister-managed.sh
+++ unregister-managed.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-instance_id=${1:-}
-tenant_id=${2:-demo_tenant}
-okapi_proxy_address=${3:-http://localhost:9130}
-module_id=${4:-mod-inventory-storage-7.0.0-SNAPSHOT}
+tenant_id=${1:-demo_tenant}
+okapi_proxy_address=${2:-http://localhost:9130}
 
 ./okapi-registration/managed-deployment/unregister.sh \
-  ${instance_id} \
-  ${module_id} \
   ${okapi_proxy_address} \
   ${tenant_id}
 
