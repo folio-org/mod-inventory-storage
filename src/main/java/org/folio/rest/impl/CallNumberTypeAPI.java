d75b3db15bc5364bf4238d3f1a8a3995c0076335
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-11-13 00:50:06 +0100

    Add end-point call-number-types, property callNumberTypeId MODINVSTOR-210

---
+++ src/main/java/org/folio/rest/impl/CallNumberTypeAPI.java
@@ -0,0 +1,286 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.folio.rest.impl;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
+
+import org.folio.rest.jaxrs.model.CallNumberType;
+import org.folio.rest.jaxrs.model.CallNumberTypes;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
+import org.folio.rest.persist.PgExceptionUtil;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.persist.cql.CQLWrapper;
+import org.folio.rest.tools.messages.MessageConsts;
+import org.folio.rest.tools.messages.Messages;
+import org.folio.rest.tools.utils.TenantTool;
+import org.z3950.zing.cql.CQLParseException;
+import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
+import org.z3950.zing.cql.cql2pgjson.FieldException;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+/**
+ *
+ * @author ne
+ */
+public class CallNumberTypeAPI implements org.folio.rest.jaxrs.resource.CallNumberTypes {
+  public static final String REFERENCE_TABLE  = "call_number_type";
+
+  private static final String LOCATION_PREFIX = "/call-number-types/";
+  private static final Logger log             = LoggerFactory.getLogger(CallNumberTypeAPI.class);
+  private final Messages messages             = Messages.getInstance();
+  private String idFieldName                  = "_id";
+
+  public CallNumberTypeAPI(Vertx vertx, String tenantId) {
+    PostgresClient.getInstance(vertx, tenantId).setIdField(idFieldName);
+  }
+
+  @Override
+  public void getCallNumberTypes(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    /**
+     * http://host:port/call-number-types
+     */
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+        CQLWrapper cql = getCQL(query, limit, offset);
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).get(REFERENCE_TABLE, CallNumberType.class,
+            new String[]{"*"}, cql, true, true,
+            reply -> {
+              try {
+                if (reply.succeeded()) {
+                  CallNumberTypes callNumberTypes = new CallNumberTypes();
+                  List<CallNumberType> callNumberType = reply.result().getResults();
+                  callNumberTypes.setCallNumberTypes(callNumberType);
+                  callNumberTypes.setTotalRecords(reply.result().getResultInfo().getTotalRecords());
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetCallNumberTypesResponse.respond200WithApplicationJson(callNumberTypes)));
+                }
+                else{
+                  log.error(reply.cause().getMessage(), reply.cause());
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetCallNumberTypesResponse
+                      .respond400WithTextPlain(reply.cause().getMessage())));
+                }
+              } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetCallNumberTypesResponse
+                    .respond500WithTextPlain(messages.getMessage(
+                        lang, MessageConsts.InternalServerError))));
+              }
+            });
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+        String message = messages.getMessage(lang, MessageConsts.InternalServerError);
+        if (e.getCause() instanceof CQLParseException) {
+          message = " CQL parse error " + e.getLocalizedMessage();
+        }
+        asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetCallNumberTypesResponse
+            .respond500WithTextPlain(message)));
+      }
+    });
+  }
+
+  @Override
+  public void postCallNumberTypes(String lang, CallNumberType entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    vertxContext.runOnContext(v -> {
+      try {
+        String id = entity.getId();
+        if (id == null) {
+          id = UUID.randomUUID().toString();
+          entity.setId(id);
+        }
+
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).save(REFERENCE_TABLE, id, entity,
+            reply -> {
+              try {
+                if (reply.succeeded()) {
+                  String ret = reply.result();
+                  entity.setId(ret);
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PostCallNumberTypesResponse
+                    .respond201WithApplicationJson(entity, PostCallNumberTypesResponse.headersFor201().withLocation(LOCATION_PREFIX + ret))));
+                } else {
+                  String msg = PgExceptionUtil.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringPost(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(PostCallNumberTypesResponse
+                      .respond400WithTextPlain(msg)));
+                }
+              } catch (Exception e) {
+                internalServerErrorDuringPost(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringPost(e, lang, asyncResultHandler);
+      }
+    });
+  }
+
+  @Override
+  public void getCallNumberTypesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+
+        Criterion c = new Criterion(
+            new Criteria().addField(idFieldName).setJSONB(false).setOperation("=").setValue("'"+id+"'"));
+
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).get(REFERENCE_TABLE, CallNumberType.class, c, true,
+            reply -> {
+              try {
+                if (reply.failed()) {
+                  String msg = PgExceptionUtil.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringGetById(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(GetCallNumberTypesByIdResponse.
+                      respond404WithTextPlain(msg)));
+                  return;
+                }
+                @SuppressWarnings("unchecked")
+                List<CallNumberType> illPolicy = (List<CallNumberType>) reply.result().getResults();
+                if (illPolicy.isEmpty()) {
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetCallNumberTypesByIdResponse
+                      .respond404WithTextPlain(id)));
+                }
+                else{
+                  asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(GetCallNumberTypesByIdResponse
+                      .respond200WithApplicationJson(illPolicy.get(0))));
+                }
+              } catch (Exception e) {
+                internalServerErrorDuringGetById(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringGetById(e, lang, asyncResultHandler);
+      }
+    });
+  }
+
+  @Override
+  public void deleteCallNumberTypesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    vertxContext.runOnContext(v -> {
+      try {
+        String tenantId = TenantTool.tenantId(okapiHeaders);
+        PostgresClient postgres = PostgresClient.getInstance(vertxContext.owner(), tenantId);
+        postgres.delete(REFERENCE_TABLE, id,
+            reply -> {
+              try {
+                if (reply.failed()) {
+                  String msg = PgExceptionUtil.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringDelete(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(DeleteCallNumberTypesByIdResponse
+                      .respond400WithTextPlain(msg)));
+                  return;
+                }
+                int updated = reply.result().getUpdated();
+                if (updated != 1) {
+                  String msg = messages.getMessage(lang, MessageConsts.DeletedCountError, 1, updated);
+                  log.error(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(DeleteCallNumberTypesByIdResponse
+                      .respond404WithTextPlain(msg)));
+                  return;
+                }
+                asyncResultHandler.handle(Future.succeededFuture(DeleteCallNumberTypesByIdResponse
+                        .respond204()));
+              } catch (Exception e) {
+                internalServerErrorDuringDelete(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringDelete(e, lang, asyncResultHandler);
+      }
+    });
+  }
+
+  @Override
+  public void putCallNumberTypesById(String id, String lang, CallNumberType entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    vertxContext.runOnContext(v -> {
+      String tenantId = TenantTool.tenantId(okapiHeaders);
+      try {
+        if (entity.getId() == null) {
+          entity.setId(id);
+        }
+        PostgresClient.getInstance(vertxContext.owner(), tenantId).update(REFERENCE_TABLE, entity, id,
+            reply -> {
+              try {
+                if (reply.succeeded()) {
+                  if (reply.result().getUpdated() == 0) {
+                    asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutCallNumberTypesByIdResponse
+                        .respond404WithTextPlain(messages.getMessage(lang, MessageConsts.NoRecordsUpdated))));
+                  } else{
+                    asyncResultHandler.handle(io.vertx.core.Future.succeededFuture(PutCallNumberTypesByIdResponse
+                        .respond204()));
+                  }
+                } else {
+                  String msg = PgExceptionUtil.badRequestMessage(reply.cause());
+                  if (msg == null) {
+                    internalServerErrorDuringPut(reply.cause(), lang, asyncResultHandler);
+                    return;
+                  }
+                  log.info(msg);
+                  asyncResultHandler.handle(Future.succeededFuture(PutCallNumberTypesByIdResponse
+                      .respond400WithTextPlain(msg)));
+                }
+              } catch (Exception e) {
+                internalServerErrorDuringPut(e, lang, asyncResultHandler);
+              }
+            });
+      } catch (Exception e) {
+        internalServerErrorDuringPut(e, lang, asyncResultHandler);      }
+    });
+  }
+  
+  private CQLWrapper getCQL(String query, int limit, int offset) throws FieldException {
+    CQL2PgJSON cql2pgJson = new CQL2PgJSON(REFERENCE_TABLE+".jsonb");
+    return new CQLWrapper(cql2pgJson, query).setLimit(new Limit(limit)).setOffset(new Offset(offset));
+  }
+
+  private void internalServerErrorDuringPost(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(PostCallNumberTypesResponse
+        .respond500WithTextPlain(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+  
+  private void internalServerErrorDuringDelete(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(DeleteCallNumberTypesByIdResponse
+        .respond500WithTextPlain(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+
+  private void internalServerErrorDuringGetById(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(GetCallNumberTypesByIdResponse
+        .respond500WithTextPlain(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+
+  private void internalServerErrorDuringPut(Throwable e, String lang, Handler<AsyncResult<Response>> handler) {
+    log.error(e.getMessage(), e);
+    handler.handle(Future.succeededFuture(PutCallNumberTypesByIdResponse
+        .respond500WithTextPlain(messages.getMessage(lang, MessageConsts.InternalServerError))));
+  }
+}
