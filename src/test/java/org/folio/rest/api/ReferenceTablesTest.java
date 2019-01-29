efe0f58921a2a26ac933670c22eeb5b86a5bbf08
Niels Erik G. Nielsen <nielserik@indexdata.com>
2019-01-29 21:38:42 +0100

    Add create tests, reference table MODINVSTOR-41

--- src/test/java/org/folio/rest/api/ReferenceTablesTest.java
+++ src/test/java/org/folio/rest/api/ReferenceTablesTest.java
@@ -7,9 +7,12 @@ package org.folio.rest.api;
 
 import static org.folio.rest.api.TestBase.client;
 import static org.folio.rest.support.http.InterfaceUrls.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -19,6 +22,24 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.folio.rest.api.entities.AlternativeTitleType;
+import org.folio.rest.api.entities.CallNumberType;
+import org.folio.rest.api.entities.ClassificationType;
+import org.folio.rest.api.entities.ContributorNameType;
+import org.folio.rest.api.entities.ContributorType;
+import org.folio.rest.api.entities.ElectronicAccessRelationship;
+import org.folio.rest.api.entities.HoldingsNoteType;
+import org.folio.rest.api.entities.HoldingsType;
+import org.folio.rest.api.entities.IdentifierType;
+import org.folio.rest.api.entities.IllPolicy;
+import org.folio.rest.api.entities.InstanceFormat;
+import org.folio.rest.api.entities.InstanceStatus;
+import org.folio.rest.api.entities.InstanceType;
+import org.folio.rest.api.entities.ItemNoteType;
+import org.folio.rest.api.entities.JsonEntity;
+import org.folio.rest.api.entities.ModeOfIssuance;
+import org.folio.rest.api.entities.StatisticalCode;
+import org.folio.rest.api.entities.StatisticalCodeType;
 import org.folio.rest.support.Response;
 import org.folio.rest.support.ResponseHandler;
 import org.junit.Test;
@@ -36,8 +57,15 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(alternativeTitleTypesUrl(""));
+    URL apiUrl = alternativeTitleTypesUrl("");
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("alternative title types", searchResponse, 5, 40);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new AlternativeTitleType("Test alternative title type", "test source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
   }
 
   @Test
@@ -47,8 +75,16 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(callNumberTypesUrl(""));
+    URL apiUrl = callNumberTypesUrl("");
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("call number types", searchResponse, 5, 40);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new CallNumberType("Test call number type", "test source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -58,8 +94,16 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(classificationTypesUrl(""));
+    URL apiUrl = classificationTypesUrl("");
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("classification types", searchResponse, 2, 20);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new ClassificationType("Test classfication type")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -69,8 +113,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(contributorNameTypesUrl(""));
+    URL apiUrl = contributorNameTypesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("contributor name types", searchResponse, 3, 10);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new ContributorNameType("Test contributor name type", "100")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -80,8 +133,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(contributorTypesUrl(""));
+    URL apiUrl = contributorTypesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("contributor types", searchResponse, 20, 500);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new ContributorType("Test contributor type", "Test Code", "Test Source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -91,8 +153,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(electronicAccessRelationshipsUrl(""));
+    URL apiUrl = electronicAccessRelationshipsUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("electronic access relationship types", searchResponse, 2, 20);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new ElectronicAccessRelationship("Test electronic access relationship type")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -102,8 +173,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(holdingsNoteTypesUrl(""));
+    URL apiUrl = holdingsNoteTypesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("holdings note types", searchResponse, 5, 20);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new HoldingsNoteType("Test holdings note type", "test source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -113,8 +193,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(holdingsTypesUrl(""));
+    URL apiUrl = holdingsTypesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("holdings types", searchResponse, 3, 20);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new HoldingsType("Test holdings note type", "test source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -124,8 +213,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(identifierTypesUrl(""));
+    URL apiUrl = identifierTypesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("identifier types", searchResponse, 8, 30);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new IdentifierType("Test identifier type")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -135,8 +233,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(illPoliciesUrl(""));
+    URL apiUrl = illPoliciesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("ILL policies", searchResponse, 5, 20);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new IllPolicy("Test ILL policy", "Test source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -146,8 +253,16 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(instanceFormatsUrl(""));
+    URL apiUrl = instanceFormatsUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("instance formats", searchResponse, 20, 200);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new InstanceFormat("Test instance format", "Test Code", "Test Source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
   }
 
   @Test
@@ -157,8 +272,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(instanceStatusesUrl(""));
+    URL apiUrl = instanceStatusesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("instance statuses", searchResponse, 5, 20);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new InstanceStatus("Test instance status", "Test Code", "Test Source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -168,8 +292,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(instanceTypesUrl(""));
+    URL apiUrl = instanceTypesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("instance types (resource types)", searchResponse, 10, 100);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new InstanceType("Test instance type", "Test Code", "Test Source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -179,8 +312,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(itemNoteTypesUrl(""));
+    URL apiUrl = itemNoteTypesUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("item note types", searchResponse, 5, 20);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new ItemNoteType("Test item note type", "Test source")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -190,8 +332,17 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(modesOfIssuanceUrl(""));
+    URL apiUrl = modesOfIssuanceUrl("");
+
+    Response searchResponse = getReferenceRecords(apiUrl);
     validateNumberOfReferenceRecords("modes of issuance", searchResponse, 4, 10);
+
+    Response postResponse = createReferenceRecord(
+            apiUrl,
+            new ModeOfIssuance("Test mode of issuance")
+    );
+    assertThat(postResponse.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   @Test
@@ -201,19 +352,31 @@ public class ReferenceTablesTest extends TestBase {
           TimeoutException,
           ExecutionException,
           UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(statisticalCodesUrl(""));
-    validateNumberOfReferenceRecords("statistical codes", searchResponse, 10, 500);
-  }
 
-  @Test
-  public void statisticalCodeTypes()
-          throws InterruptedException,
-          MalformedURLException,
-          TimeoutException,
-          ExecutionException,
-          UnsupportedEncodingException {
-    Response searchResponse = getReferenceRecords(statisticalCodeTypesUrl(""));
-    validateNumberOfReferenceRecords("statistical code types", searchResponse, 2, 50);
+    URL statisticalCodeTypesUrl = statisticalCodeTypesUrl("");
+
+    Response searchResponseCodeTypes = getReferenceRecords(statisticalCodeTypesUrl);
+    validateNumberOfReferenceRecords("statistical code types", searchResponseCodeTypes, 2, 50);
+
+    String statisticalCodeTypeId = "8c5b634a-0a4a-47ec-b9b2-d66980656ffd";
+
+    Response postResponseCodeType = createReferenceRecord(
+            statisticalCodeTypesUrl,
+            new StatisticalCodeType(statisticalCodeTypeId, "Test statistical code type", "Test source")
+    );
+    assertThat(postResponseCodeType.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
+    URL statisticalCodesUrl = statisticalCodesUrl("");
+
+    Response searchResponseCodes = getReferenceRecords(statisticalCodesUrl);
+    validateNumberOfReferenceRecords("statistical codes", searchResponseCodes, 10, 500);
+
+    Response postResponseCode = createReferenceRecord(
+            statisticalCodeTypesUrl,
+            new StatisticalCode("Test statistical name", "Test statistical code", statisticalCodeTypeId, "Test source")
+    );
+    assertThat(postResponseCode.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
   }
 
   private Response getReferenceRecords(URL baseUrl)
@@ -232,11 +395,24 @@ public class ReferenceTablesTest extends TestBase {
   }
 
   private void validateNumberOfReferenceRecords(String dataDescription, Response searchResponse, int min, int max) {
-
     Integer totalRecords = searchResponse.getJson().getInteger("totalRecords");
     assertTrue(String.format("Could not retrieve record count for %s", dataDescription), totalRecords != null);
     assertTrue(String.format("Expected <=%s \"%s\", found %s", max, dataDescription, totalRecords), max >= totalRecords);
     assertTrue(String.format("Expected >=%s \"%s\", found %s", min, dataDescription, totalRecords), min <= totalRecords);
-
   }
+
+  public Response createReferenceRecord(URL referenceUrl, JsonEntity referenceObject)
+  throws ExecutionException, InterruptedException, TimeoutException {
+
+    CompletableFuture<Response> createCompleted = new CompletableFuture<>();
+    client.post(
+            referenceUrl,
+            referenceObject.getJson(),
+            StorageTestSuite.TENANT_ID,
+            ResponseHandler.json(createCompleted)
+    );
+    Response postResponse = createCompleted.get(5, TimeUnit.SECONDS);
+    return postResponse;
+  }
+
 }
---
+++ src/test/java/org/folio/rest/api/entities/InstanceType.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.folio.rest.api.entities;
+
+/**
+ *
+ * @author ne
+ */
+public class InstanceType extends JsonEntity {
+  public static final String NAME_KEY = "name";
+  public static final String CODE_KEY = "code";
+  public static final String SOURCE_KEY = "source";
+
+  public InstanceType (String name, String code, String source) {
+    super.setProperty(NAME_KEY, name);
+    super.setProperty(CODE_KEY, code);
+    super.setProperty(SOURCE_KEY, source);
+  }
+
+  @Override
+  public JsonEntity put(String propertyKey, Object value) {
+    setProperty(propertyKey, value);
+    return this;
+  }
+
+}
