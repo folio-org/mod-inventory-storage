8a8b160c06b2443234e9cef14efd899333f954d5
Oleh Pochernin <Oleh_Pochernin@epam.com>
2019-08-23 16:00:38 +0300

    [MODINVSTOR-318] Holdings type test

---
+++ src/test/java/org/folio/rest/api/HoldingsTypeTest.java
@@ -0,0 +1,62 @@
+package org.folio.rest.api;
+
+import static org.folio.rest.api.StorageTestSuite.TENANT_ID;
+import static org.folio.rest.support.http.InterfaceUrls.holdingsTypesUrl;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.folio.rest.support.Response;
+import org.folio.rest.support.ResponseHandler;
+import org.folio.rest.support.http.ResourceClient;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public class HoldingsTypeTest extends TestBase {
+
+  private static ResourceClient holdingsTypeClient;
+
+  @BeforeClass
+  public static void beforeAll() {
+    holdingsTypeClient = ResourceClient.forHoldingsType(client);
+  }
+
+  @Test
+  public void cannotCreateHoldingsTypeWithDuplicateName()
+    throws InterruptedException, MalformedURLException, TimeoutException, ExecutionException {
+
+    JsonObject holdingsType = new JsonObject()
+      .put("name", "Custom")
+      .put("source", "folio");
+
+    holdingsTypeClient.create(holdingsType);
+
+    CompletableFuture<Response> postCompleted = new CompletableFuture<>();
+    client.post(holdingsTypesUrl(""), holdingsType, TENANT_ID, ResponseHandler.json(postCompleted));
+
+    Response response = postCompleted.get(5, TimeUnit.SECONDS);
+    assertThat(response.getStatusCode(), is(422));
+
+    JsonArray errors = response.getJson().getJsonArray("errors");
+    assertThat(errors.size(), is(1));
+
+    JsonObject error = errors.getJsonObject(0);
+    assertThat(error.getString("code"), is("name.duplicate"));
+    assertThat(error.getString("message"), is("Cannot create entity with not unique name"));
+
+    JsonArray errorParameters = error.getJsonArray("parameters");
+    assertThat(errorParameters.size(), is(1));
+
+    JsonObject parameter = errorParameters.getJsonObject(0);
+    assertThat(parameter.getString("key"), is("fieldLabel"));
+    assertThat(parameter.getString("value"), is("name"));
+  }
+}
