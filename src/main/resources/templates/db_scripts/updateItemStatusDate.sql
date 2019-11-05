e5249bc1f93e8ea178c3be0901c1a66fb42f61df
VRohach <Volodymyr_Rohach@epam.com>
2019-11-05 12:24:59 +0200

    MODINVSTOR-367 - Maintain item status date even if item record is edited. 1. Trigger name changed. 2. Extra check removed. 3. 'IS DISTINCT FROM' mechanism added.

--- src/main/resources/templates/db_scripts/updateItemStatusDate.sql
+++ src/main/resources/templates/db_scripts/updateItemStatusDate.sql
@@ -1,23 +1,19 @@
 
 -- Updates item status date if status name was changed.
 -- Item status date stores with timezone.
-CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.update_status_date() RETURNS TRIGGER
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.update_item_status_date() RETURNS TRIGGER
 AS $$
   DECLARE
 	  newStatus text;
   BEGIN
   	newStatus = NEW.jsonb->'status'->>'name';
-  IF newStatus IS NULL THEN  RETURN NEW;
-  END IF;
-	IF (TG_OP='UPDATE') THEN
-	  IF (newStatus!=OLD.jsonb->'status'->>'name') THEN
+	  IF (newStatus IS DISTINCT FROM OLD.jsonb->'status'->>'name') THEN
           NEW.jsonb = jsonb_set(NEW.jsonb, '{status,date}', to_jsonb(CURRENT_TIMESTAMP(3)), true);
 	  END IF;
-	END IF;
 	RETURN NEW;
   END;
   $$ LANGUAGE 'plpgsql';
 
-DROP TRIGGER IF EXISTS update_status_date ON ${myuniversity}_${mymodule}.item;
-CREATE TRIGGER update_status_date BEFORE UPDATE
-ON ${myuniversity}_${mymodule}.item FOR EACH ROW EXECUTE PROCEDURE ${myuniversity}_${mymodule}.update_status_date();
+DROP TRIGGER IF EXISTS update_item_status_date ON ${myuniversity}_${mymodule}.item;
+CREATE TRIGGER update_item_status_date BEFORE UPDATE
+ON ${myuniversity}_${mymodule}.item FOR EACH ROW EXECUTE PROCEDURE ${myuniversity}_${mymodule}.update_item_status_date();
