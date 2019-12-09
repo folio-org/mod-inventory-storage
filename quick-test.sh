8bc9f59511faee10faf5a95048aa896e4d175558
Marc Johnson <marc@k-int.co.uk>
2019-12-09 12:43:10 +0000

    Generate test report during quick test script MODINVSTOR-405

--- quick-test.sh
+++ quick-test.sh
@@ -6,6 +6,13 @@
 
 mvn -q clean org.jacoco:jacoco-maven-plugin:prepare-agent test -Dorg.folio.inventory.storage.test.database=external
 
+# Convert .xml reports into .html report, but without the CSS or images
+mvn surefire-report:report-only
+
+# Put the CSS and images where they need to be without the rest of the
+# time-consuming stuff
+mvn site -DgenerateReports=false
+
 test_results=$?
 
 if [ $test_results != 0 ]; then
