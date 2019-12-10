04d0691553a463826d7fc029045ff1d410214d4d
Marc Johnson <marc@k-int.co.uk>
2019-12-10 21:49:11 +0000

    removed jetbrains not null annotation in PostgreSQL error matcher MODINVSTOR-405

--- src/test/java/org/folio/rest/support/matchers/PostgresErrorMessageMatchers.java
+++ src/test/java/org/folio/rest/support/matchers/PostgresErrorMessageMatchers.java
@@ -1,14 +1,12 @@
 package org.folio.rest.support.matchers;
 
 import org.hamcrest.Matcher;
-import org.jetbrains.annotations.NotNull;
 
 import static org.hamcrest.CoreMatchers.containsString;
 
 public class PostgresErrorMessageMatchers {
   private PostgresErrorMessageMatchers() { }
 
-  @NotNull
   public static Matcher<String> isMaximumSequenceValueError(final String sequenceName) {
     return containsString(
       "ErrorMessage(fields=[(Severity, ERROR), (V, ERROR), (SQLSTATE, 2200H), " +
