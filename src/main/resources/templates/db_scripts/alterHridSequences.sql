c9e618b754cb1ed5650b5df17077d985ffb37526
Bohdan Suprun <bohdan_suprun@epam.com>
2019-12-13 17:00:45 +0200

    MODINVSTOR-406, MODINVSTOR-410, MODINVSTOR-411, MODINVSTOR-412: Set owner for item/instance/holding HRID sequences.

--- src/main/resources/templates/db_scripts/alterHridSequences.sql
+++ src/main/resources/templates/db_scripts/alterHridSequences.sql
@@ -1,10 +1,13 @@
 -- Increase the initial size of the HRID sequences
 
 ALTER SEQUENCE IF EXISTS ${myuniversity}_${mymodule}.hrid_instances_seq
-  AS BIGINT MAXVALUE 99999999999;
+  AS BIGINT MAXVALUE 99999999999
+  OWNED BY ${myuniversity}_${mymodule}.hrid_settings.jsonb;
 
 ALTER SEQUENCE IF EXISTS ${myuniversity}_${mymodule}.hrid_holdings_seq
-  AS BIGINT MAXVALUE 99999999999;
+  AS BIGINT MAXVALUE 99999999999
+  OWNED BY ${myuniversity}_${mymodule}.hrid_settings.jsonb;
 
 ALTER SEQUENCE IF EXISTS ${myuniversity}_${mymodule}.hrid_items_seq
-  AS BIGINT MAXVALUE 99999999999;
+  AS BIGINT MAXVALUE 99999999999
+  OWNED BY ${myuniversity}_${mymodule}.hrid_settings.jsonb;
