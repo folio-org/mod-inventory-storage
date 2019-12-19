805a9265808aca756ff7e293cea98cbd70c29c88
Bohdan Suprun <bohdan_suprun@epam.com>
2019-12-19 17:15:26 +0200

    MODINVSTOR-416, MODINVSTOR-332: Add migration script for copyNumbers property.

---
+++ src/main/resources/templates/db_scripts/migrateItemCopyNumberToSingleValue.sql
@@ -0,0 +1,12 @@
+START TRANSACTION;
+-- Make the array of copy numbers as a single value, take the first value from the array
+UPDATE ${myuniversity}_${mymodule}.item AS it
+  SET jsonb = jsonb_set(it.jsonb, '{copyNumber}', it.jsonb#>'{copyNumbers, 0}')
+WHERE it.jsonb->'copyNumbers' IS NOT NULL AND jsonb_array_length(it.jsonb->'copyNumbers') > 0;
+
+-- Remove the copyNumbers property even if it has zero length.
+UPDATE ${myuniversity}_${mymodule}.item AS it
+  SET jsonb = it.jsonb - 'copyNumbers'
+WHERE it.jsonb->'copyNumbers' IS NOT NULL;
+
+END TRANSACTION;
