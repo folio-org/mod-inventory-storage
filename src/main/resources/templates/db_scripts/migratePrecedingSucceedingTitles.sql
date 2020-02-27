51fba9135d6fb2e5d70b2893d9e2413fa8d5a578
Svitlana-Zmiivska <Svitlana_Zmiivska@epam.com>
2020-02-27 14:37:15 +0200

    MODINVSTOR-447 Migrate connected preceding / succeeding titles from instance relationships to the new model

---
+++ src/main/resources/templates/db_scripts/migratePrecedingSucceedingTitles.sql
@@ -0,0 +1,8 @@
+INSERT INTO ${myuniversity}_${mymodule}.preceding_succeeding_title (id, jsonb)
+  SELECT id, jsonb_build_object(
+    'id', jsonb->'id',
+    'metadata', jsonb->'metadata',
+    'precedingInstanceId', jsonb->'superInstanceId',
+    'succeedingInstanceId', jsonb->'subInstanceId')
+  FROM ${myuniversity}_${mymodule}.instance_relationship as ir
+  WHERE ir.instanceRelationshipTypeId ='cde80cc2-0c8b-4672-82d4-721e51dcb990';
