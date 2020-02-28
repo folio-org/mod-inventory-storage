268e51758190e8d09fc274a478ad0efec37c6c0a
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-02-28 14:24:53 +0100

    Add each missing *Test.class to StorageTestSuite

--- src/test/java/org/folio/rest/api/StorageTestSuite.java
+++ src/test/java/org/folio/rest/api/StorageTestSuite.java
@@ -38,6 +38,7 @@ import static org.junit.Assert.assertThat;
   InstanceStorageTest.class,
   HoldingsStorageTest.class,
   ItemStorageTest.class,
+  HoldingsTypeTest.class,
   LoanTypeTest.class,
   MaterialTypeTest.class,
   ContributorTypesTest.class,
@@ -55,7 +56,10 @@ import static org.junit.Assert.assertThat;
   SampleDataTest.class,
   HridSettingsStorageTest.class,
   HridSettingsStorageParameterizedTest.class,
+  ItemCopyNumberMigrationScriptTest.class,
   ItemEffectiveCallNumberComponentsTest.class,
+  ItemEffectiveCallNumberDataUpgradeTest.class,
+  ModesOfIssuanceMigrationScriptTest.class,
   PrecedingSucceedingTitleTest.class
 })
 public class StorageTestSuite {
