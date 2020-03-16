7619a7295a1a704a8edfb0221957dbecd05ed821
Kevin Day <kday@library.tamu.edu>
2020-03-16 11:05:27 -0500

    MODINVSTOR-450: remove unused alias
    The alias appears to have been added without being used.
    It should either be removed or the rest of the query should use it.
    This commit opts to remove the alias.

--- src/main/resources/templates/db_scripts/renameModesOfIssuance.sql
+++ src/main/resources/templates/db_scripts/renameModesOfIssuance.sql
@@ -16,7 +16,7 @@ UPDATE ${myuniversity}_${mymodule}.mode_of_issuance AS it
 SET jsonb = jsonb_set(it.jsonb, '{name}', '"single unit"')
 WHERE it.jsonb->>'id' = '9d18a02f-5897-4c31-9106-c9abb5c7ae8b';
 
-UPDATE ${myuniversity}_${mymodule}.mode_of_issuance AS it
+UPDATE ${myuniversity}_${mymodule}.mode_of_issuance
 SET jsonb = jsonb || '{"name": "unspecified", "source": "folio"}'::jsonb
 WHERE jsonb->>'id' = '612bbd3d-c16b-4bfb-8517-2afafc60204a';
 
