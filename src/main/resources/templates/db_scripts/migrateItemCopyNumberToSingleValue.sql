16a51569ad3442c73fb80989fbbdfb7c5a9ffb56
Kevin Day <kday@library.tamu.edu>
2020-03-16 08:47:41 -0500

    MODINVSTOR-450: CASE can be used outside of pgsql
    This simplifies the logic and allows for not depending on any functions
    or triggers.

--- src/main/resources/templates/db_scripts/migrateItemCopyNumberToSingleValue.sql
+++ src/main/resources/templates/db_scripts/migrateItemCopyNumberToSingleValue.sql
@@ -6,30 +6,14 @@ WHERE tgrelid = '${myuniversity}_${mymodule}.item'::regclass::oid
   AND tgisinternal IS FALSE
   AND tgenabled = 'O';
 
--- Temporary Trigger: perform conditional logic to perform upgrades in one update without a WHERE.
-CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.temp_upgrade_items()
-  RETURNS TRIGGER AS $$
-  BEGIN
-    IF (TG_OP = 'UPDATE' AND NEW.jsonb->'copyNumbers' IS NOT NULL) THEN
-      IF (NEW.jsonb->>'copyNumbers' IS NOT NULL
-        AND jsonb_array_length(NEW.jsonb->'copyNumbers') > 0) THEN
-        NEW.jsonb := jsonb_set(NEW.jsonb, '{copyNumber}', NEW.jsonb#>'{copyNumbers, 0}');
-      END IF;
-      NEW.jsonb := NEW.jsonb - 'copyNumbers';
-    END IF;
-    RETURN NEW;
-  END;
-  $$ LANGUAGE 'plpgsql';
-CREATE TRIGGER temp_upgrade_items_trigger
-  BEFORE UPDATE ON ${myuniversity}_${mymodule}.item
-  FOR EACH ROW EXECUTE PROCEDURE ${myuniversity}_${mymodule}.temp_upgrade_items();
-
 UPDATE ${myuniversity}_${mymodule}.item
-  SET id = id
+SET jsonb = CASE WHEN
+  jsonb->>'copyNumbers' IS NOT NULL
+  AND jsonb_array_length(jsonb->'copyNumbers') > 0
+  THEN jsonb_set(jsonb - 'copyNumbers', '{copyNumber}', jsonb#>'{copyNumbers, 0}')
+  ELSE jsonb - 'copyNumbers'
+END
 WHERE jsonb->'copyNumbers' IS NOT NULL;
-
-DROP TRIGGER temp_upgrade_items_trigger ON ${myuniversity}_${mymodule}.item;
-DROP FUNCTION ${myuniversity}_${mymodule}.temp_upgrade_items;
 
 UPDATE pg_trigger
   SET tgenabled = 'O'
