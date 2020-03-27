eecc5bbc3b3eb2ed59806ab1053c346c9cf9c976
Dmytro Popov <dmytro_popov@epam.com>
2020-03-27 14:16:40 +0200

    MODINVSTOR-366: fix table names

--- src/main/resources/templates/db_scripts/softDeleteInHoldingsItemsInstances.sql
+++ src/main/resources/templates/db_scripts/softDeleteInHoldingsItemsInstances.sql
@@ -4,11 +4,11 @@
 
 alter table ${myuniversity}_${mymodule}.instance add "deleted_date" timestamp;
 alter table ${myuniversity}_${mymodule}.item add "deleted_date" timestamp;
-alter table ${myuniversity}_${mymodule}.holding_record add "deleted_date" timestamp;
+alter table ${myuniversity}_${mymodule}.holdings_record add "deleted_date" timestamp;
 
 alter table ${myuniversity}_${mymodule}.item rename to item_log;
 alter table ${myuniversity}_${mymodule}.instance rename to instance_log;
-alter table ${myuniversity}_${mymodule}.holding_record rename to holding_record_log;
+alter table ${myuniversity}_${mymodule}.holdings_record rename to holdings_record_log;
 
 CREATE OR REPLACE VIEW holdings_record AS SELECT * from holdings_record_log where deleted_date IS NULL;
 CREATE OR REPLACE VIEW instance AS SELECT * from instance_log where deleted_date IS NULL;
