3cff800c890c00597671649f16e4be6055f9104a
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-03-30 18:27:33 +0200

    Replace default SQL column name 'value' by 'identifier'

--- src/main/resources/templates/db_scripts/isbn.sql
+++ src/main/resources/templates/db_scripts/isbn.sql
@@ -2,7 +2,7 @@
 -- This takes each ISBN, normalizes it using RMB's normalize_digits(text),
 -- and concatenates the results using a space as separator.
 CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.normalize_isbns(jsonb_array jsonb) RETURNS text AS $$
-  SELECT string_agg(${myuniversity}_${mymodule}.normalize_digits(value->>'value'), ' ')
-  FROM jsonb_array_elements($1)
-  WHERE value->>'identifierTypeId' = '8261054f-be78-422d-bd51-4ed9f33c3422';
+  SELECT string_agg(${myuniversity}_${mymodule}.normalize_digits(identifier->>'value'), ' ')
+  FROM jsonb_array_elements($1) as identifier
+  WHERE identifier->>'identifierTypeId' = '8261054f-be78-422d-bd51-4ed9f33c3422';
 $$ LANGUAGE sql IMMUTABLE PARALLEL SAFE STRICT;
