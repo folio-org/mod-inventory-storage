2acb8be6c63bec21f80f6fd574e60fbe69943182
Dmytro Popov <dmytro_popov@epam.com>
2020-04-09 22:10:21 +0300

    MODOAIPMH-105: Drop deleted_date index

--- src/main/resources/templates/db_scripts/softdelete/softDeleteForHoldingsInstancesItems.sql
+++ src/main/resources/templates/db_scripts/softdelete/softDeleteForHoldingsInstancesItems.sql
@@ -1,15 +1,10 @@
 -- add deleted_date field to the tables
--- create indexees on deleted date
 -- rename instance, holdings and item tables
 -- create views with the name of the corresponding tables
 
 ALTER TABLE ${myuniversity}_${mymodule}.instance ADD "deleted_date" TIMESTAMP;
 ALTER TABLE ${myuniversity}_${mymodule}.item ADD "deleted_date" TIMESTAMP;
 ALTER TABLE ${myuniversity}_${mymodule}.holdings_record ADD "deleted_date" TIMESTAMP;
---
-CREATE INDEX instance_deleted_date__index ON ${myuniversity}_${mymodule}.instance (deleted_date);
-CREATE INDEX item_deleted_date__index ON ${myuniversity}_${mymodule}.item (deleted_date);
-CREATE INDEX holdings_record_deleted_date__index ON ${myuniversity}_${mymodule}.holdings_record (deleted_date);
 --
 ALTER TABLE ${myuniversity}_${mymodule}.instance RENAME TO instance_log;
 ALTER TABLE ${myuniversity}_${mymodule}.item RENAME TO item_log;
