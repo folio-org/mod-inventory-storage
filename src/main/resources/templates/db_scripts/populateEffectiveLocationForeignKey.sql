7a7394470bdd952fcbc071bb300a0fc161bcaeaa
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-04-14 17:13:49 +0300

    Revert "Revert trigger disabling for large migrations."
    This reverts commit 326733c75eb9197a8f6be3046395c49406022513.

--- src/main/resources/templates/db_scripts/populateEffectiveLocationForeignKey.sql
+++ src/main/resources/templates/db_scripts/populateEffectiveLocationForeignKey.sql
@@ -1,18 +1,18 @@
 START TRANSACTION;
 
---UPDATE pg_trigger
---  SET tgenabled = 'D'
---WHERE tgrelid = '${myuniversity}_${mymodule}.item'::regclass::oid
---  AND tgisinternal IS FALSE
---  AND tgenabled = 'O';
+UPDATE pg_trigger
+  SET tgenabled = 'D'
+WHERE tgrelid = '${myuniversity}_${mymodule}.item'::regclass::oid
+  AND tgisinternal IS FALSE
+  AND tgenabled = 'O';
 
 UPDATE ${myuniversity}_${mymodule}.item
   SET effectiveLocationId = (jsonb->>'effectiveLocationId')::uuid;
 
---UPDATE pg_trigger
---  SET tgenabled = 'O'
---WHERE tgrelid = '${myuniversity}_${mymodule}.item'::regclass::oid
---  AND tgisinternal IS FALSE
---  AND tgenabled = 'D';
+UPDATE pg_trigger
+  SET tgenabled = 'O'
+WHERE tgrelid = '${myuniversity}_${mymodule}.item'::regclass::oid
+  AND tgisinternal IS FALSE
+  AND tgenabled = 'D';
 
 END TRANSACTION;
