4b6dcf47539703c6eed511b2c0773dff895553d6
Dmytro Popov <dmytro_popov@epam.com>
2020-05-04 17:26:59 +0300

    MODINVSTOR-486: Use timestamp with time zone, update revert script

--- src/main/resources/templates/db_scripts/oaipmh/createOaiPmhViewFunction.sql
+++ src/main/resources/templates/db_scripts/oaipmh/createOaiPmhViewFunction.sql
@@ -1,20 +1,18 @@
-create or replace function ${myuniversity}_${mymodule}.strToTimestamp(text) returns timestamp as
+create or replace function ${myuniversity}_${mymodule}.strToTimestamp(text) returns timestamptz as
 $$
-select to_timestamp($1, 'YYYY-MM-DDTHH:MI:SS.MS''Z''')::timestamp
+select $1::timestamptz
 $$ language sql immutable
                 strict;
 
-create or replace function ${myuniversity}_${mymodule}.dateOrMin(timestamp with time zone) returns timestamp with time zone as
+create or replace function ${myuniversity}_${mymodule}.dateOrMin(timestamptz) returns timestamptz as
 $$
-select coalesce($1, timestamp '1970-01-01')
-$$ language sql immutable
-                strict;
+select coalesce($1, timestamptz '1970-01-01')
+$$ language sql immutable;
 
-create or replace function ${myuniversity}_${mymodule}.dateOrMax(timestamp with time zone) returns timestamp with time zone as
+create or replace function ${myuniversity}_${mymodule}.dateOrMax(timestamptz) returns timestamptz as
 $$
-select coalesce($1, timestamp '2050-01-01')
-$$ language sql immutable
-                strict;
+select coalesce($1, timestamptz '2050-01-01')
+$$ language sql immutable;
 
 create or replace function ${myuniversity}_${mymodule}.getElectronicAccessName(val jsonb) returns jsonb as
 $$
@@ -35,43 +33,43 @@ create index if not exists audit_holdings_record_createddate_idx on ${myuniversi
 
 create index if not exists audit_item_createddate_idx on ${myuniversity}_${mymodule}.audit_item ((strToTimestamp(jsonb ->> 'createdDate')));
 
-create or replace function ${myuniversity}_${mymodule}.pmh_view_function(startDate timestamp with time zone,
-                                                                         endDate timestamp with time zone,
+create or replace function ${myuniversity}_${mymodule}.pmh_view_function(startDate timestamptz,
+                                                                         endDate timestamptz,
                                                                          deletedRecordSupport bool default true,
                                                                          skipSuppressedFromDiscoveryRecords bool default true)
     returns table
             (
                 instanceId             uuid,
-                instanceUpdatedDate    timestamp with time zone,
+                instanceUpdatedDate    timestamptz,
                 deleted                boolean,
                 itemsAndHoldingsFields jsonb
             )
 as
 $body$
 with instanceIdsInRange as ( select inst.id                                              as instanceId,
-                                    (inst.jsonb #>> '{metadata,updatedDate}')::timestamp as maxDate
+                                    (inst.jsonb #>> '{metadata,updatedDate}')::timestamptz as maxDate
                              from ${myuniversity}_${mymodule}.instance inst
-                             where (inst.jsonb #>> '{metadata,updatedDate}')::timestamp between dateOrMin($1) and dateOrMax($2)
+                             where (inst.jsonb #>> '{metadata,updatedDate}')::timestamptz between dateOrMin($1) and dateOrMax($2)
 
                              union all
                              select instanceid,
-                                    greatest((item.jsonb #>> '{metadata,updatedDate}')::timestamp,
-                                             (hr.jsonb #>> '{metadata,updatedDate}')::timestamp) as maxDate
+                                    greatest((item.jsonb #>> '{metadata,updatedDate}')::timestamptz,
+                                             (hr.jsonb #>> '{metadata,updatedDate}')::timestamptz) as maxDate
                              from holdings_record hr
                                       join ${myuniversity}_${mymodule}.item item on item.holdingsrecordid = hr.id
-                             where ((hr.jsonb #>> '{metadata,updatedDate}')::timestamp between dateOrMin($1) and dateOrMax($2) or
-                                    (item.jsonb #>> '{metadata,updatedDate}')::timestamp between dateOrMin($1) and dateOrMax($2))
+                             where ((hr.jsonb #>> '{metadata,updatedDate}')::timestamptz between dateOrMin($1) and dateOrMax($2) or
+                                    (item.jsonb #>> '{metadata,updatedDate}')::timestamptz between dateOrMin($1) and dateOrMax($2))
 
                              union all
                              select (audit_holdings_record.jsonb #>> '{record,instanceId}')::uuid,
-                                    greatest((audit_item.jsonb #>> '{record,updatedDate}')::timestamp,
-                                             (audit_holdings_record.jsonb #>> '{record,updatedDate}')::timestamp) as maxDate
+                                    greatest((audit_item.jsonb #>> '{record,updatedDate}')::timestamptz,
+                                             (audit_holdings_record.jsonb #>> '{record,updatedDate}')::timestamptz) as maxDate
                              from audit_holdings_record audit_holdings_record
                                       join audit_item audit_item
                                            on (audit_item.jsonb ->> '{record,holdingsRecordId}')::uuid =
                                               audit_holdings_record.id
-                             where ((audit_holdings_record.jsonb #>> '{record,updatedDate}')::timestamp between dateOrMin($1) and dateOrMax($2) or
-                                    (audit_item.jsonb #>> '{record,updatedDate}')::timestamp between dateOrMin($1) and dateOrMax($2)) ),
+                             where ((audit_holdings_record.jsonb #>> '{record,updatedDate}')::timestamptz between dateOrMin($1) and dateOrMax($2) or
+                                    (audit_item.jsonb #>> '{record,updatedDate}')::timestamptz between dateOrMin($1) and dateOrMax($2)) ),
      instanceIdsAndDatesInRange as ( select instanceId, max(instanceIdsInRange.maxDate) as maxDate
                                      from instanceIdsInRange,
                                           instance
@@ -124,11 +122,11 @@ select instanceIdsAndDatesInRange.instanceId,
 from instanceIdsAndDatesInRange
 union all
 select (audit_instance.jsonb #>> '{record,id}')::uuid      as instanceId,
-       (jsonb ->> 'createdDate')::timestamp with time zone as maxDate,
+       (jsonb ->> 'createdDate')::timestamptz as maxDate,
        true                                                as deleted,
        null                                                as itemFields
 from ${myuniversity}_${mymodule}.audit_instance
 where $3
-  and (jsonb ->> 'createdDate')::timestamp with time zone between dateOrMin($1) and dateOrMax($2)
+  and (jsonb ->> 'createdDate')::timestamptz between dateOrMin($1) and dateOrMax($2)
 
 $body$ language sql;
--- src/main/resources/templates/db_scripts/oaipmh/revert_createOaiPmhViewFunction.sql
+++ src/main/resources/templates/db_scripts/oaipmh/revert_createOaiPmhViewFunction.sql
@@ -8,13 +8,12 @@ drop index if exists audit_holdings_record_createddate_idx;
 
 drop index if exists audit_item_createddate_idx;
 
-drop function if exists ${myuniversity}_${mymodule}.pmh_view_function;
+drop function if exists ${myuniversity}_${mymodule}.pmh_view_function(timestamptz, timestamptz, bool, bool);
 
-drop function if exists ${myuniversity}_${mymodule}.strToTimestamp;
+drop function if exists ${myuniversity}_${mymodule}.strToTimestamp(text);
 
-drop function if exists ${myuniversity}_${mymodule}.dateOrMin;
+drop function if exists ${myuniversity}_${mymodule}.dateOrMin(timestamptz);
 
-drop function if exists ${myuniversity}_${mymodule}.dateOrMax;
+drop function if exists ${myuniversity}_${mymodule}.dateOrMax(timestamptz);
 
-drop function if exists ${myuniversity}_${mymodule}.getElectronicAccessName;
-
+drop function if exists ${myuniversity}_${mymodule}.getElectronicAccessName(val jsonb);
