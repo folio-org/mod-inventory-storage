fe8f8777a24904bd9ee850527e67c87155ba1c9e
Dmytro Popov <dmytro_popov@epam.com>
2020-07-02 19:36:29 +0300

    MODINVSTOR-527: Update indexes and pmh view function to improve performance

--- src/main/resources/templates/db_scripts/oaipmh/createOaiPmhViewFunction.sql
+++ src/main/resources/templates/db_scripts/oaipmh/createOaiPmhViewFunction.sql
@@ -1,10 +1,3 @@
--- This is needed to drop indexes with "old" name
-drop index if exists ${myuniversity}_${mymodule}.item_metadata_updateddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.holdings_record_metadata_updateddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.audit_instance_createddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.audit_holdings_record_createddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.audit_item_createddate_idx;
-
 create or replace function ${myuniversity}_${mymodule}.strToTimestamp(text) returns timestamptz as
 $$
 select $1::timestamptz
@@ -25,23 +18,21 @@ create or replace function ${myuniversity}_${mymodule}.getElectronicAccessName(v
 $$
 select jsonb_agg(distinct e)
 from ( select e || jsonb_build_object('name', ( select jsonb ->> 'name'
-                                                from ${myuniversity}_${mymodule}.electronic_access_relationship ear
+                                                from electronic_access_relationship ear
                                                 where id = (e ->> 'relationshipId')::uuid )) e
        from jsonb_array_elements($1) as e ) e1
 $$ language sql strict;
 
-drop index if exists ${myuniversity}_${mymodule}.item_pmh_metadata_updateddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.holdings_record_pmh_metadata_updateddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.audit_instance_pmh_createddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.audit_holdings_record_pmh_createddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.audit_item_pmh_createddate_idx;
-drop index if exists ${myuniversity}_${mymodule}.instance_pmh_metadata_updateddate_idx;
-
 create index if not exists instance_pmh_metadata_updateddate_idx on ${myuniversity}_${mymodule}.instance ((strToTimestamp(jsonb -> 'metadata' ->> 'updatedDate')));
+
 create index if not exists item_pmh_metadata_updateddate_idx on ${myuniversity}_${mymodule}.item ((strToTimestamp(jsonb -> 'metadata' ->> 'updatedDate')));
-create index if not exists holdings_record_pmh_metadata_updateddate_idx on ${myuniversity}_${mymodule}.holdings_record ((strToTimestamp(jsonb -> 'metadata' ->> 'updatedDate')));
+
+create index if not exists holdings_pmh_record_metadata_updateddate_idx on ${myuniversity}_${mymodule}.holdings_record ((strToTimestamp(jsonb -> 'metadata' ->> 'updatedDate')));
+
 create index if not exists audit_instance_pmh_createddate_idx on ${myuniversity}_${mymodule}.audit_instance ((strToTimestamp(jsonb ->> 'createdDate')));
+
 create index if not exists audit_holdings_record_pmh_createddate_idx on ${myuniversity}_${mymodule}.audit_holdings_record ((strToTimestamp(jsonb -> 'record' ->> 'updatedDate')));
+
 create index if not exists audit_item_pmh_createddate_idx on ${myuniversity}_${mymodule}.audit_item ((strToTimestamp(jsonb -> 'record' ->> 'updatedDate')));
 
 create or replace function ${myuniversity}_${mymodule}.pmh_view_function(startDate timestamptz,
@@ -57,7 +48,7 @@ create or replace function ${myuniversity}_${mymodule}.pmh_view_function(startDa
             )
 as
 $body$
-with instanceIdsInRange as ( select inst.id                                                      as instanceId,
+with instanceIdsInRange as ( select inst.id                                              as instanceId,
                                     (strToTimestamp(inst.jsonb -> 'metadata' ->> 'updatedDate')) as maxDate
                              from ${myuniversity}_${mymodule}.instance inst
                              where (strToTimestamp(inst.jsonb -> 'metadata' ->> 'updatedDate')) between dateOrMin($1) and dateOrMax($2)
@@ -73,8 +64,8 @@ with instanceIdsInRange as ( select inst.id                                     
 
                              union all
                              select (audit_holdings_record.jsonb #>> '{record,instanceId}')::uuid,
-                                    greatest((strtotimestamp(audit_item.jsonb -> 'record' ->> 'updatedDate')),
-                                             (strtotimestamp(audit_holdings_record.jsonb -> 'record' ->> 'updatedDate'))) as maxDate
+                                    greatest((audit_item.jsonb #>> '{record,updatedDate}')::timestamptz,
+                                             (audit_holdings_record.jsonb #>> '{record,updatedDate}')::timestamptz) as maxDate
                              from audit_holdings_record audit_holdings_record
                                       join audit_item audit_item
                                            on (audit_item.jsonb ->> '{record,holdingsRecordId}')::uuid =
@@ -109,14 +100,9 @@ select instanceIdsAndDatesInRange.instanceId,
                                                                                                      itemLocLib.id,
                                                                                                      'libraryName',
                                                                                                      itemLocLib.jsonb ->> 'name')),
-                                                    'volume',
-                                                    item.jsonb -> 'volume',
-                                                    'enumeration',
-                                                    item.jsonb -> 'enumeration',
-                                                    'materialType',
-                                                    mt.jsonb -> 'name',
-                                                    'electronicAccess',
-                                                    getElectronicAccessName(
+                                                    'volume', item.jsonb -> 'volume', 'enumeration',
+                                                    item.jsonb -> 'enumeration', 'materialType', mt.jsonb -> 'name',
+                                                    'electronicAccess', getElectronicAccessName(
                                                                 coalesce(item.jsonb #> '{electronicAccess}', '[]'::jsonb) ||
                                                                 coalesce(hr.jsonb #> '{electronicAccess}', '[]'::jsonb)))) items
                 from holdings_record hr
@@ -137,10 +123,10 @@ select instanceIdsAndDatesInRange.instanceId,
                 group by 1 ) itemAndHoldingsAttrs )
 from instanceIdsAndDatesInRange
 union all
-select (audit_instance.jsonb #>> '{record,id}')::uuid as instanceId,
-       strToTimestamp(jsonb ->> 'createdDate')         as maxDate,
-       true                                           as deleted,
-       null                                           as itemFields
+select (audit_instance.jsonb #>> '{record,id}')::uuid      as instanceId,
+       (jsonb ->> 'createdDate')::timestamptz as maxDate,
+       true                                                as deleted,
+       null                                                as itemFields
 from ${myuniversity}_${mymodule}.audit_instance
 where $3
   and strToTimestamp(jsonb ->> 'createdDate') between dateOrMin($1) and dateOrMax($2)
