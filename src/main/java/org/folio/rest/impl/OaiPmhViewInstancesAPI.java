9a41b7053004da65c3c9af3554a8cc3c8ded46ef
Oleksandr Yatsenko <Oleksandr_Yatsenko1@epam.com>
2020-07-21 20:31:10 +0300

    Fixed code according to code reviewers comments:   - added marking methods with @Validate according to RAML specification.

--- src/main/java/org/folio/rest/impl/OaiPmhViewInstancesAPI.java
+++ src/main/java/org/folio/rest/impl/OaiPmhViewInstancesAPI.java
@@ -15,6 +15,7 @@ import javax.validation.constraints.Pattern;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang.StringUtils;
+import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.OaipmhInstanceIds;
 import org.folio.rest.jaxrs.resource.OaiPmhView;
 import org.folio.rest.persist.PgUtil;
@@ -44,6 +45,7 @@ public class OaiPmhViewInstancesAPI implements OaiPmhView {
   private static final String SQL_UPDATED_INSTANCES_IDS = "select * from pmh_get_updated_instances_ids($1,$2,$3,$4);";
   private static final String SQL_INSTANCES = "select * from pmh_instance_view_function($1,$2);";
 
+  @Validate
   @Override
   public void getOaiPmhViewInstances(String startDate, String endDate, boolean deletedRecordSupport,
       boolean skipSuppressedFromDiscoveryRecords, String lang, RoutingContext routingContext, Map<String, String> okapiHeaders,
@@ -94,6 +96,7 @@ public class OaiPmhViewInstancesAPI implements OaiPmhView {
     }
   }
 
+  @Validate
   @Override
   public void postOaiPmhViewEnrichedInstances(OaipmhInstanceIds entity, RoutingContext routingContext,
       Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
@@ -106,6 +109,7 @@ public class OaiPmhViewInstancesAPI implements OaiPmhView {
       "Select from oai pmh instances view completed successfully");
   }
 
+  @Validate
   @Override
   public void getOaiPmhViewUpdatedInstanceIds(String startDate, String endDate, boolean deletedRecordSupport, boolean skipSuppressedFromDiscoveryRecords, @Pattern(regexp = "[a-zA-Z]{2}") String lang, RoutingContext routingContext,
       Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
