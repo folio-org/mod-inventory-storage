cc47cea9361760f0430e65a86bd4b407e51c948c
Julian Ladisch <julianladisch@users.noreply.github.com>
2020-09-02 17:55:28 +0200

    Replace populateMetaDataForList by RMB's MetadataUtil.populateMetadata

--- src/main/java/org/folio/rest/impl/InstanceStorageBatchAPI.java
+++ src/main/java/org/folio/rest/impl/InstanceStorageBatchAPI.java
@@ -7,22 +7,19 @@ import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.folio.okapi.common.OkapiToken;
 import org.folio.rest.jaxrs.model.Instance;
 import org.folio.rest.jaxrs.model.Instances;
 import org.folio.rest.jaxrs.model.InstancesBatchResponse;
-import org.folio.rest.jaxrs.model.Metadata;
 import org.folio.rest.jaxrs.resource.InstanceStorageBatchInstances;
 import org.folio.rest.persist.PgUtil;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.support.HridManager;
+import org.folio.rest.tools.utils.MetadataUtil;
 import javax.ws.rs.core.Response;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -30,8 +27,6 @@ import java.util.stream.Collectors;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.folio.rest.RestVerticle.MODULE_SPECIFIC_ARGS;
-import static org.folio.rest.RestVerticle.OKAPI_HEADER_TOKEN;
-import static org.folio.rest.RestVerticle.OKAPI_USERID_HEADER;
 
 public class InstanceStorageBatchAPI implements InstanceStorageBatchInstances {
 
@@ -49,7 +44,7 @@ public class InstanceStorageBatchAPI implements InstanceStorageBatchInstances {
     vertxContext.runOnContext(v -> {
       try {
         PostgresClient postgresClient = PgUtil.postgresClient(vertxContext, okapiHeaders);
-        populateMetaDataForList(entity.getInstances(), okapiHeaders);
+        MetadataUtil.populateMetadata(entity.getInstances(), okapiHeaders);
         executeInBatch(entity.getInstances(),
           (instances, saveFutures) -> saveInstances(instances, postgresClient, saveFutures))
           .setHandler(ar -> {
@@ -179,29 +174,5 @@ public class InstanceStorageBatchAPI implements InstanceStorageBatchInstances {
     response.setTotalRecords(response.getInstances().size());
     return response;
   }
-
-  private void populateMetaDataForList(List<Instance> list, Map<String, String> okapiHeaders) {
-    String userId = okapiHeaders.getOrDefault(OKAPI_USERID_HEADER, "");
-    String token = okapiHeaders.getOrDefault(OKAPI_HEADER_TOKEN, "");
-    if (userId == null && token != null) {
-      userId = userIdFromToken(token);
-    }
-    Metadata md = new Metadata();
-    md.setUpdatedDate(new Date());
-    md.setCreatedDate(md.getUpdatedDate());
-    md.setCreatedByUserId(userId);
-    md.setUpdatedByUserId(userId);
-    list.forEach(instance -> instance.setMetadata(md));
-  }
-
-  private static String userIdFromToken(String token) {
-    try {
-      return new OkapiToken(token).getUserIdWithoutValidation();
-    } catch (Exception e) {
-      log.warn("Invalid x-okapi-token: " + token, e);
-      return null;
-    }
-  }
-
 
 }
