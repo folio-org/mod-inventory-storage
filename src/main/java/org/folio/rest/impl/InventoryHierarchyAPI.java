f27d67da747da373e787af85bc2853a840eb2f42
Illia Daliek <illia_daliek@epam.com>
2020-10-08 15:57:50 +0300

    fix initial load

--- src/main/java/org/folio/rest/impl/InventoryHierarchyAPI.java
+++ src/main/java/org/folio/rest/impl/InventoryHierarchyAPI.java
@@ -1,7 +1,5 @@
 package org.folio.rest.impl;
 
-import static java.lang.String.format;
-
 import java.util.Map;
 import java.util.UUID;
 
@@ -23,17 +21,18 @@ public class InventoryHierarchyAPI extends AbstractInstanceRecordsAPI implements
 
   private static final String SQL_UPDATED_INSTANCES_IDS = "select * from get_updated_instance_ids_view($1,$2,$3,$4,$5);";
   private static final String SQL_INSTANCES = "select * from get_items_and_holdings_view($1,$2);";
-  private static final String SQL_INITIAL_LOAD = "SELECT id as instanceId,\n" +
+  private static final String SUPPRESSED_TRUE_FILTER = " WHERE (instance.jsonb ->> 'discoverySuppress')::bool = false";
+  private static final String SQL_INITIAL_LOAD = "SELECT id as \"instanceId\",\n" +
     "       instance.jsonb ->> 'source' AS source,\n" +
-    "       strToTimestamp(instance.jsonb -> 'metadata' ->> 'updatedDate') AS updatedDate,\n" +
-    "       (instance.jsonb ->> 'discoverySuppress')::bool AS suppressFromDiscovery,\n" +
+    "       strToTimestamp(instance.jsonb -> 'metadata' ->> 'updatedDate') AS \"updatedDate\",\n" +
+    "       (instance.jsonb ->> 'discoverySuppress')::bool AS \"suppressFromDiscovery\",\n" +
     "       false AS deleted\n" +
-    "FROM instance WHERE (instance.jsonb ->> 'discoverySuppress')::bool = %s";
-  private static final String SQL_INITIAL_LOAD_DELETED_RECORDS_SUPPORT_PART = "UNION ALL\n" +
-    "\tSELECT (jsonb #>> '{record,id}')::uuid            AS instanceId,\n" +
+    "FROM instance";
+  private static final String SQL_INITIAL_LOAD_DELETED_RECORDS_SUPPORT_PART = " UNION ALL\n" +
+    "\tSELECT (jsonb #>> '{record,id}')::uuid            AS \"instanceId\",\n" +
     "        jsonb #>> '{record,source}'                 AS source,\n" +
-    "        strToTimestamp(jsonb ->> 'createdDate')     AS updatedDate,\n" +
-    "        false                                       AS suppressFromDiscovery,\n" +
+    "        strToTimestamp(jsonb ->> 'createdDate')     AS \"updatedDate\",\n" +
+    "        false                                       AS \"suppressFromDiscovery\",\n" +
     "        true                                        AS deleted\n" +
     "\tFROM audit_instance";
 
@@ -42,20 +41,25 @@ public class InventoryHierarchyAPI extends AbstractInstanceRecordsAPI implements
   public void getInventoryHierarchyUpdatedInstanceIds(String startDate, String endDate, boolean deletedRecordSupport, boolean skipSuppressedFromDiscoveryRecords,
       boolean onlyInstanceUpdateDate, @Pattern(regexp = "[a-zA-Z]{2}") String lang, RoutingContext routingContext, Map<String, String> okapiHeaders,
       Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-    String sql;
     if(StringUtils.isEmpty(startDate) && StringUtils.isEmpty(endDate)) {
-      sql = deletedRecordSupport ? SQL_INITIAL_LOAD + SQL_INITIAL_LOAD_DELETED_RECORDS_SUPPORT_PART : SQL_INITIAL_LOAD;
-      sql = format(sql, skipSuppressedFromDiscoveryRecords);
+      String sql = SQL_INITIAL_LOAD;
+      if(skipSuppressedFromDiscoveryRecords) {
+        sql+=SUPPRESSED_TRUE_FILTER;
+      }
+      if(deletedRecordSupport) {
+        sql+=SQL_INITIAL_LOAD_DELETED_RECORDS_SUPPORT_PART;
+      }
       fetchRecordsByQuery(sql, () -> new ArrayTuple(0),
         routingContext, okapiHeaders, asyncResultHandler, vertxContext,
         "Get updated instances completed successfully");
+    } else {
+      fetchRecordsByQuery(SQL_UPDATED_INSTANCES_IDS,
+        () -> createPostgresParams(startDate, endDate, deletedRecordSupport, skipSuppressedFromDiscoveryRecords, tuple -> {
+          tuple.addBoolean(onlyInstanceUpdateDate);
+        }),
+        routingContext, okapiHeaders, asyncResultHandler, vertxContext,
+        "Get updated instances completed successfully");
     }
-    fetchRecordsByQuery(SQL_UPDATED_INSTANCES_IDS,
-      () -> createPostgresParams(startDate, endDate, deletedRecordSupport, skipSuppressedFromDiscoveryRecords, tuple -> {
-        tuple.addBoolean(onlyInstanceUpdateDate);
-      }),
-      routingContext, okapiHeaders, asyncResultHandler, vertxContext,
-      "Get updated instances completed successfully");
   }
 
   @Validate
