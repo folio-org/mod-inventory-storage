d38e4a0b702a814e03f71ce6182e3641bbc9705f
Kyle Felker <felkerk@gvsu.edu>
2020-11-04 09:41:11 -0500

    Update src/main/java/org/folio/rest/support/StatusUpdatedDateGenerator.java
    Co-authored-by: Marc Johnson <marc.johnson@k-int.com>

--- src/main/java/org/folio/rest/support/StatusUpdatedDateGenerator.java
+++ src/main/java/org/folio/rest/support/StatusUpdatedDateGenerator.java
@@ -4,10 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public final class StatusUpdatedDateGenerator {
-
     public static String generateStatusUpdatedDate() {
       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
       return sdf.format(new Date());
     } 
-    
 }
