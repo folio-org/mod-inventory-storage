94496908a90131271c1449ee2ab6ee163b5c42ef
Andrii_Novytskyi <Andrii_Novytskyi@epam.com>
2020-12-03 18:47:01 +0200

    MODINVSTOR-626 Initial commit

--- src/main/java/org/folio/rest/impl/InstanceBulkAPI.java
+++ src/main/java/org/folio/rest/impl/InstanceBulkAPI.java
@@ -8,9 +8,10 @@ import org.folio.cql2pgjson.CQL2PgJSON;
 import org.folio.cql2pgjson.exception.FieldException;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.InstanceBulkIdsGetField;
+import org.folio.rest.jaxrs.model.InstanceBulkIdsGetRecordType;
 import org.folio.rest.persist.PgUtil;
 import org.folio.rest.persist.cql.CQLWrapper;
-import org.folio.rest.support.InstanceID;
+import org.folio.rest.support.RecordID;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
@@ -22,6 +23,8 @@ import io.vertx.ext.web.RoutingContext;
 
 public class InstanceBulkAPI implements org.folio.rest.jaxrs.resource.InstanceBulk {
   public static final String INSTANCE_TABLE = "instance";
+  public static final String HOLDING_TABLE = "holdings_record";
+  public static final String HOLDING_TYPE = "HOLDINGS";
 
   private static final Logger LOG = LoggerFactory.getLogger(InstanceBulkAPI.class);
 
@@ -33,15 +36,21 @@ public class InstanceBulkAPI implements org.folio.rest.jaxrs.resource.InstanceBu
   @Validate
   @Override
   public void getInstanceBulkIds(InstanceBulkIdsGetField field,
-      String query, String lang, RoutingContext routingContext,
-      Map<String, String> okapiHeaders,
-      Handler<AsyncResult<Response>> asyncResultHandler,
-      Context vertxContext) {
+        InstanceBulkIdsGetRecordType recordType, String query, String lang,
+        RoutingContext routingContext, Map<String, String> okapiHeaders,
+        Handler<AsyncResult<Response>> asyncResultHandler,
+        Context vertxContext) {
 
     try {
       CQLWrapper wrapper = getCQL(query);
-      PgUtil.streamGet(INSTANCE_TABLE, InstanceID.class, wrapper, null,
-        "ids", routingContext, okapiHeaders, vertxContext);
+
+      if (recordType.toString().equals(HOLDING_TYPE)) {
+        PgUtil.streamGet(HOLDING_TABLE, RecordID.class, wrapper, null,
+          "ids", routingContext, okapiHeaders, vertxContext);
+      } else {
+        PgUtil.streamGet(INSTANCE_TABLE, RecordID.class, wrapper, null,
+          "ids", routingContext, okapiHeaders, vertxContext);
+      }
     } catch (Exception e) {
       LOG.error(e.getMessage(), e);
       asyncResultHandler.handle(Future.succeededFuture(GetInstanceBulkIdsResponse
