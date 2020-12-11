0d0dde2b97f334e6eeeaf44f40517ef478fa5991
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-12-11 17:20:41 +0200

    Add tests for utility method.

---
+++ src/test/java/org/folio/rest/support/CompletableFutureUtilTest.java
@@ -0,0 +1,30 @@
+package org.folio.rest.support;
+
+import static io.vertx.core.Future.failedFuture;
+import static io.vertx.core.Future.succeededFuture;
+import static org.folio.rest.support.CompletableFutureUtil.mapFutureResultToJavaFuture;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.Test;
+
+public class CompletableFutureUtilTest {
+  @Test
+  public void shouldReturnSuccess() {
+    final CompletableFuture<Void> future = new CompletableFuture<>();
+
+    mapFutureResultToJavaFuture(future).handle(succeededFuture());
+
+    assertTrue(future.isDone());
+  }
+
+  @Test
+  public void shouldReturnFailureWhenHandlerFailed() {
+    final CompletableFuture<Void> future = new CompletableFuture<>();
+
+    mapFutureResultToJavaFuture(future).handle(failedFuture("error"));
+
+    assertTrue(future.isCompletedExceptionally());
+  }
+}
