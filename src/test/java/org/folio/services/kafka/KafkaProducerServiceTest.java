ae2638583ed8d7a4c3c83b777909ec62c4272de8
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-12-18 15:27:32 +0200

    Fix test.

--- src/test/java/org/folio/services/kafka/KafkaProducerServiceTest.java
+++ src/test/java/org/folio/services/kafka/KafkaProducerServiceTest.java
@@ -4,6 +4,7 @@ import static org.folio.services.kafka.topic.KafkaTopic.INVENTORY_INSTANCE;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.mock;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -16,7 +17,7 @@ public class KafkaProducerServiceTest {
   public void canThrowJacksonExceptionWhenCannotParseObject(){
     final var producer = new KafkaProducerService(mock(KafkaProducer.class));
 
-    assertThrows("Unable to deserialize message", IllegalArgumentException.class,
+    assertThrows("Unable to deserialize message", ExecutionException.class,
       () -> producer.sendMessage("id", new Object(), INVENTORY_INSTANCE)
       .toCompletionStage().toCompletableFuture().get(1, TimeUnit.SECONDS));
   }
