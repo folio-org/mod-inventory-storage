628fb263ab51589d9738889151d43c7ffacff221
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-12-21 16:27:20 +0200

    Fix code smell.

--- src/main/java/org/folio/rest/exceptions/InventoryProcessingException.java
+++ src/main/java/org/folio/rest/exceptions/InventoryProcessingException.java
@@ -3,7 +3,7 @@ package org.folio.rest.exceptions;
 import java.util.Objects;
 
 public abstract class InventoryProcessingException extends RuntimeException {
-  public InventoryProcessingException(Object message) {
+  protected InventoryProcessingException(Object message) {
     super(Objects.toString(message));
   }
 }
