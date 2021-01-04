075d52e7be966a6b5e000e25ddd72ebf3d601308
Bohdan Suprun <Bohdan_Suprun@epam.com>
2021-01-04 13:04:36 +0200

    Refactor responseUtil and batch operation context.

--- src/main/java/org/folio/rest/support/ResponseUtil.java
+++ src/main/java/org/folio/rest/support/ResponseUtil.java
@@ -5,22 +5,24 @@ import static org.folio.HttpStatus.HTTP_NO_CONTENT;
 
 import javax.ws.rs.core.Response;
 
+import org.folio.HttpStatus;
+
 public final class ResponseUtil {
   private ResponseUtil() {}
 
   public static boolean isUpdateSuccessResponse(Response response) {
-    return responseIsInStatus(response, HTTP_NO_CONTENT.toInt());
+    return responseHasStatus(response, HTTP_NO_CONTENT);
   }
 
   public static boolean isDeleteSuccessResponse(Response response) {
-    return responseIsInStatus(response, HTTP_NO_CONTENT.toInt());
+    return responseHasStatus(response, HTTP_NO_CONTENT);
   }
 
   public static boolean isCreateSuccessResponse(Response response) {
-    return responseIsInStatus(response, HTTP_CREATED.toInt());
+    return responseHasStatus(response, HTTP_CREATED);
   }
 
-  private static boolean responseIsInStatus(Response response, int expectedStatus) {
-    return response != null && response.getStatus() == expectedStatus;
+  private static boolean responseHasStatus(Response response, HttpStatus expectedStatus) {
+    return response != null && response.getStatus() == expectedStatus.toInt();
   }
 }
--- src/main/java/org/folio/services/batch/BatchOperationContext.java
+++ src/main/java/org/folio/services/batch/BatchOperationContext.java
@@ -3,8 +3,6 @@ package org.folio.services.batch;
 import static java.util.Collections.unmodifiableList;
 import static java.util.stream.Stream.concat;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -13,20 +11,20 @@ public final class BatchOperationContext<T>{
   private final List<T> recordsToBeUpdated;
   private final List<T> existingRecordsBeforeUpdate;
 
-  public BatchOperationContext(Collection<T> recordsToBeCreated, Collection<T> recordsToBeUpdated,
-    Collection<T> existingRecords) {
+  public BatchOperationContext(List<T> recordsToBeCreated, List<T> recordsToBeUpdated,
+    List<T> existingRecords) {
 
-    this.recordsToBeCreated = new ArrayList<>(recordsToBeCreated);
-    this.recordsToBeUpdated = new ArrayList<>(recordsToBeUpdated);
-    this.existingRecordsBeforeUpdate = new ArrayList<>(existingRecords);
+    this.recordsToBeCreated = unmodifiableList(recordsToBeCreated);
+    this.recordsToBeUpdated = unmodifiableList(recordsToBeUpdated);
+    this.existingRecordsBeforeUpdate = unmodifiableList(existingRecords);
   }
 
   public List<T> getRecordsToBeCreated() {
-    return unmodifiableList(recordsToBeCreated);
+    return recordsToBeCreated;
   }
 
   public List<T> getExistingRecordsBeforeUpdate() {
-    return unmodifiableList(existingRecordsBeforeUpdate);
+    return existingRecordsBeforeUpdate;
   }
 
   public Stream<T> allRecordsStream() {
