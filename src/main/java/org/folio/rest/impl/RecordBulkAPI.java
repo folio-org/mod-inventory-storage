c2c5c33b41052bd7a127a3e7fd46e84b73abe201
Illia Borysenko <60609010+illia-borysenko@users.noreply.github.com>
2021-01-15 14:58:57 +0200

    MODINVSTOR-657 Add limit query param to /instance-bulk/ids endpoint (#563)

--- src/main/java/org/folio/rest/impl/RecordBulkAPI.java
+++ src/main/java/org/folio/rest/impl/RecordBulkAPI.java
@@ -1,9 +1,10 @@
 package org.folio.rest.impl;
 
-import java.util.Map;
-
-import javax.ws.rs.core.Response;
-
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.ext.web.RoutingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.cql2pgjson.CQL2PgJSON;
@@ -11,15 +12,14 @@ import org.folio.cql2pgjson.exception.FieldException;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.RecordBulkIdsGetField;
 import org.folio.rest.jaxrs.model.RecordBulkIdsGetRecordType;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
 import org.folio.rest.persist.PgUtil;
 import org.folio.rest.persist.cql.CQLWrapper;
 import org.folio.rest.support.RecordID;
 
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Context;
-import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.ext.web.RoutingContext;
+import javax.ws.rs.core.Response;
+import java.util.Map;
 
 public class RecordBulkAPI implements org.folio.rest.jaxrs.resource.RecordBulk {
   public static final String INSTANCE_TABLE = "instance";
@@ -28,26 +28,27 @@ public class RecordBulkAPI implements org.folio.rest.jaxrs.resource.RecordBulk {
 
   private static final Logger LOG = LogManager.getLogger();
 
-  private CQLWrapper getCQL(String query, String table) throws FieldException {
+  private CQLWrapper getCQL(String query, String table, int limit, int offset) throws FieldException {
     CQL2PgJSON cql2pgJson = new CQL2PgJSON(table + ".jsonb");
-    return new CQLWrapper(cql2pgJson, query);
+    return new CQLWrapper(cql2pgJson, query)
+      .setLimit(new Limit(limit))
+      .setOffset(new Offset(offset));
   }
 
   @Validate
   @Override
   public void getRecordBulkIds(RecordBulkIdsGetField field,
-        RecordBulkIdsGetRecordType recordType, String query, String lang,
-        RoutingContext routingContext, Map<String, String> okapiHeaders,
-        Handler<AsyncResult<Response>> asyncResultHandler,
+        RecordBulkIdsGetRecordType recordType, int limit, String query,
+        int offset, String lang, RoutingContext routingContext,
+        Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler,
         Context vertxContext) {
-
     try {
       if (recordType.toString().equalsIgnoreCase(HOLDING_TYPE)) {
-        CQLWrapper wrapper = getCQL(query, HOLDING_TABLE);
+        CQLWrapper wrapper = getCQL(query, HOLDING_TABLE, limit, offset);
         PgUtil.streamGet(HOLDING_TABLE, RecordID.class, wrapper, null,
           "ids", routingContext, okapiHeaders, vertxContext);
       } else {
-        CQLWrapper wrapper = getCQL(query, INSTANCE_TABLE);
+        CQLWrapper wrapper = getCQL(query, INSTANCE_TABLE, limit, offset);
         PgUtil.streamGet(INSTANCE_TABLE, RecordID.class, wrapper, null,
           "ids", routingContext, okapiHeaders, vertxContext);
       }
