421e0b54acc2bf0577d99ee70b7f90adaf5c6435
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2021-02-19 13:02:48 +0300

    Fix MODINVSTOR-675 (ERROR: cannot replace existing key).

--- src/main/resources/templates/db_scripts/populateRetainLeadingZeroesSetting.sql
+++ src/main/resources/templates/db_scripts/populateRetainLeadingZeroesSetting.sql
@@ -4,6 +4,8 @@ UPDATE ${myuniversity}_${mymodule}.hrid_settings
   SET jsonb = (SELECT jsonb_insert(jsonb, '{commonRetainLeadingZeroes}','true', true)
                FROM ${myuniversity}_${mymodule}.hrid_settings
                WHERE id = 'a501f2a8-5b31-48b2-874d-2191e48db8cd')
-WHERE id = 'a501f2a8-5b31-48b2-874d-2191e48db8cd';
+WHERE id = 'a501f2a8-5b31-48b2-874d-2191e48db8cd'
+     AND (SELECT jsonb::json->'commonRetainLeadingZeroes'
+      FROM ${myuniversity}_${mymodule}.hrid_settings WHERE id = 'a501f2a8-5b31-48b2-874d-2191e48db8cd') IS NULL;
 
 END TRANSACTION;
