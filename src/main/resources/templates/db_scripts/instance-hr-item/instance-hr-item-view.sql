007ab079eab3e95333369a86983ae6880e182694
Bohdan Suprun <Bohdan_Suprun@epam.com>
2021-03-09 19:34:19 +0200

    MODINVSTOR-693: Return instances without holdings or (and) items.

--- src/main/resources/templates/db_scripts/instance-hr-item/instance-hr-item-view.sql
+++ src/main/resources/templates/db_scripts/instance-hr-item/instance-hr-item-view.sql
@@ -1,12 +1,17 @@
 CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.instance_holdings_item_view
 AS
-SELECT instance.id as id,
+SELECT instance.id as id, (instance.jsonb ||
   JSONB_BUILD_OBJECT(
-	  'instanceId', instance.id,
-	  'instance', instance.jsonb,
-	  'holdingsRecords', jsonb_agg(DISTINCT holdings_record.jsonb),
-	  'items', jsonb_agg(item.jsonb)) AS jsonb
+	  'holdings', COALESCE(
+		  jsonb_agg(DISTINCT holdings_record.jsonb)
+		  FILTER (WHERE holdings_record.id IS NOT NULL),
+		  '[]'::jsonb),
+	  'items', COALESCE(
+		  jsonb_agg(DISTINCT item.jsonb)
+		  FILTER (WHERE item.id IS NOT NULL),
+		  '[]'::jsonb))
+	) AS jsonb
   FROM ${myuniversity}_${mymodule}.instance
-    JOIN ${myuniversity}_${mymodule}.holdings_record ON instance.id = holdings_record.instanceId
-    JOIN ${myuniversity}_${mymodule}.item ON holdings_record.id = item.holdingsRecordId
+    LEFT JOIN ${myuniversity}_${mymodule}.holdings_record ON instance.id = holdings_record.instanceId
+    LEFT JOIN ${myuniversity}_${mymodule}.item ON holdings_record.id = item.holdingsRecordId
   GROUP BY instance.id;
--- src/test/java/org/folio/rest/api/InventoryViewTest.java
+++ src/test/java/org/folio/rest/api/InventoryViewTest.java
@@ -6,12 +6,17 @@ import static org.folio.rest.api.ItemStorageTest.nod;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import org.folio.rest.jaxrs.model.Holding;
 import org.folio.rest.jaxrs.model.HoldingsItem;
-import org.folio.rest.jaxrs.model.HoldingsRecords2;
 import org.folio.rest.jaxrs.model.InventoryViewInstance;
+import org.folio.rest.support.IndividualResource;
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
@@ -19,41 +24,120 @@ public class InventoryViewTest extends TestBaseWithInventoryUtil {
   @Test
   public void shouldReturnInstanceWithRecords() {
     var instanceOne = instancesClient.create(instance(randomUUID()));
-    var holdingForOne = createHolding(instanceOne.getId(), mainLibraryLocationId, null);
-    createHolding(instanceOne.getId(), secondFloorLocationId, null);
+    var holdingsForOne = List.of(
+      createHolding(instanceOne.getId(), mainLibraryLocationId, null),
+      createHolding(instanceOne.getId(), secondFloorLocationId, null)
+    );
+    var itemsForOne = List.of(
+      createItem(nod(holdingsForOne.get(0))).getString("id"),
+      createItem(nod(holdingsForOne.get(1))).getString("id")
+    );
 
-    createItem(nod(holdingForOne));
-    createItem(nod(holdingForOne));
+    var instanceTwo = instancesClient.create(instance(randomUUID()));
+    var holdingsForTwo = List.of(
+      createHolding(instanceTwo.getId(), mainLibraryLocationId, null),
+      createHolding(instanceTwo.getId(), secondFloorLocationId, null),
+      createHolding(instanceTwo.getId(), fourthFloorLocationId, null));
+    var itemsForTwo = List.of(
+      createItem(nod(holdingsForTwo.get(0))).getString("id"),
+      createItem(nod(holdingsForTwo.get(0))).getString("id"),
+      createItem(nod(holdingsForTwo.get(1))).getString("id"),
+      createItem(nod(holdingsForTwo.get(2))).getString("id"));
+
+    var instances = inventoryViewClient.getMany("id==(%s or %s)",
+      instanceTwo.getId(), instanceOne.getId());
+
+    assertThat(instances.size(), is(2));
+
+    var firstInstance = getInstanceById(instances, instanceOne.getId());
+    var secondInstance = getInstanceById(instances, instanceTwo.getId());
+
+    assertThat(getHoldingIds(firstInstance), matchesInAnyOrder(holdingsForOne));
+    assertThat(getItemIds(firstInstance), matchesInAnyOrder(itemsForOne));
+
+    assertThat(getHoldingIds(secondInstance), matchesInAnyOrder(holdingsForTwo));
+    assertThat(getItemIds(secondInstance), matchesInAnyOrder(itemsForTwo));
+  }
+
+  @Test
+  public void shouldReturnInstanceEvenIfNoItems() {
+    var instanceOne = instancesClient.create(instance(randomUUID()));
+    var holdingForOne = createHolding(instanceOne.getId(), mainLibraryLocationId, null);
 
     var instanceTwo = instancesClient.create(instance(randomUUID()));
     var holdings = List.of(
       createHolding(instanceTwo.getId(), mainLibraryLocationId, null),
       createHolding(instanceTwo.getId(), secondFloorLocationId, null),
       createHolding(instanceTwo.getId(), fourthFloorLocationId, null));
-    var items = List.of(
-      createItem(nod(holdings.get(0))).getString("id"),
-      createItem(nod(holdings.get(0))).getString("id"),
-      createItem(nod(holdings.get(1))).getString("id"),
-      createItem(nod(holdings.get(2))).getString("id"));
 
-    var instances = inventoryViewClient.getMany("id==%s", instanceTwo.getId());
+    var instances = inventoryViewClient.getMany("id==(%s or %s)",
+      instanceTwo.getId(), instanceOne.getId());
 
-    assertThat(instances.size(), is(1));
+    assertThat(instances.size(), is(2));
 
-    var inventoryViewInstance = instances.get(0).getJson()
-      .mapTo(InventoryViewInstance.class);
+    var firstInstance = getInstanceById(instances, instanceOne.getId());
+    var secondInstance = getInstanceById(instances, instanceTwo.getId());
 
-    assertThat(inventoryViewInstance.getInstanceId(), is(instanceTwo.getId().toString()));
-    assertThat(inventoryViewInstance.getInstance().getId(), is(instanceTwo.getId().toString()));
 
-    var actualHoldingIds = inventoryViewInstance.getHoldingsRecords().stream()
-      .map(HoldingsRecords2::getId).map(UUID::fromString).collect(toList());
-    var actualItemIds = inventoryViewInstance.getItems().stream()
-      .map(HoldingsItem::getId).collect(toList());
+    assertThat(firstInstance.getHoldings().get(0).getId(), is(holdingForOne.toString()));
+    assertThat(getHoldingIds(secondInstance), matchesInAnyOrder(holdings));
 
-    assertThat(actualHoldingIds, containsInAnyOrder(holdings.stream().map(Matchers::is)
-      .collect(toList())));
-    assertThat(actualItemIds, containsInAnyOrder(items.stream().map(Matchers::is)
-      .collect(toList())));
+    isNonNullEmpty(firstInstance.getItems());
+    isNonNullEmpty(secondInstance.getItems());
+  }
+
+  @Test
+  public void shouldReturnInstanceEvenIfNoHoldings() {
+    var instanceOne = instancesClient.create(instance(randomUUID()));
+    var instanceTwo = instancesClient.create(instance(randomUUID()));
+
+    var instances = inventoryViewClient.getMany("id==(%s or %s)",
+      instanceTwo.getId(), instanceOne.getId());
+
+    assertThat(instances.size(), is(2));
+
+    var returnedInstances = instances.stream()
+      .map(resource -> resource.getJson().mapTo(InventoryViewInstance.class))
+      .collect(Collectors.toList());
+
+    for (InventoryViewInstance returnedInstance : returnedInstances) {
+      isNonNullEmpty(returnedInstance.getHoldings());
+      isNonNullEmpty(returnedInstance.getItems());
+
+      assertTrue(returnedInstance.getId().equals(instanceOne.getId().toString())
+        || returnedInstance.getId().equals(instanceTwo.getId().toString()));
+    }
+  }
+
+  private List<UUID> getHoldingIds(InventoryViewInstance instance) {
+    return instance.getHoldings().stream()
+      .map(Holding::getId)
+      .map(UUID::fromString)
+      .collect(toList());
+  }
+
+  private List<String> getItemIds(InventoryViewInstance instance) {
+    return instance.getItems().stream()
+      .map(HoldingsItem::getId)
+      .collect(toList());
+  }
+
+  private <T> Matcher<Iterable<? extends T>> matchesInAnyOrder(List<T> records) {
+    return containsInAnyOrder(records.stream()
+    .map(Matchers::is)
+    .collect(Collectors.toList()));
+  }
+
+  private void isNonNullEmpty(List<?> aList) {
+    assertThat(aList, notNullValue());
+    assertThat(aList.size(), is(0));
+  }
+
+  private InventoryViewInstance getInstanceById(List<IndividualResource> instances, UUID id) {
+    return instances.stream()
+      .filter(res -> res.getId().equals(id))
+      .map(r -> r.getJson().mapTo(InventoryViewInstance.class))
+      .findFirst()
+      .orElseThrow(() -> new AssertionError("No instance"));
   }
 }
