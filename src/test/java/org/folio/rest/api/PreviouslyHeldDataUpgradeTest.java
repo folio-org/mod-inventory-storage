f1814210f3c7e216e49bbe18fe5e0339aac4c8d2
Kyle Felker <felkerk@gvsu.edu>
2021-03-23 11:21:36 -0400

    test for migration script written, not working

---
+++ src/test/java/org/folio/rest/api/PreviouslyHeldDataUpgradeTest.java
@@ -0,0 +1,71 @@
+package org.folio.rest.api;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.isNull;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.support.IndividualResource;
+import org.folio.util.ResourceUtil;
+import io.vertx.core.json.JsonObject;
+
+import io.vertx.core.Vertx;
+import junitparams.JUnitParamsRunner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@RunWith(JUnitParamsRunner.class)
+public class PreviouslyHeldDataUpgradeTest extends TestBaseWithInventoryUtil {
+    Logger log = LogManager.getLogger(PreviouslyHeldDataUpgradeTest.class);
+    private static Vertx vertx = Vertx.vertx();
+    private static UUID instanceId = UUID.randomUUID();
+    private static final String SET_DEFAULT_PREVIOUSLY_HELD = ResourceUtil
+        .asString("templates/db_scripts/setPreviouslyHeldDefault.sql")
+        .replace("${myuniversity}_${mymodule}", "test_tenant_mod_inventory_storage");
+
+    @Test
+    public void canMigrateToDefaultPreviouslyHeldValue() {
+        instancesClient.create(instance(instanceId));
+
+        String query = "UPDATE test_tenant_mod_inventory_storage.instance " +
+            "SET jsonb = JSONB_SET(instance.jsonb, '{previouslyHeld}', TO_JSONB(null)) " +
+            "WHERE jsonb->>'previouslyHeld' IS NOT NULL;";
+             
+        runSql(query);
+
+        JsonObject instance = instancesClient.getById(instanceId).getJson();
+        assertThat(instance.getBoolean("previouslyHeld"), isNull());
+
+        runSql(SET_DEFAULT_PREVIOUSLY_HELD);
+
+        JsonObject instanceAfter = instancesClient.getById(instanceId).getJson();
+        assertEquals(instanceAfter.getBoolean("previouslyHeld"), false);
+    }
+
+    private void runSql(String sql) {
+        CompletableFuture<Void> future = new CompletableFuture<>();
+    
+        PostgresClient.getInstance(vertx).execute(sql, handler -> {
+          if (handler.failed()) {
+            future.completeExceptionally(handler.cause());
+            return;
+          }
+          future.complete(null);
+        });
+    
+        try {
+          future.get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+          throw new RuntimeException(e);
+        }
+    }
+}
