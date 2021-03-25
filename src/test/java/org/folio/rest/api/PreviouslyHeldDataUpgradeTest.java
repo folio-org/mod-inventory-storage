658fb50deb0ab0df86b19ff1b8df2ef667cf7660
Kyle Felker <felkerk@gvsu.edu>
2021-03-25 14:21:06 -0400

    data migration test now working

--- src/test/java/org/folio/rest/api/PreviouslyHeldDataUpgradeTest.java
+++ src/test/java/org/folio/rest/api/PreviouslyHeldDataUpgradeTest.java
@@ -14,50 +14,58 @@ import org.junit.runner.RunWith;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.util.ResourceUtil;
 import io.vertx.core.json.JsonObject;
-
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
 import io.vertx.core.Vertx;
 import junitparams.JUnitParamsRunner;
 
 @RunWith(JUnitParamsRunner.class)
 public class PreviouslyHeldDataUpgradeTest extends TestBaseWithInventoryUtil {
-    private static Vertx vertx = Vertx.vertx();
-    private static UUID instanceId = UUID.randomUUID();
-    private static final String SET_DEFAULT_PREVIOUSLY_HELD = ResourceUtil
-        .asString("templates/db_scripts/setPreviouslyHeldDefault.sql")
-        .replace("${myuniversity}_${mymodule}", "test_tenant_mod_inventory_storage");
+  private static Vertx vertx = Vertx.vertx();
+  private static UUID instanceId = UUID.randomUUID();
+  private static final String SET_DEFAULT_PREVIOUSLY_HELD = ResourceUtil
+    .asString("templates/db_scripts/setPreviouslyHeldDefault.sql")
+    .replace("${myuniversity}_${mymodule}", "test_tenant_mod_inventory_storage");
 
-    @Test
-    public void canMigrateToDefaultPreviouslyHeldValue() {
-        instancesClient.create(instance(instanceId));
+  @Test
+  public void canMigrateToDefaultPreviouslyHeldValue() throws 
+    InterruptedException, ExecutionException, TimeoutException{
+    instancesClient.create(instance(instanceId));
 
-        String query = "UPDATE test_tenant_mod_inventory_storage.instance SET jsonb = jsonb - 'previouslyHeld';";
+    //Becasue calling the API will result in the previouslyHeld value being automatically
+    //set to false, it is neccessary to test the migration script by pulling data directly from
+    //the back-end database. 
+
+    String query = "UPDATE test_tenant_mod_inventory_storage.instance SET jsonb = jsonb - 'previouslyHeld';";
              
-        runSql(query);
-
-        JsonObject instance = instancesClient.getById(instanceId).getJson();
-        assertNull(instance.getBoolean("previouslyHeld"));
-
-        runSql(SET_DEFAULT_PREVIOUSLY_HELD);
-
-        JsonObject instanceAfter = instancesClient.getById(instanceId).getJson();
-        assertEquals(instanceAfter.getBoolean("previouslyHeld"), false);
-    }
-
-    private void runSql(String sql) {
-        CompletableFuture<Void> future = new CompletableFuture<>();
+    runSql(query);
     
-        PostgresClient.getInstance(vertx).execute(sql, handler -> {
-          if (handler.failed()) {
-            future.completeExceptionally(handler.cause());
-            return;
-          }
-          future.complete(null);
-        });
+    String migrationQuery = "SELECT jsonb FROM test_tenant_mod_inventory_storage.instance WHERE id = '" + instanceId.toString() + "';";
     
-        try {
-          future.get(1, TimeUnit.SECONDS);
-        } catch (InterruptedException | ExecutionException | TimeoutException e) {
-          throw new RuntimeException(e);
-        }
-    }
+    RowSet<Row> result = runSql(migrationQuery);
+    assertEquals(result.rowCount(), 1);
+    JsonObject entry = result.iterator().next().toJson();
+    assertNull(entry.getJsonObject("jsonb").getBoolean("previouslyHeld"));
+
+    runSql(SET_DEFAULT_PREVIOUSLY_HELD);
+
+    RowSet<Row> migrationResult = runSql(migrationQuery);
+    assertEquals(migrationResult.rowCount(), 1);
+    JsonObject migrationEntry = migrationResult.iterator().next().toJson();
+    assertEquals(false, migrationEntry.getJsonObject("jsonb").getBoolean("previouslyHeld")); 
+  }
+
+  private RowSet<Row> runSql(String sql) throws 
+    InterruptedException, ExecutionException, TimeoutException {
+    CompletableFuture<RowSet<Row>> future = new CompletableFuture<>();
+    
+    PostgresClient.getInstance(vertx).execute(sql, handler -> {
+      if (handler.failed()) {
+        future.completeExceptionally(handler.cause());
+      }
+      future.complete(handler.result());
+    });
+  
+    return future.get(5, TimeUnit.SECONDS);
+  }
 }
