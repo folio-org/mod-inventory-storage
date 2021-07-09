2814a2e31c6063de0812771c74db5ba813db6a5d
Marc Johnson <marc@k-int.co.uk>
2021-07-09 11:09:28 +0100

    Include environment in topic names when publishing messages MODINVSTOR-738

--- src/main/java/org/folio/services/kafka/topic/KafkaTopic.java
+++ src/main/java/org/folio/services/kafka/topic/KafkaTopic.java
@@ -2,15 +2,15 @@ package org.folio.services.kafka.topic;
 
 public class KafkaTopic {
   public static KafkaTopic instance() {
-    return new KafkaTopic("inventory.instance");
+    return new KafkaTopic(prefixWithEnvironment("inventory.instance"));
   }
 
   public static KafkaTopic holdingsRecord() {
-    return new KafkaTopic("inventory.holdings-record");
+    return new KafkaTopic(prefixWithEnvironment("inventory.holdings-record"));
   }
 
   public static KafkaTopic item() {
-    return new KafkaTopic("inventory.item");
+    return new KafkaTopic(prefixWithEnvironment("inventory.item"));
   }
 
   private final String topicName;
@@ -21,5 +21,11 @@ public class KafkaTopic {
 
   public String getTopicName() {
     return topicName;
+  }
+
+  private static String prefixWithEnvironment(String topicName) {
+    final var environmentName = System.getenv().getOrDefault("ENV", "folio");
+
+    return environmentName + "." + topicName;
   }
 }
