1e26f66e3df0383aeefc1e15aaf07bc930d951ca
Kyle Felker <felkerk@gvsu.edu>
2021-08-31 16:10:47 -0400

    endpoint at least partially working

--- src/main/java/org/folio/rest/impl/ItemStorageDereferencedAPI.java
+++ src/main/java/org/folio/rest/impl/ItemStorageDereferencedAPI.java
@@ -8,25 +8,55 @@ import java.util.Map;
 import javax.ws.rs.core.Response;
 
 import org.folio.rest.annotations.Validate;
+import org.folio.rest.impl.InstanceStorageAPI.PreparedCQL;
 import org.folio.rest.jaxrs.model.Item;
 import org.folio.rest.jaxrs.resource.ItemStorage;
 import org.folio.rest.jaxrs.resource.ItemStorageDereferenced;
+import org.folio.rest.jaxrs.model.Item;
+import org.folio.rest.jaxrs.model.Items;
 import org.folio.rest.persist.PgUtil;
-import org.folio.services.item.ItemService;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.persist.cql.CQLWrapper;
+import org.folio.cql2pgjson.CQL2PgJSON;
+import org.folio.cql2pgjson.exception.CQLFeatureUnsupportedException;
+import org.folio.cql2pgjson.exception.FieldException;
+import org.folio.cql2pgjson.exception.QueryValidationException;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
-import io.vertx.ext.web.RoutingContext;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 
 /**
  * CRUD for Dereferenced Items.
  * This is an experimental endpoint.
  * Aim is to determine if dereferencing item records
- * results in performance improvement.
+ * results in performance improvement when performing
+ * checkout/checkin operations.
  */
 public class ItemStorageDereferencedAPI implements ItemStorageDereferenced {
+  private static final Logger logger = LogManager.getLogger("okapi");
   public static final String ITEM_TABLE = "item";
+  private static final String JSON_COLUMN = "jsonb";
+  private String sqlQuery= "SELECT item.jsonb as itemrecord, holdingstable.jsonb as holdingsrecord,\n" + 
+  "instancetable.jsonb as instancerecord, materialtypetable.jsonb as materialtyperecord,\n" + 
+  "locationtable.jsonb as permlocation, locationtabletemp.jsonb as templocation, locationtableeffective.jsonb as effectivelocation,\n"+
+  "loantable.jsonb as permloantype, loantabletemp.jsonb as temploantype\n"+
+  "FROM item\n"+
+  "INNER JOIN holdings_record as holdingstable on item.holdingsrecordid=holdingstable.id\n"+
+  "INNER JOIN instance as instancetable on holdingstable.instanceid=instancetable.id\n"+
+  "INNER JOIN material_type as materialtypetable on item.materialtypeid=materialtypetable.id\n"+
+  "LEFT JOIN location as locationtable on item.permanentlocationid = locationtable.id\n"+
+  "LEFT JOIN location as locationtabletemp on item.temporarylocationid = locationtabletemp.id\n"+
+  "LEFT JOIN location as locationtableeffective on item.effectivelocationid = locationtableeffective.id\n"+
+  "INNER JOIN loan_type as loantable on item.permanentloantypeid=loantable.id\n"+
+  "LEFT JOIN loan_type as loantabletemp on item.temporaryloantypeid=loantabletemp.id\n";
 
   @Validate
   @Override
@@ -36,8 +66,35 @@ public class ItemStorageDereferencedAPI implements ItemStorageDereferenced {
     Handler<AsyncResult<Response>> asyncResultHandler,
     Context vertxContext) {
 
-    PgUtil.streamGet(ITEM_TABLE, Item.class, query, offset, limit, null, "items",
-      routingContext, okapiHeaders, vertxContext);
+    String whereClause = "";
+    logger.info("query is: " + query);
+    if (query != null) {
+      try {
+        CQLWrapper wrapper = new CQLWrapper(new CQL2PgJSON(ITEM_TABLE + "." + JSON_COLUMN), query, limit, offset);
+        logger.info("translated SQL query: " + wrapper.toString());
+        whereClause = wrapper.toString();
+      } catch(Exception e) {
+        GetItemStorageDereferencedItemsResponse.respond400WithTextPlain("Invalid CQL query: " + e.getMessage());
+      }
+    } else {
+      whereClause = "LIMIT " + limit + " OFFSET " + offset;
+    }
+
+    PostgresClient postgresClient = PgUtil.postgresClient(vertxContext, okapiHeaders);
+    postgresClient.select(sqlQuery + whereClause, asyncResult -> {
+      if (asyncResult.failed()) {
+        String failureMessage = asyncResult.cause().getMessage();
+        GetItemStorageDereferencedItemsResponse.respond500WithTextPlain("Unable to retrieve data from database: " + failureMessage);
+      }
+      RowSet<Row> result = asyncResult.result();
+      if (result.size() == 0) {
+        GetItemStorageDereferencedItemsResponse.respond404WithTextPlain("No records found matching search criteria.");
+      }
+
+      logger.info("result is " + result.size() + " entries.");
+      logger.info("Retrived rows: ");
+      result.forEach(row -> logger.info(row.deepToString()));
+    });
   }
 
   @Validate
@@ -47,7 +104,7 @@ public class ItemStorageDereferencedAPI implements ItemStorageDereferenced {
       io.vertx.core.Handler<io.vertx.core.AsyncResult<Response>> asyncResultHandler,
       Context vertxContext) {
 
-    PgUtil.getById(ITEM_TABLE, Item.class, itemId, okapiHeaders, vertxContext,
-        GetItemStorageItemsByItemIdResponse.class, asyncResultHandler);
   }
+
+
 }
