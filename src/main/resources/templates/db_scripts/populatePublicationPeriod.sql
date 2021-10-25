4b8b3ddbc46d4e30fdb688249a570c86c2ae6976
Oleksii Kuzminov <40833111+OleksiiKuzminov@users.noreply.github.com>
2021-10-25 14:47:10 +0300

    MODINVSTOR-812 Improve ways to populate publicationPeriod field (#686)
    (cherry picked from commit 3fbf8d2f5976efe1b8eeffae82cafc92d9c5550d)

--- src/main/resources/templates/db_scripts/populatePublicationPeriod.sql
+++ src/main/resources/templates/db_scripts/populatePublicationPeriod.sql
@@ -8,6 +8,10 @@ SET search_path TO ${myuniversity}_${mymodule};
 
 START TRANSACTION;
 
+CREATE INDEX IF NOT EXISTS instance_publication_period on ${myuniversity}_${mymodule}.instance ((jsonb -> 'publicationPeriod'));
+
+ALTER TABLE instance DISABLE TRIGGER USER;
+
 CREATE OR REPLACE FUNCTION parse_start_year(jsonb) RETURNS int AS $$
   SELECT COALESCE(
     (regexp_match($1->'publication'->0->>'dateOfPublication', '(\d{4})\w{0,2}(?:\s?-|\sand|\s?,)'))[1],
@@ -40,21 +44,19 @@ UPDATE instance
 SET jsonb = jsonb_set(jsonb, '{publicationPeriod}', parse_publication_period(jsonb))
 WHERE jsonb->>'publicationPeriod' IS NULL AND parse_publication_period(jsonb) IS NOT NULL;
 
--- You may disable triggers if you are sure that no other process updates instance records:
---
--- ALTER TABLE instance DISABLE TRIGGER USER;
---
 -- UPDATE instance
 -- SET jsonb = jsonb || jsonb_build_object(
 --   'publicationPeriod', parse_publication_period(jsonb),
 --   '_version', to_jsonb(coalesce((jsonb->>'_version')::numeric + 1, 1)))
 -- WHERE jsonb->>'publicationPeriod' IS NULL AND parse_publication_period(jsonb) IS NOT NULL;
---
--- ALTER TABLE instance ENABLE TRIGGER USER;
+
+ALTER TABLE instance ENABLE TRIGGER USER;
 
 DROP FUNCTION
   parse_start_year(jsonb),
   parse_end_year(jsonb),
   parse_publication_period(jsonb);
+
+DROP INDEX IF EXISTS instance_publication_period;
 
 END TRANSACTION;
