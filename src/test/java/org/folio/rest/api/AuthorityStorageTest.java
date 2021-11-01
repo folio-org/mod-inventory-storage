0ae264886b9a9bbc0e7a94f51f6a99f31ad0a7e5
oleksii_kuzminov <oleksii_kuzminov@epam.com>
2021-11-01 15:17:32 +0200

    MODINVSTOR-787 Create CRUD REST API for Authorities tests fixed

--- src/test/java/org/folio/rest/api/AuthorityStorageTest.java
+++ src/test/java/org/folio/rest/api/AuthorityStorageTest.java
@@ -3,8 +3,6 @@ package org.folio.rest.api;
 import io.vertx.core.json.JsonObject;
 import junitparams.JUnitParamsRunner;
 import lombok.SneakyThrows;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.folio.rest.jaxrs.model.Authority;
 import org.folio.rest.support.Response;
 import org.folio.rest.support.ResponseHandler;
