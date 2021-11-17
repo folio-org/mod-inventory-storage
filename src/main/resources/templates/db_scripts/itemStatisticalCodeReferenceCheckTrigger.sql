7f1533141161d8640190e42a74074e1abbf1375d
Julian Ladisch <julianladisch@users.noreply.github.com>
2021-11-17 12:07:41 +0100

    Use DROP TRIGGER to make it idempotent
    This is a workaround because PostgreSQL before version 14 doesn't support
    CREATE OR REPLACE TRIGGER.

--- src/main/resources/templates/db_scripts/itemStatisticalCodeReferenceCheckTrigger.sql
+++ src/main/resources/templates/db_scripts/itemStatisticalCodeReferenceCheckTrigger.sql
@@ -6,11 +6,11 @@ DECLARE
   invalid text;
 BEGIN
   SELECT ref
-  INTO invalid
-  FROM jsonb_array_elements_text(NEW.jsonb->'statisticalCodeIds') ref
-  LEFT JOIN statistical_code ON id=ref::uuid
-  WHERE id IS NULL
-  LIMIT 1;
+    INTO invalid
+    FROM jsonb_array_elements_text(NEW.jsonb->'statisticalCodeIds') ref
+    LEFT JOIN statistical_code ON id=ref::uuid
+    WHERE id IS NULL
+    LIMIT 1;
   IF FOUND THEN
     RAISE foreign_key_violation USING
       MESSAGE='statistical code doesn''t exist: ' || invalid,
@@ -22,12 +22,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+DROP TRIGGER IF EXISTS check_statistical_code_references_on_insert ON item CASCADE;
 CREATE TRIGGER check_statistical_code_references_on_insert
   BEFORE INSERT ON item
   FOR EACH ROW
   WHEN (NEW.jsonb->'statisticalCodeIds' IS NOT NULL AND NEW.jsonb->'statisticalCodeIds' <> '[]')
   EXECUTE FUNCTION check_statistical_code_references();
 
+DROP TRIGGER IF EXISTS check_statistical_code_references_on_update ON item CASCADE;
 CREATE TRIGGER check_statistical_code_references_on_update
   BEFORE UPDATE ON item
   FOR EACH ROW
