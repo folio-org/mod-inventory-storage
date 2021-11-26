62c7c7a06eac3e3b3d10e7f085e86ae917869dc8
Adam Dickmeiss <adam@indexdata.dk>
2021-11-26 15:40:54 +0100

    No need for Future

--- src/test/java/org/folio/rest/api/KafkaTopicsExistsTest.java
+++ src/test/java/org/folio/rest/api/KafkaTopicsExistsTest.java
@@ -1,6 +1,5 @@
 package org.folio.rest.api;
 
-import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -39,24 +38,22 @@ public class KafkaTopicsExistsTest extends TestBase {
 
   @Test
   public void topics(TestContext context) {
-    Future<Void> future = kafkaAdminClient.createTopics(
-      List.of(
-        new NewTopic("T1", 1, REPLICATION_FACTOR),
-        new NewTopic("T2", 1, REPLICATION_FACTOR)
-      ));
-    future = future.compose(x -> kafkaAdminClient.createTopics(
-      List.of(
-        new NewTopic("T1", 1, REPLICATION_FACTOR),
-        new NewTopic("T3", 1, REPLICATION_FACTOR)
-      ))
-    ).otherwise(cause -> {
-      if (cause instanceof org.apache.kafka.common.errors.TopicExistsException) {
-        return null;
-      }
-      throw new RuntimeException(cause);
-    });
-    // test that T3 got inserted, while T1 already being present
-    future.compose(x -> kafkaAdminClient.listTopics())
+    kafkaAdminClient.createTopics(
+        List.of(
+          new NewTopic("T1", 1, REPLICATION_FACTOR),
+          new NewTopic("T2", 1, REPLICATION_FACTOR)
+        ))
+      .compose(x -> kafkaAdminClient.createTopics(
+        List.of(
+          new NewTopic("T1", 1, REPLICATION_FACTOR),
+          new NewTopic("T3", 1, REPLICATION_FACTOR)
+        ))
+      ).otherwise(cause -> {
+        if (cause instanceof org.apache.kafka.common.errors.TopicExistsException) {
+          return null;
+        }
+        throw new RuntimeException(cause);
+      }).compose(x -> kafkaAdminClient.listTopics())
       .onComplete(context.asyncAssertSuccess(topics ->
         context.assertTrue(topics.containsAll(List.of("T1", "T2", "T3")))
       )).mapEmpty();
