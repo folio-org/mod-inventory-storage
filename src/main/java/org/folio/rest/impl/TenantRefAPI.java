a139f6776c82b47dd9830ec7ca8802421e8307b9
felkerk <felkerk@gvsu.edu>
2022-04-25 15:09:51 -0400

    adding related reference types to list

--- src/main/java/org/folio/rest/impl/TenantRefAPI.java
+++ src/main/java/org/folio/rest/impl/TenantRefAPI.java
@@ -72,7 +72,8 @@ public class TenantRefAPI extends TenantAPI {
     "bound-with/instances",
     "bound-with/holdingsrecords",
     "bound-with/items",
-    "bound-with/bound-with-parts"
+    "bound-with/bound-with-parts",
+    "related-instance-types"
   };
 
   List<JsonObject> servicePoints = null;
