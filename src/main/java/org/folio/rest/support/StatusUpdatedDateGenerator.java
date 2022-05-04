6bbc265e51509ccc8bef3ecba7350abce7028946
Olamide Kolawole <okolawole@ebsco.com>
2022-05-04 18:02:35 -0500

    resolve potential datetime issues

--- src/main/java/org/folio/rest/support/StatusUpdatedDateGenerator.java
+++ src/main/java/org/folio/rest/support/StatusUpdatedDateGenerator.java
@@ -1,11 +1,12 @@
 package org.folio.rest.support;
 
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public final class StatusUpdatedDateGenerator {
     private final static DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
     public static String generateStatusUpdatedDate() {
-      return df.format(ZonedDateTime.now());
+      return df.format(ZonedDateTime.now(ZoneOffset.UTC));
     }
 }
