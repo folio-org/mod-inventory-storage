3f02ec90233aa5f1a7dfc52d91aba4f3c4f7c477
Shans Kaluhin <shans_kaluhin@epam.com>
2022-07-11 04:53:00 +0300

    check if call number match shelfKey

--- src/main/java/org/folio/services/CallNumberUtils.java
+++ src/main/java/org/folio/services/CallNumberUtils.java
@@ -1,6 +1,7 @@
 package org.folio.services;
 
 import java.util.Optional;
+import java.util.regex.Pattern;
 import org.marc4j.callnum.CallNumber;
 import org.marc4j.callnum.DeweyCallNumber;
 import org.marc4j.callnum.LCCallNumber;
@@ -9,7 +10,13 @@ public class CallNumberUtils {
 
   private CallNumberUtils() {}
 
+  private static final Pattern SHELF_KEY_PATTERN =
+    Pattern.compile("([A-Z]+)\\s(\\d{2,}(\\.\\d+)?)(\\s[A-Z]\\d{1,10}){0,2}(\\s.{0,10}){0,10}");
+
   public static Optional<String> getShelfKeyFromCallNumber(String callNumber) {
+    if (SHELF_KEY_PATTERN.matcher(callNumber).matches()) {
+      return Optional.of(callNumber).map(String::trim);
+    }
     return Optional.ofNullable(callNumber)
       .flatMap(cn -> getValidShelfKey(new LCCallNumber(cn))
         .or(() -> getValidShelfKey(new DeweyCallNumber(cn))))
