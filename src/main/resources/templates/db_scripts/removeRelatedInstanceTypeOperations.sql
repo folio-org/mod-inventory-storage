c708b9a92ac2c53cdb98a1bd457f02eadbeda498
Huff <jeremythuff@gmail.com>
2022-08-11 10:17:02 -0500

    Added new remove operations script

---
+++ src/main/resources/templates/db_scripts/removeRelatedInstanceTypeOperations.sql
@@ -0,0 +1,2 @@
+DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.set_related_instance_type_md_json CASCADE;
+DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.related_instance_type_set_md CASCADE;
