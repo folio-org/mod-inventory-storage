8c5fac1e0272dd7117a729afd7fe61d1cccfc00f
Rincy Mathew <rmathew@tamu.edu>
2022-08-11 15:44:34 -0500

    Removed removeRelatedInstanceTypeOperations sql file and updated schema json fle

--- src/main/resources/templates/db_scripts/removeRelatedInstanceTypeOperations.sql
+++
@@ -1,2 +0,0 @@
-DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.set_related_instance_type_md_json CASCADE;
-DROP FUNCTION IF EXISTS ${myuniversity}_${mymodule}.related_instance_type_set_md CASCADE;
