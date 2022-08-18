5b6750553260bc94b0f1c406588a7b041399eef6
Oleksandr Oliinyk <oleksandr_oliinyk1@epam.com>
2022-08-18 17:20:56 +0300

    Remove restrictions from crud methods as they are used by TenantLoading service

--- src/main/java/org/folio/rest/impl/AuthoritySourceFileAPI.java
+++ src/main/java/org/folio/rest/impl/AuthoritySourceFileAPI.java
@@ -2,10 +2,8 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
-import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import java.util.Map;
-import java.util.Set;
 import javax.ws.rs.core.Response;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.AuthoritySourceFile;
@@ -14,25 +12,6 @@ import org.folio.rest.persist.PgUtil;
 
 public class AuthoritySourceFileAPI implements org.folio.rest.jaxrs.resource.AuthoritySourceFiles {
   public static final String REFERENCE_TABLE = "authority_source_file";
-
-  public static final Set<String> PRE_DEFINED_SOURCE_FILE_IDS =
-    Set.of(
-      "cb58492d-018e-442d-9ce3-35aabfc524aa",
-      "191874a0-707a-4634-928e-374ee9103225",
-      "b224845c-5026-4594-8b55-61d39ecf0541",
-      "ccebe5d8-5bfe-46f5-bfa2-79f257c249c9",
-      "4b531a84-d4fe-44e5-b75f-542ec71b2f62",
-      "67d1ec4b-a19a-4324-9f19-473b49e370ac",
-      "2c0e41b5-8ffb-4856-aa64-76648a6f6b18",
-      "af045f2f-e851-4613-984c-4bc13430454a",
-      "837e2c7b-037b-4113-9dfd-b1b8aeeb1fb8",
-      "6ddf21a6-bc2f-4cb0-ad96-473e1f82da23",
-      "b0f38dbe-5bc0-477d-b1ee-6d9878a607f7",
-      "70ff583b-b8c9-483e-ac21-cb4a9217898b"
-    );
-
-  public static final String UPDATE_PRE_DEFINED_SOURCE_FILE_ERROR = "Modification of pre-defined authority source file is not allowed";
-  public static final String DELETE_PRE_DEFINED_SOURCE_FILE_ERROR = "Deletion of pre-defined authority source file is not allowed";
 
   @Override
   @Validate
@@ -74,15 +53,9 @@ public class AuthoritySourceFileAPI implements org.folio.rest.jaxrs.resource.Aut
                                              Map<String, String> okapiHeaders,
                                              Handler<AsyncResult<Response>> asyncResultHandler,
                                              Context vertxContext) {
-    if (!PRE_DEFINED_SOURCE_FILE_IDS.contains(id)) {
-      PgUtil.deleteById(REFERENCE_TABLE, id, okapiHeaders, vertxContext,
-        DeleteAuthoritySourceFilesByIdResponse.class,
-        asyncResultHandler);
-    } else {
-      asyncResultHandler.handle(Future.succeededFuture(
-        DeleteAuthoritySourceFilesByIdResponse.
-          respond400WithTextPlain(DELETE_PRE_DEFINED_SOURCE_FILE_ERROR)));
-    }
+    PgUtil.deleteById(REFERENCE_TABLE, id, okapiHeaders, vertxContext,
+      DeleteAuthoritySourceFilesByIdResponse.class,
+      asyncResultHandler);
   }
 
   @Override
@@ -92,14 +65,8 @@ public class AuthoritySourceFileAPI implements org.folio.rest.jaxrs.resource.Aut
                                           Map<String, String> okapiHeaders,
                                           Handler<AsyncResult<Response>> asyncResultHandler,
                                           Context vertxContext) {
-    if (!PRE_DEFINED_SOURCE_FILE_IDS.contains(id)) {
-      PgUtil.put(REFERENCE_TABLE, entity, id, okapiHeaders, vertxContext,
-        PutAuthoritySourceFilesByIdResponse.class,
-        asyncResultHandler);
-    } else {
-      asyncResultHandler.handle(Future.succeededFuture(
-        PutAuthoritySourceFilesByIdResponse
-          .respond400WithTextPlain(UPDATE_PRE_DEFINED_SOURCE_FILE_ERROR)));
-    }
+    PgUtil.put(REFERENCE_TABLE, entity, id, okapiHeaders, vertxContext,
+      PutAuthoritySourceFilesByIdResponse.class,
+      asyncResultHandler);
   }
 }
