27719507c2b76db3ca73cd9399ca9b0bd86b23f7
Oleksandr Oliinyk <oleksandr_oliinyk1@epam.com>
2022-08-18 13:53:33 +0300

    Refactoring codes type

--- src/test/java/org/folio/rest/api/AuthoritySourceFileAPITest.java
+++ src/test/java/org/folio/rest/api/AuthoritySourceFileAPITest.java
@@ -1,16 +1,13 @@
 package org.folio.rest.api;
 
 import static org.folio.HttpStatus.HTTP_BAD_REQUEST;
-import static org.folio.HttpStatus.HTTP_NOT_FOUND;
-import static org.folio.rest.api.entities.AuthoritySourceFile.CODE_KEY;
-import static org.folio.rest.api.entities.AuthoritySourceFile.NAME_KEY;
-import static org.folio.rest.api.entities.AuthoritySourceFile.TYPE_KEY;
 import static org.folio.rest.impl.AuthoritySourceFileAPI.DELETE_PRE_DEFINED_SOURCE_FILE_ERROR;
 import static org.folio.rest.impl.AuthoritySourceFileAPI.PRE_DEFINED_SOURCE_FILE_IDS;
 import static org.folio.rest.impl.AuthoritySourceFileAPI.UPDATE_PRE_DEFINED_SOURCE_FILE_ERROR;
 import static org.junit.Assert.assertEquals;
 
 import io.vertx.core.json.JsonObject;
+import java.util.List;
 import java.util.UUID;
 import junitparams.JUnitParamsRunner;
 import org.folio.rest.support.http.ResourceClient;
@@ -21,31 +18,6 @@ import org.junit.runner.RunWith;
 public class AuthoritySourceFileAPITest extends TestBase {
 
   private static ResourceClient authoritySourceFileClient = ResourceClient.forAuthoritySourceFiles(client);
-
-  @Test
-  public void shouldCreateUpdateAndDeleteSourceFile() {
-    var id = UUID.randomUUID();
-    var newSourceFile = new JsonObject().put("id", id)
-      .put(CODE_KEY, "tst")
-      .put(TYPE_KEY, "Subjects")
-      .put(NAME_KEY, "new source file");
-
-    var storedSourceFile = authoritySourceFileClient.create(newSourceFile).getJson();
-
-    storedSourceFile.put(NAME_KEY, "updated source file");
-
-    authoritySourceFileClient.replace(id, storedSourceFile);
-
-    var updatedSourceFile = authoritySourceFileClient.getById(id).getJson();
-    assertEquals("tst", updatedSourceFile.getString(CODE_KEY));
-    assertEquals("Subjects", updatedSourceFile.getString(TYPE_KEY));
-    assertEquals("updated source file", updatedSourceFile.getString(NAME_KEY));
-
-    authoritySourceFileClient.delete(id);
-
-    var response = authoritySourceFileClient.getById(id);
-    assertEquals(HTTP_NOT_FOUND.toInt(), response.getStatusCode());
-  }
 
   @Test
   public void shouldNotDeletePreDefinedSourceFile() {
@@ -61,7 +33,7 @@ public class AuthoritySourceFileAPITest extends TestBase {
   public void shouldNotUpdatePreDefinedSourceFile() {
     var id = PRE_DEFINED_SOURCE_FILE_IDS.stream().findAny().orElseThrow();
     var entity = new JsonObject().put("id", id)
-      .put("code", "tst")
+      .put("codes", List.of("tst"))
       .put("type", "Subjects")
       .put("name", "test");
 
--- src/test/java/org/folio/rest/api/entities/AuthoritySourceFile.java
+++ src/test/java/org/folio/rest/api/entities/AuthoritySourceFile.java
@@ -1,13 +1,15 @@
 package org.folio.rest.api.entities;
 
+import java.util.List;
+
 public class AuthoritySourceFile extends JsonEntity {
   public static final String NAME_KEY = "name";
-  public static final String CODE_KEY = "code";
+  public static final String CODES_KEY = "codes";
   public static final String TYPE_KEY = "type";
 
-  public AuthoritySourceFile(String name, String code, String type) {
+  public AuthoritySourceFile(String name, List<String> codes, String type) {
     super.setProperty(NAME_KEY, name);
-    super.setProperty(CODE_KEY, code);
+    super.setProperty(CODES_KEY, codes);
     super.setProperty(TYPE_KEY, type);
   }
 
