762b891ead47f8f9c3f451ee4401fc87f8dc7713
Shans Kaluhin <shans_kaluhin@epam.com>
2022-10-25 14:20:30 +0300

    use default methods

--- src/main/java/org/folio/InventoryKafkaTopic.java
+++ src/main/java/org/folio/InventoryKafkaTopic.java
@@ -3,20 +3,18 @@ package org.folio;
 import org.folio.kafka.services.KafkaTopic;
 
 public enum InventoryKafkaTopic implements KafkaTopic {
-  INSTANCE("instance", 50),
-  ITEM("item", 50),
-  HOLDINGS_RECORD("holdings-record", 50),
-  AUTHORITY("authority", 50),
-  INSTANCE_CONTRIBUTION("instance-contribution", 50),
-  BOUND_WITH("bound-with", 50),
-  ASYNC_MIGRATION("async-migration", 50);
+  INSTANCE("instance"),
+  ITEM("item"),
+  HOLDINGS_RECORD("holdings-record"),
+  AUTHORITY("authority"),
+  INSTANCE_CONTRIBUTION("instance-contribution"),
+  BOUND_WITH("bound-with"),
+  ASYNC_MIGRATION("async-migration");
 
   private final String topic;
-  private final int partitions;
 
-  InventoryKafkaTopic(String topic, int partitions) {
+  InventoryKafkaTopic(String topic) {
     this.topic = topic;
-    this.partitions = partitions;
   }
 
   @Override
@@ -27,15 +25,5 @@ public enum InventoryKafkaTopic implements KafkaTopic {
   @Override
   public String topicName() {
     return topic;
-  }
-
-  @Override
-  public int numPartitions() {
-    return partitions;
-  }
-
-  @Override
-  public short replicationFactor() {
-    return 0;
   }
 }
