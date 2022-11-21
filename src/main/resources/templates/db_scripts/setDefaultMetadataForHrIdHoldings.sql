f6401a9f4b6ca65e23408183a5cee5a60f031724
Roman Chernetskyi <Roman_Chernetskyi@epam.com>
2022-11-21 12:08:59 +0200

    [MODINVSTOR-956] Added created by field

--- src/main/resources/templates/db_scripts/setDefaultMetadataForHrIdHoldings.sql
+++ src/main/resources/templates/db_scripts/setDefaultMetadataForHrIdHoldings.sql
@@ -1,6 +1,22 @@
+ALTER TABLE ${myuniversity}_${mymodule}.hrid_settings
+  ADD COLUMN IF NOT EXISTS
+    creation_date timestamp,
+  ADD COLUMN IF NOT EXISTS
+    created_by text;
+
+UPDATE ${myuniversity}_${mymodule}.hrid_settings
+	SET creation_date = NOW()
+WHERE creation_date IS NULL;
+
+UPDATE ${myuniversity}_${mymodule}.hrid_settings
+	SET created_by = '00000000-0000-0000-0000-000000000000'
+WHERE created_by IS NULL;
+
 UPDATE ${myuniversity}_${mymodule}.hrid_settings
 	SET jsonb = jsonb_set(jsonb, '{metadata}', jsonb_build_object(
   'createdDate', to_jsonb(to_char(NOW(), 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"')),
-  'updatedDate', to_jsonb(to_char(NOW(), 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"'))
+  'updatedDate', to_jsonb(to_char(NOW(), 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"')),
+  'createdByUserId', '00000000-0000-0000-0000-000000000000',
+  'updatedByUserId', '00000000-0000-0000-0000-000000000000'
   ))
 WHERE jsonb -> 'metadata' IS NULL;
