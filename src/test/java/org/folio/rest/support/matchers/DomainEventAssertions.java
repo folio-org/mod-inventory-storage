cf14654a6c30dc93b32233d776cbd13e7fc71660
Julian Ladisch <julianladisch@users.noreply.github.com>
2022-11-24 16:34:07 +0100

    MODINVSTOR-998: Fix Kafka test failure in HoldingsStorageTest:348
    When upgrading to Vert.x 4.3.5 and RMB 35.0.4 we get build failures:
    
    30% of all GitHub Actions builds fail with this message:
    
    ```
      HoldingsStorageTest.canReplaceAHoldingAtSpecificLocation:348
        Expected: is "UPDATE"
             but: was "CREATE"
    ```
    
    Fix this by matching the expected Kafka message at any position in the Kafka message stream, not only at the last position.

--- src/test/java/org/folio/rest/support/matchers/DomainEventAssertions.java
+++ src/test/java/org/folio/rest/support/matchers/DomainEventAssertions.java
@@ -29,7 +29,6 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 
@@ -113,6 +112,23 @@ public final class DomainEventAssertions {
     assertThat(updateEvent.value().getJsonObject("new"), is(newRecord));
 
     assertHeaders(updateEvent.headers());
+  }
+
+  public static boolean hasUpdateEvent(Collection<KafkaConsumerRecord<String, JsonObject>> events,
+      JsonObject oldRecord, JsonObject newRecord) {
+
+    if (events == null) {
+      return false;
+    }
+    for (var event : events) {
+      try {
+        assertUpdateEvent(event, oldRecord, newRecord);
+        return true;
+      } catch (AssertionError e) {
+        // ignore
+      }
+    }
+    return false;
   }
 
   private static void assertHeaders(List<KafkaHeader> headers) {
@@ -311,14 +327,9 @@ public final class DomainEventAssertions {
 
   public static void assertUpdateEventForHolding(JsonObject oldHr, JsonObject newHr) {
     final String id = newHr.getString("id");
-    final String oldInstanceId = oldHr.getString("instanceId");
     final String newInstanceId = newHr.getString("instanceId");
-    final int holdingsEventsCountGreaterThan = oldInstanceId.equals(newInstanceId) ? 2 : 1;
 
-    await()
-      .until(() -> getHoldingsEvents(newInstanceId, id).size(), greaterThanOrEqualTo(holdingsEventsCountGreaterThan));
-
-    assertUpdateEvent(getLastHoldingEvent(newInstanceId, id), oldHr, newHr);
+    await().until(() -> hasUpdateEvent(getHoldingsEvents(newInstanceId, id), oldHr, newHr));
   }
 
   private static String getInstanceIdForItem(JsonObject newItem) {
