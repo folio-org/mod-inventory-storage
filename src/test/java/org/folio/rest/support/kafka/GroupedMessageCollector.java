89c4cb652ca5491c112c3619346125c3cf43c270
Marc Johnson <marc@k-int.co.uk>
2023-01-04 11:53:32 +0000

    Extract class for collected messages MODINVSTOR-1001

--- src/test/java/org/folio/rest/support/kafka/GroupedMessageCollector.java
+++ src/test/java/org/folio/rest/support/kafka/GroupedMessageCollector.java
@@ -1,11 +1,6 @@
 package org.folio.rest.support.kafka;
 
-import static java.util.Collections.emptyList;
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.function.Function;
 
 import org.folio.rest.support.messages.EventMessage;
@@ -15,31 +10,30 @@ import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 
 class GroupedMessageCollector {
   private final Function<KafkaConsumerRecord<String, JsonObject>, String> groupKeyMap;
-  private final Map<String, List<EventMessage>> collectedMessages;
+  private final GroupedCollectedMessages groupedCollectedMessages = new GroupedCollectedMessages();
 
   GroupedMessageCollector(
     Function<KafkaConsumerRecord<String, JsonObject>, String> groupKeyMap) {
 
     this.groupKeyMap = groupKeyMap;
-    collectedMessages = new HashMap<>();
   }
 
   void acceptMessage(KafkaConsumerRecord<String, JsonObject> message) {
-    final var keyBucket = collectedMessages.computeIfAbsent(
-      groupKeyMap.apply(message), v -> new ArrayList<>());
+    final var key = groupKeyMap.apply(message);
+    final var eventMessage = EventMessage.fromConsumerRecord(message);
 
-    keyBucket.add(EventMessage.fromConsumerRecord(message));
+    groupedCollectedMessages.add(key, eventMessage);
   }
 
   List<EventMessage> messagesByGroupKey(String key) {
-    return collectedMessages.getOrDefault(key, emptyList());
+    return groupedCollectedMessages.messagesByGroupKey(key);
   }
 
   int groupCount() {
-    return collectedMessages.size();
+    return groupedCollectedMessages.groupCount();
   }
 
   void empty() {
-    collectedMessages.clear();
+    groupedCollectedMessages.empty();
   }
 }
