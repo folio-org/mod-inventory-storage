d251d99072fa470cf7ed344ec19428694fdb29e1
Pavlo Smahin <Pavlo_Smahin@epam.com>
2023-02-02 00:07:39 +0200

    MODINVSTOR-1018 Add database level conversion for instance-sets

--- src/main/java/org/folio/persist/InstanceInternalRepository.java
+++ src/main/java/org/folio/persist/InstanceInternalRepository.java
@@ -7,7 +7,6 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
-import io.vertx.core.json.Json;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.RowStream;
@@ -21,11 +20,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.folio.cql2pgjson.CQL2PgJSON;
 import org.folio.persist.entity.InstanceInternal;
-import org.folio.persist.entity.InstanceSetsInternal;
 import org.folio.rest.exceptions.BadRequestException;
 import org.folio.rest.jaxrs.model.Instance;
-import org.folio.rest.jaxrs.model.InstanceSet;
-import org.folio.rest.jaxrs.model.InstanceSets;
 import org.folio.rest.persist.Criteria.Criterion;
 import org.folio.rest.persist.SQLConnection;
 import org.folio.rest.persist.cql.CQLQueryValidationException;
@@ -40,10 +36,6 @@ public class InstanceInternalRepository extends AbstractRepository<Instance> {
 
   public InstanceInternalRepository(Context context, Map<String, String> okapiHeaders) {
     super(postgresClient(context, okapiHeaders), INSTANCE_TABLE, Instance.class);
-  }
-
-  private static <T> List<T> getListSafe(final List<T> collection, boolean returnEmptyList) {
-    return collection == null || (!returnEmptyList && collection.isEmpty()) ? null : collection;
   }
 
   private static Instance toInstance(InstanceInternal internal) {
@@ -118,26 +110,26 @@ public class InstanceInternalRepository extends AbstractRepository<Instance> {
     try {
       StringBuilder sql = new StringBuilder(200);
       sql.append("SELECT jsonb_build_object('id', id");
+      if (instance) {
+        sql.append(", 'instance', migrate_series_and_subjects(jsonb)");
+      }
       if (holdingsRecords) {
         sql.append(", 'holdingsRecords', holdings_records");
       }
-      if (instance) {
-        sql.append(", 'instance', jsonb");
-      }
-      if (succeedingTitles) {
-        sql.append(", 'succeedingTitles', succeeding_titles");
+      if (items) {
+        sql.append(", 'items', items");
       }
       if (precedingTitles) {
         sql.append(", 'precedingTitles', preceding_titles");
       }
-      if (subInstanceRelationships) {
-        sql.append(", 'subInstanceRelationships', sub_instance_relationships");
+      if (succeedingTitles) {
+        sql.append(", 'succeedingTitles', succeeding_titles");
       }
       if (superInstanceRelationships) {
         sql.append(", 'superInstanceRelationships', super_instance_relationships");
       }
-      if (items) {
-        sql.append(", 'items', items");
+      if (subInstanceRelationships) {
+        sql.append(", 'subInstanceRelationships', sub_instance_relationships");
       }
       sql.append(")::text FROM ")
         .append(postgresClientFuturized.getFullTableName(INSTANCE_SET_VIEW))
@@ -147,37 +139,23 @@ public class InstanceInternalRepository extends AbstractRepository<Instance> {
 
       var field = new CQL2PgJSON(INSTANCE_TABLE + ".jsonb");
       var cqlWrapper = new CQLWrapper(field, query, limit, offset, "none");
-      sql.append(cqlWrapper);
+      sql.append(cqlWrapper.toString());
 
       return postgresClient.select(sql.toString())
         .map(rowSet -> {
           StringBuilder json = new StringBuilder("{\"instanceSets\":[\n");
           boolean first = true;
-          for (Row row : rowSet) {
+          var iterator = rowSet.iterator();
+          while (iterator.hasNext()) {
             if (first) {
               first = false;
             } else {
               json.append(",\n");
             }
-            json.append(row.getString(0));
+            json.append(iterator.next().getString(0));
           }
           json.append("\n]}");
-          var instanceSetsInternal = Json.decodeValue(json.toString(), InstanceSetsInternal.class);
-
-          var instanceSets = instanceSetsInternal.getInstanceSets().stream()
-            .map(instanceSetInternal -> new InstanceSet()
-              .withId(instanceSetInternal.getId())
-              .withItems(getListSafe(instanceSetInternal.getItems(), items))
-              .withInstance(toInstance(instanceSetInternal.getInstance()))
-              .withHoldingsRecords(getListSafe(instanceSetInternal.getHoldingsRecords(), holdingsRecords))
-              .withSubInstanceRelationships(getListSafe(instanceSetInternal.getSubInstanceRelationships(), subInstanceRelationships))
-              .withSucceedingTitles(getListSafe(instanceSetInternal.getSucceedingTitles(), succeedingTitles))
-              .withPrecedingTitles(getListSafe(instanceSetInternal.getPrecedingTitles(), precedingTitles))
-              .withSuperInstanceRelationships(getListSafe(instanceSetInternal.getSuperInstanceRelationships(), superInstanceRelationships))
-            )
-            .collect(Collectors.toList());
-          var encode = Json.encode(new InstanceSets().withInstanceSets(instanceSets));
-          return Response.ok(encode, MediaType.APPLICATION_JSON_TYPE).build();
+          return Response.ok(json.toString(), MediaType.APPLICATION_JSON_TYPE).build();
         });
     } catch (CQLQueryValidationException e) {
       return Future.failedFuture(new BadRequestException(e.getMessage()));
@@ -185,4 +163,5 @@ public class InstanceInternalRepository extends AbstractRepository<Instance> {
       return Future.failedFuture(e);
     }
   }
+
 }
---
+++ src/main/resources/templates/db_scripts/convertSeriesAndSubjects.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION ${myuniversity}_${mymodule}.migrate_series_and_subjects(jsonb) RETURNS jsonb AS $$
+DECLARE
+  instance jsonb := $1;
+BEGIN
+  IF jsonb_typeof(instance->'series'->0) = 'string' THEN
+    instance = jsonb_set(instance, '{series}',
+      (SELECT COALESCE(jsonb_agg(v), '[]')
+              FROM (SELECT jsonb_build_object('value', jsonb_array_elements_text(instance->'series')) AS v) x));
+  END IF;
+  IF jsonb_typeof(instance->'subjects'->0) = 'string' THEN
+    instance = jsonb_set(instance, '{subjects}',
+      (SELECT COALESCE(jsonb_agg(v), '[]')
+              FROM (SELECT jsonb_build_object('value', jsonb_array_elements_text(instance->'subjects')) AS v) x));
+  END IF;
+  RETURN instance;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE LEAKPROOF PARALLEL SAFE;
