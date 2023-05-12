c46c587d2e9f7f5e1fcb2a02de84ec2c0b2441fc
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2023-05-12 13:00:47 +0200

    - Add time metrics

--- src/main/java/org/folio/services/authority/AuthorityService.java
+++ src/main/java/org/folio/services/authority/AuthorityService.java
@@ -6,6 +6,7 @@ import static org.folio.rest.persist.PgUtil.deleteById;
 import static org.folio.rest.persist.PgUtil.post;
 import static org.folio.rest.persist.PgUtil.put;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
@@ -14,9 +15,13 @@ import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
 import javax.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -72,26 +77,29 @@ public class AuthorityService {
   public Future<Void> updateAuthorities(String authoritiesFilePath, Integer batchSize) {
     var updateFutures = new LinkedList<Future<RowSet<Row>>>();
 
+    var wholeStart = System.nanoTime();
+    var readTimeMs = new AtomicLong();
+    var parseTimeMs = new AtomicLong();
     log.info("Starting read/write for file {}", authoritiesFilePath);
+
     try (var reader = new BufferedReader(new FileReader(authoritiesFilePath))) {
       String line;
       var authorities = new LinkedList<Authority>();
-      var batchCounter = 1;
+      var batchCounter = 0;
       var recordCounter = 0;
 
-      while ((line = reader.readLine()) != null) {
-        authorities.add(objectMapper.readValue(line, Authority.class));
+      while ((line = executeTimed(() -> readLine(reader), readTimeMs)) != null) {
+        var finalLine = line;
+        var authority = executeTimed(() -> parseAuthority(finalLine), parseTimeMs);
+        authorities.add(authority);
 
         if (authorities.size() == batchSize) {
-          recordCounter += authorities.size();
-          var currentBatchNumber = batchCounter;
-          log.info("Writing batch number {} with batch size {} for file {}",
-            batchCounter++, batchSize, authoritiesFilePath);
-
-          recordCounter += updateAuthorities(authoritiesFilePath, currentBatchNumber, authorities, updateFutures);
+          batchCounter++;
+          recordCounter += updateAuthorities(authoritiesFilePath, batchCounter, authorities, updateFutures);
         }
       }
       if (!authorities.isEmpty()) {
+        batchCounter++;
         recordCounter += updateAuthorities(authoritiesFilePath, batchCounter, authorities, updateFutures);
       }
 
@@ -104,13 +112,27 @@ public class AuthorityService {
         recordCounter, batchCounter - 1, batchSize);
     } catch (Exception ex) {
       log.warn("Exception occurred during processing records for file {}: ", authoritiesFilePath, ex);
+      return Future.failedFuture(new IllegalStateException("Failed to process records.", ex));
     }
+    var startDb = System.nanoTime();
 
-    return GenericCompositeFuture.join(updateFutures)
-      .onSuccess(compositeFuture -> log.info("Successfully finished writing data to db for file {}",
-        authoritiesFilePath))
-      .onFailure(throwable -> log.warn("Db write failed for file {}: ", authoritiesFilePath, throwable))
-      .compose(compositeFuture -> Future.succeededFuture()); //todo: proper exception handling/passing
+    var promise = Promise.<Void>promise();
+    GenericCompositeFuture.join(updateFutures)
+      .onComplete(result -> {
+        if (result.succeeded()) {
+          var endDb = System.nanoTime();
+          var durationDbMs = TimeUnit.NANOSECONDS.toMillis(endDb - startDb);
+          var durationTotal = TimeUnit.NANOSECONDS.toMillis(endDb - wholeStart);
+          log.info("Success for file {}. Read time: {}s, parseTime: {}s, writeTime: {}s. Total time: {}",
+            authoritiesFilePath, getSeconds(readTimeMs.get()), getSeconds(parseTimeMs.get()), getSeconds(durationDbMs),
+            getSeconds(durationTotal));
+          promise.complete();
+        }
+
+        log.warn("Db write failed for file {}: ", authoritiesFilePath, result.cause());
+        promise.fail(result.cause());
+      });
+    return promise.future();
   }
 
   private Integer updateAuthorities(String authoritiesFilePath, Integer currentBatchNumber,
@@ -118,8 +140,11 @@ public class AuthorityService {
                                                 LinkedList<Future<RowSet<Row>>> updateFutures) {
 
     var updateFuture = authorityRepository.update(List.copyOf(authorities))
-      .onFailure(throwable -> log.warn("Failed to write batch number {} to db for file {} with error ",
-        currentBatchNumber, authoritiesFilePath, throwable));
+      .onFailure(throwable -> {
+        log.warn("Failed to write batch number {} to db for file {} with error ",
+        currentBatchNumber, authoritiesFilePath, throwable);
+        throw new IllegalStateException("Unable to write authorities batch to db.");
+      });
     updateFutures.add(updateFuture);
     var updated = authorities.size();
     authorities.clear();
@@ -144,5 +169,36 @@ public class AuthorityService {
   public Future<Void> deleteAllAuthorities() {
     return authorityRepository.deleteAll()
       .compose(notUsed -> domainEventService.publishAllRemoved());
+  }
+
+  private String readLine(BufferedReader reader) {
+    try {
+      return reader.readLine();
+    } catch (IOException e) {
+      throw new IllegalStateException("Unable to read line.", e);
+    }
+  }
+
+  private Authority parseAuthority(String json) {
+    try {
+      return objectMapper.readValue(json, Authority.class);
+    } catch (JsonProcessingException e) {
+      throw new IllegalStateException("Unable to parse authority.", e);
+    }
+  }
+
+  private <T> T executeTimed(Supplier<T> supplier, AtomicLong counter) {
+    var start = System.nanoTime();
+    var result = supplier.get();
+    var end = System.nanoTime();
+
+    var durationMs = TimeUnit.NANOSECONDS.toMillis(end - start);
+    counter.getAndAdd(durationMs);
+
+    return result;
+  }
+
+  private double getSeconds(long millis) {
+    return ((double) millis) / 1_000;
   }
 }
