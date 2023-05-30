f7cd0e30319050bbf942c8e211b337cccf7c67c6
Roman_Barannyk <roman_barannyk@epam.com>
2023-05-30 19:25:58 +0300

    MODINVSTOR-1043 fix code smell

--- src/main/java/org/folio/persist/ServicePointRepository.java
+++ src/main/java/org/folio/persist/ServicePointRepository.java
@@ -8,7 +8,7 @@ import org.folio.rest.jaxrs.model.Servicepoint;
 
 public class ServicePointRepository extends AbstractRepository<Servicepoint> {
 
-  public static final String SERVICE_POINT_TABLE = "service_point";
+  private static final String SERVICE_POINT_TABLE = "service_point";
 
   public ServicePointRepository(Context context, Map<String, String> okapiHeaders) {
     super(postgresClient(context, okapiHeaders), SERVICE_POINT_TABLE, Servicepoint.class);
