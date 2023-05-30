fad6c8d9bbebd16b77923bdaaa5feb60456264e3
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-05-30 08:14:30 +0200

    Remove SELECT used for debugging

--- src/main/resources/templates/db_scripts/hridSettingsView.sql
+++ src/main/resources/templates/db_scripts/hridSettingsView.sql
@@ -1,6 +1,4 @@
-SELECT 'hridSettingsView.sql';
-
-CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.hrid_settings_view AS
+CREATE OR REPLACE VIEW hrid_settings_view AS
   SELECT jsonb_set(jsonb_set(jsonb_set(jsonb,
     '{instances,currentNumber}', to_jsonb(hrid_instances_seq.last_value
                                           - CASE WHEN hrid_instances_seq.is_called THEN 0 ELSE 1 END)),
@@ -9,14 +7,10 @@ CREATE OR REPLACE VIEW ${myuniversity}_${mymodule}.hrid_settings_view AS
     '{items,currentNumber}',     to_jsonb(hrid_items_seq.last_value
                                           - CASE WHEN hrid_items_seq.is_called     THEN 0 ELSE 1 END))
     AS jsonb
-  FROM ${myuniversity}_${mymodule}.hrid_settings,
-       ${myuniversity}_${mymodule}.hrid_instances_seq,
-       ${myuniversity}_${mymodule}.hrid_holdings_seq,
-       ${myuniversity}_${mymodule}.hrid_items_seq;
+  FROM hrid_settings, hrid_instances_seq, hrid_holdings_seq, hrid_items_seq;
 
-ALTER TABLE    ${myuniversity}_${mymodule}.hrid_settings      OWNER TO ${myuniversity}_${mymodule};
-ALTER SEQUENCE ${myuniversity}_${mymodule}.hrid_instances_seq OWNER TO ${myuniversity}_${mymodule};
-ALTER SEQUENCE ${myuniversity}_${mymodule}.hrid_holdings_seq  OWNER TO ${myuniversity}_${mymodule};
-ALTER SEQUENCE ${myuniversity}_${mymodule}.hrid_items_seq     OWNER TO ${myuniversity}_${mymodule};
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA ${myuniversity}_${mymodule}
-  TO ${myuniversity}_${mymodule};
+ALTER TABLE    hrid_settings      OWNER TO ${myuniversity}_${mymodule};
+ALTER SEQUENCE hrid_instances_seq OWNER TO ${myuniversity}_${mymodule};
+ALTER SEQUENCE hrid_holdings_seq  OWNER TO ${myuniversity}_${mymodule};
+ALTER SEQUENCE hrid_items_seq     OWNER TO ${myuniversity}_${mymodule};
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA ${myuniversity}_${mymodule} TO ${myuniversity}_${mymodule};
