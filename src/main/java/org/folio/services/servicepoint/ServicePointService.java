5e631dcdd633146a2777c9677d36acd395a875c8
Roman_Barannyk <roman_barannyk@epam.com>
2023-05-30 19:56:08 +0300

    MODINVSTOR-1043 change order of imports

--- src/main/java/org/folio/services/servicepoint/ServicePointService.java
+++ src/main/java/org/folio/services/servicepoint/ServicePointService.java
@@ -1,9 +1,9 @@
 package org.folio.services.servicepoint;
 
+import io.vertx.core.Context;
+import io.vertx.core.Future;
 import java.util.Map;
-
 import javax.ws.rs.core.Response;
-
 import org.folio.persist.ServicePointRepository;
 import org.folio.rest.exceptions.NotFoundException;
 import org.folio.rest.jaxrs.model.Servicepoint;
@@ -11,9 +11,6 @@ import org.folio.rest.jaxrs.resource.ItemStorage;
 import org.folio.services.domainevent.ServicePointDomainEventPublisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import io.vertx.core.Context;
-import io.vertx.core.Future;
 
 public class ServicePointService {
 
