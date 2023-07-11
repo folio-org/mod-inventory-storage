dcfa8133526d32c7bb7efddda6c4bab87abfaece
hasan <khasan_sidikov@epam.com>
2023-07-11 12:50:20 +0500

    MODINVSTOR-1069: Code style corrected

--- src/test/java/org/folio/services/SuDocCallNumberTest.java
+++ src/test/java/org/folio/services/SuDocCallNumberTest.java
@@ -1,10 +1,12 @@
 package org.folio.services;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class SuDocCallNumberTest {
 
