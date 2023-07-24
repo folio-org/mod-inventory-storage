6c48f1533dcbbc70ee7e1c62075f6be59b07cfef
hasanepam <120184658+hasanepam@users.noreply.github.com>
2023-07-24 22:46:03 +0500

    MODINVSTOR-1069: Added SuDoc call number implementation (#914)
    * MODINVSTOR-1069: Added SuDoc call number implementation
    
    * MODINVSTOR-1069: New test case added
    
    * MODINVSTOR-1069: Code style corrected
    
    * MODINVSTOR-1069: Regexp pattern simplified, some code smells fixed
    
    * MODINVSTOR-1069: Checking first char of call number to digit simplified
    
    * MODINVSTOR-1069: Added test cases for sorting
    
    * MODINVSTOR-1069: Code style corrected
    
    * MODINVSTOR-1069: SuDoc call number shelf key creation improved, related test added
    
    * MODINVSTOR-1069: Some refactoring added
    
    * - add test case from the story
    
    * MODINVSTOR-1069: Some refactoring added
    
    * MODINVSTOR-1069: Some improvements added to test
    
    * MODINVSTOR-1069: Some regex improvements added
    
    * MODINVSTOR-1069: Some improvements to regex and call number parsing added
    
    * MODINVSTOR-1069: SuDoc call number made first in shelf key generation
    
    * MODINVSTOR-1069: Identifying call numbers by their type id logic added
    
    * MODINVSTOR-1069: Added some improvements
    
    ---------
    
    Co-authored-by: Pavlo Smahin <Pavlo_Smahin@epam.com>

---
+++ src/main/java/org/folio/services/SuDocCallNumber.java
@@ -0,0 +1,124 @@
+package org.folio.services;
+
+import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
+import org.marc4j.callnum.AbstractCallNumber;
+import org.marc4j.callnum.Utils;
+
+public class SuDocCallNumber extends AbstractCallNumber {
+  private static final String GROUP1 = "([A-Za-z]+\\s*)";
+  private static final String GROUP2 = "(\\d+)";
+  private static final String GROUP3 = "(\\.(?:[A-Za-z]+\\d*|\\d+))";
+  private static final String GROUP4 = "(/(?:[A-Za-z]+(?:\\d+(?:-\\d+)?)?|\\d+(?:-\\d+)?))?";
+  private static final String GROUP5 = "(.*)";
+  public static final String SU_DOC_PATTERN = "^(?:" + GROUP1 + GROUP2 + GROUP3 + GROUP4 + ")?:?" + GROUP5;
+  protected static Pattern stemPattern = Pattern.compile(SU_DOC_PATTERN);
+  protected String authorSymbol;
+  protected String subordinateOffice;
+  protected String series;
+  protected String subSeries;
+
+  protected String suffix;
+  protected String shelfKey;
+
+
+  public SuDocCallNumber(String callNumber) {
+    this.parse(callNumber);
+  }
+
+  protected void init() {
+    this.rawCallNum = null;
+    this.authorSymbol = null;
+    this.subordinateOffice = null;
+    this.series = null;
+    this.subSeries = null;
+    this.suffix = null;
+    this.shelfKey = null;
+  }
+
+  @Override
+  public void parse(String s) {
+    this.init();
+    if (s == null) {
+      this.rawCallNum = null;
+    } else {
+      this.rawCallNum = s.trim();
+    }
+
+    this.parse();
+  }
+
+  protected void parse() {
+    if (this.rawCallNum != null) {
+      this.parseCallNumber();
+    }
+  }
+
+  protected void parseCallNumber() {
+    var stemMatcher = stemPattern.matcher(rawCallNum);
+    if (stemMatcher.matches()) {
+      authorSymbol = stemMatcher.group(1);
+      subordinateOffice = stemMatcher.group(2);
+      series = stemMatcher.group(3);
+      subSeries = stemMatcher.group(4);
+      suffix = stemMatcher.group(5);
+    } else {
+      suffix = rawCallNum;
+    }
+  }
+
+  @Override
+  public String getShelfKey() {
+    if (shelfKey == null) {
+      buildShelfKey();
+    }
+    return shelfKey;
+  }
+
+  @Override
+  public boolean isValid() {
+    return authorSymbol != null;
+  }
+
+  private void buildShelfKey() {
+    StringBuilder key = new StringBuilder();
+    if (authorSymbol != null) {
+      key.append(authorSymbol.trim());
+    }
+
+    appendWithSymbolIfNeeded(key, subordinateOffice);
+    appendWithSymbolIfNeeded(key, series);
+    appendWithSymbolIfNeeded(key, subSeries);
+    appendWithSymbolIfNeeded(key, suffix);
+
+    shelfKey = key.toString();
+  }
+
+  private void appendWithSymbolIfNeeded(StringBuilder key, String cnPart) {
+    if (StringUtils.isBlank(cnPart)) {
+      return;
+    }
+
+    if (cnPart.startsWith(".") || cnPart.startsWith("/") || cnPart.startsWith("-") || cnPart.startsWith(":")) {
+      cnPart = cnPart.substring(1);
+    }
+    var parts = cnPart.split("[./ -]");
+    for (String part : parts) {
+      if (key.length() > 0) {
+        key.append(' ');
+      }
+      part = part.trim();
+
+      if (StringUtils.isBlank(part)) {
+        continue;
+      }
+      if (Character.isAlphabetic(part.charAt(0))) {
+        key.append(" !");
+      } else if (part.length() >= 3) {
+        key.append("!");
+      }
+
+      Utils.appendNumericallySortable(key, part);
+    }
+  }
+}
