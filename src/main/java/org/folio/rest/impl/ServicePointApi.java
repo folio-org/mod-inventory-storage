e7fe5e3f96757325eef4b8ffbb173a59c4a20dee
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2023-08-14 11:00:19 +0300

    MODINVSTOR-1077 Fix code smell

--- src/main/java/org/folio/rest/impl/ServicePointApi.java
+++ src/main/java/org/folio/rest/impl/ServicePointApi.java
@@ -12,7 +12,6 @@ import java.util.Map;
 import java.util.UUID;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.lang3.BooleanUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.rest.RestVerticle;
