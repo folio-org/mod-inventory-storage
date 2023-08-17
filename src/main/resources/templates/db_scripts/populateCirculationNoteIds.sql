22aca18230d4473ef02d1b0cb151675376107344
Bekhzod Kurbonboev <bekhzod_kurbonboev@epam.com>
2023-08-18 00:13:01 +0500

    Clean up object creation. Add flag to detect changes

--- src/main/resources/templates/db_scripts/populateCirculationNoteIds.sql
+++ src/main/resources/templates/db_scripts/populateCirculationNoteIds.sql
@@ -1,22 +1,14 @@
-WITH item_circnotes as (
-	SELECT itemId, ARRAY_AGG(circNote) AS circNotes FROM (
-	  SELECT item.id AS itemId, CASE
-	  WHEN circulationNote ->> 'id' IS NULL
-		  THEN jsonb_build_object(
-			  'id', gen_random_uuid(),
-			  'noteType' ,circulationNote ->> 'noteType',
-			  'note', circulationNote ->> 'note',
-			  'source', circulationNote ->> 'source',
-			  'date', circulationNote ->> 'date',
-			  'staffOnly', circulationNote ->> 'staffOnly')
-		  ELSE jsonb_build_object(
-			  'id', circulationNote ->> 'id',
-			  'noteType' ,circulationNote ->> 'noteType',
-			  'note', circulationNote ->> 'note',
-			  'source', circulationNote ->> 'source',
-			  'date', circulationNote ->> 'date',
-			  'staffOnly', circulationNote ->> 'staffOnly')
-		  END as circNote
+DROP EXTENSION "uuid-ossp";
+CREATE EXTENSION "uuid-ossp";
+
+--Update circulationNotes of item only if there is at least one circulationNote with id equals null
+WITH item_circnotes AS (
+  SELECT itemId, ARRAY_AGG(circNote) AS circNotes, BOOL_OR(changed) AS changed
+  FROM (
+    SELECT
+      item.id AS itemId,
+      CASE WHEN circulationNote ->> 'id' IS NULL THEN jsonb_set(circulationNote, '{id}', to_jsonb(uuid_generate_v4())) ELSE circulationNote END AS circNote,
+      CASE WHEN circulationNote ->> 'id' IS NULL THEN true ELSE false END AS changed
     FROM ${myuniversity}_${mymodule}.item, jsonb_array_elements((jsonb ->> 'circulationNotes')::jsonb) WITH ORDINALITY arr(circulationNote, index)
   ) AS tableA
   GROUP BY itemId
@@ -24,4 +16,4 @@ WITH item_circnotes as (
 UPDATE ${myuniversity}_${mymodule}.item
 SET jsonb = jsonb_set(jsonb, '{circulationNotes}', to_jsonb(item_circnotes.circNotes))
 FROM item_circnotes
-WHERE item.id = item_circnotes.itemId;
+WHERE item.id = item_circnotes.itemId AND item_circnotes.changed;
