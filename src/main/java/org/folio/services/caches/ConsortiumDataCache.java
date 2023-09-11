9249422da04ba97192ed495a986a4bf9969f70b8
Ruslan Lavrov <ruslan_lavrov@epam.com>
2023-09-11 11:01:44 +0300

    Changed cache implementation to support envs which can contain consortium and non-consortium tenants

--- src/main/java/org/folio/services/caches/ConsortiumDataCache.java
+++ src/main/java/org/folio/services/caches/ConsortiumDataCache.java
@@ -17,14 +17,16 @@ import io.vertx.ext.web.client.WebClient;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ConsortiumDataCache {
 
   private static final Logger LOG = LogManager.getLogger(ConsortiumDataCache.class);
+  private static final String EXPIRATION_TIME_PARAM = "cache.consortium-data.expiration.time.seconds";
+  private static final String DEFAULT_EXPIRATION_TIME_SECONDS = "300";
   private static final String USER_TENANTS_PATH = "/user-tenants?limit=1"; //NOSONAR
-  private static final String CONSORTIA_KEY = "consortia";
   private static final String USER_TENANTS_FIELD = "userTenants";
   private static final String CENTRAL_TENANT_ID_FIELD = "centralTenantId";
   private static final String CONSORTIUM_ID_FIELD = "consortiumId";
@@ -33,22 +35,24 @@ public class ConsortiumDataCache {
   private final AsyncCache<String, Optional<ConsortiumData>> cache;
 
   public ConsortiumDataCache(Vertx vertx, HttpClient httpClient) {
+    int expirationTime = Integer.parseInt(System.getProperty(EXPIRATION_TIME_PARAM, DEFAULT_EXPIRATION_TIME_SECONDS));
     this.httpClient = httpClient;
     this.cache = Caffeine.newBuilder()
+      .expireAfterWrite(expirationTime, TimeUnit.SECONDS)
       .executor(task -> vertx.runOnContext(v -> task.run()))
       .buildAsync();
   }
 
-  public Future<Optional<ConsortiumData>> getConsortiumData(Map<String, String> headers) {
+  public Future<Optional<ConsortiumData>> getConsortiumData(String tenantId, Map<String, String> headers) {
     try {
-      return Future.fromCompletionStage(cache.get(CONSORTIA_KEY, (key, executor) -> loadConsortiumData(headers)));
+      return Future.fromCompletionStage(cache.get(tenantId, (key, executor) -> loadConsortiumData(key, headers)));
     } catch (Exception e) {
-      LOG.warn("getConsortiumData:: Error loading consortium data", e);
+      LOG.warn("getConsortiumData:: Error loading consortium data, tenantId: '{}'", tenantId, e);
       return Future.failedFuture(e);
     }
   }
 
-  private CompletableFuture<Optional<ConsortiumData>> loadConsortiumData(Map<String, String> headers) {
+  private CompletableFuture<Optional<ConsortiumData>> loadConsortiumData(String tenantId, Map<String, String> headers) {
     String okapiUrl = headers.get(URL);
     WebClient client = WebClient.wrap(httpClient);
     HttpRequest<Buffer> request = client.requestAbs(GET, okapiUrl + USER_TENANTS_PATH);
@@ -56,8 +60,8 @@ public class ConsortiumDataCache {
 
     return request.send().compose(response -> {
       if (response.statusCode() != HTTP_OK) {
-        String msg = String.format("Error loading consortium data, response status: %s, body: '%s'",
-          response.statusCode(), response.bodyAsString());
+        String msg = String.format("Error loading consortium data, tenantId: '%s' response status: '%s', body: '%s'",
+          tenantId, response.statusCode(), response.bodyAsString());
         LOG.warn("loadConsortiumData:: {}", msg);
         return Future.failedFuture(msg);
       }
@@ -66,7 +70,7 @@ public class ConsortiumDataCache {
         return Future.succeededFuture(Optional.<ConsortiumData>empty());
       }
 
-      LOG.info("loadConsortiumData:: Consortium data was loaded");
+      LOG.info("loadConsortiumData:: Consortium data was loaded, tenantId: '{}'", tenantId);
       JsonObject userTenant = userTenants.getJsonObject(0);
       return Future.succeededFuture(Optional.of(
         new ConsortiumData(userTenant.getString(CENTRAL_TENANT_ID_FIELD), userTenant.getString(CONSORTIUM_ID_FIELD))));
