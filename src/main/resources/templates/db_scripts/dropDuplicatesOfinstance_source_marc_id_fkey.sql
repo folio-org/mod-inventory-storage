e28555a0c75ff48101e6c65081aefa141597f84d
Andrei_Razumnov <andrei_razumnov@epam.com>
2023-09-15 13:21:12 +0400

    MODINVSTOR-1086 Add script to remove fk duplicates.

---
+++ src/main/resources/templates/db_scripts/dropDuplicatesOfinstance_source_marc_id_fkey.sql
@@ -0,0 +1,40 @@
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey1;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey2;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey3;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey4;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey5;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey6;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey7;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey8;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey9;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey10;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey11;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey12;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey13;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey14;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey15;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey16;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey17;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey18;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey19;
+ALTER TABLE ${myuniversity}_${mymodule}.instance_source_marc
+  DROP CONSTRAINT IF EXISTS instance_source_marc_id_fkey20;
