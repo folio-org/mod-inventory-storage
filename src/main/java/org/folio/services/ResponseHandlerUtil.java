e84203ae29c42545fd14fc08cc1b27070a16dae0
GeloPakDev1 <Oleg_Pak1@epam.com>
2024-01-22 17:28:02 +0500

    fix(response-message): Make response message more informative.
    Previously, during POST on the /instance-storage/instances with instance with existing hrid, response body contained database-related info "lower(f_unaccent(jsonb ->> 'hrid'::text))" and processed as success to the handler in the postInstanceStorageInstances method. This commit solves this problem by creating handler to process error message and make it informative for user.
    
    - Create ResponseHandlerUtil class to handle response messages.
    
    Closes MODINVSTOR-1100

--- src/main/java/org/folio/services/ResponseHandlerUtil.java
+++ src/main/java/org/folio/services/ResponseHandlerUtil.java
@@ -6,36 +6,25 @@ import javax.ws.rs.core.Response;
 
 public final class ResponseHandlerUtil {
   private static final String HRID_ERROR_MESSAGE = "lower(f_unaccent(jsonb ->> 'hrid'::text))";
-  private static final String HRID = "HRID ";
+  private static final String HRID = "HRID";
 
   private ResponseHandlerUtil() {
   }
 
   public static Response handleResponse(Response response) {
-    var errorMessage = response.getEntity().toString();
-    if (errorMessage.contains(HRID_ERROR_MESSAGE) && (errorMessage.contains("instance"))) {
-      return createResponse(errorMessage);
-    } else {
+    if (response.getStatus() != 400) {
       return response;
     }
-  }
-
-  private static Response createResponse(String errorMessage) {
-    var message = extractErrorMessage(errorMessage, HRID_ERROR_MESSAGE);
-    return textPlainResponse(400, HRID + message);
+    var errorMessage = response.getEntity().toString();
+    if (errorMessage.contains(HRID_ERROR_MESSAGE) && (errorMessage.contains("instance"))) {
+      errorMessage = errorMessage.replace(HRID_ERROR_MESSAGE, HRID);
+      return textPlainResponse(400, errorMessage);
+    }
+    return response;
   }
 
   private static Response textPlainResponse(int status, String message) {
     return Response.status(status).header(CONTENT_TYPE, "text/plain")
       .entity(message).build();
-  }
-
-  public static String extractErrorMessage(String input, String substringToFind) {
-    int index = input.indexOf(substringToFind);
-    if (index != -1) {
-      return input.substring(index + substringToFind.length()).trim();
-    } else {
-      return input;
-    }
   }
 }
