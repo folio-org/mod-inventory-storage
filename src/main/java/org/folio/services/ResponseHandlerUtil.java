a72848c6d06c2db308257b2d20d9714ffb6a0df7
GeloPakDev1 <Oleg_Pak1@epam.com>
2024-01-24 19:16:22 +0500

    fix(response-message): Make response message more informative.
    Previously, during POST on the /instance-storage/instances with instance with existing hrid, response body contained database-related info "lower(f_unaccent(jsonb ->> 'hrid'::text))" and processed as success to the handler in the postInstanceStorageInstances method. This commit solves this problem by creating handler to process error message and make it informative for user.
    
    - Create ResponseHandlerUtil class to handle response messages.
    
    Closes MODINVSTOR-1100

--- src/main/java/org/folio/services/ResponseHandlerUtil.java
+++ src/main/java/org/folio/services/ResponseHandlerUtil.java
@@ -2,12 +2,10 @@ package org.folio.services;
 
 import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 
-import java.util.logging.Logger;
 import javax.ws.rs.core.Response;
 import org.folio.rest.jaxrs.model.Errors;
 
 public final class ResponseHandlerUtil {
-  private static final Logger logger = Logger.getLogger(ResponseHandlerUtil.class.getName());
   private static final String HRID_ERROR_MESSAGE = "lower(f_unaccent(jsonb ->> 'hrid'::text))";
   private static final String HRID = "HRID";
 
@@ -16,19 +14,17 @@ public final class ResponseHandlerUtil {
 
   public static Response handleHridError(Response response) {
     var statusCode = response.getStatus();
+
     if (statusCode == 201) {
       return response;
     }
-    var errorMessage = getErrorMessage(response.getEntity());
-    logger.info("Status code is" + statusCode + " and error message is " + errorMessage);
 
+    var errorMessage = getErrorMessage(response.getEntity());
     if (errorMessage.contains(HRID_ERROR_MESSAGE)
       && errorMessage.contains("instance") && statusCode == 400) {
-      logger.info("Constructing response with 400 status code and message: " + errorMessage);
       return createResponse(400, errorMessage);
     } else if (errorMessage.contains(HRID_ERROR_MESSAGE)
       && (errorMessage.contains("item") || errorMessage.contains("holdings_record")) && statusCode == 422) {
-      logger.info("Constructing response with 422 status code and message: " + errorMessage);
       return createResponse(422, errorMessage);
     }
     return response;
@@ -37,13 +33,10 @@ public final class ResponseHandlerUtil {
   private static String getErrorMessage(Object responseEntity) {
     var errorMessage = responseEntity.toString();
     if (responseEntity instanceof Errors errors) {
-      logger.info("responseEntity is instance of Errors");
       errorMessage = errors.getErrors().get(0).getMessage();
     } else if (responseEntity instanceof String message) {
-      logger.info("responseEntity is instance of String");
       errorMessage = message;
     }
-    logger.info("Error message is " + errorMessage);
     return errorMessage;
   }
 
