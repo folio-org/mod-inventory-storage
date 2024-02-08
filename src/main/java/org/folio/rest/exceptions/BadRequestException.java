d8a9105d5ebd1d2013427c6bc993e203881bba0c
GeloPakDev1 <Oleg_Pak1@epam.com>
2024-02-08 14:56:11 +0500

    refactor(revert-solution): Revert previous temporary solution

--- src/main/java/org/folio/rest/exceptions/BadRequestException.java
+++ src/main/java/org/folio/rest/exceptions/BadRequestException.java
@@ -1,5 +1,5 @@
 package org.folio.rest.exceptions;
-
+//
 public final class BadRequestException extends InventoryProcessingException {
   public BadRequestException(Object message) {
     super(message);
