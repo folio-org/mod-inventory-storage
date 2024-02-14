8fe29e2b766df4fdcb02474e4cabffea5c5a9c4e
psmagin <pavlo_smahin@epam.com>
2024-02-14 18:47:15 +0200

    - change vertx to the same as RMB version

--- src/main/java/org/folio/services/SuDocCallNumber.java
+++ src/main/java/org/folio/services/SuDocCallNumber.java
@@ -1,7 +1,7 @@
 package org.folio.services;
 
 import java.util.regex.Pattern;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.marc4j.callnum.AbstractCallNumber;
 import org.marc4j.callnum.Utils;
 
