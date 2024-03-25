27867b98dc7a09887e84c7570f250752669128ef
psmagin <pavlo_smahin@epam.com>
2024-03-25 21:06:18 +0200

    feat(locations): implement domain event production for location create/update/delete
    Closes: MODINVSTOR-1181

--- src/main/java/org/folio/InventoryKafkaTopic.java
+++ src/main/java/org/folio/InventoryKafkaTopic.java
@@ -1,9 +1,13 @@
 package org.folio;
 
+import java.util.Map;
+import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.folio.kafka.services.KafkaTopic;
 
 public enum InventoryKafkaTopic implements KafkaTopic {
+
   INSTANCE("instance"),
   ITEM("item"),
   HOLDINGS_RECORD("holdings-record"),
@@ -11,13 +15,21 @@ public enum InventoryKafkaTopic implements KafkaTopic {
   BOUND_WITH("bound-with"),
   ASYNC_MIGRATION("async-migration"),
   SERVICE_POINT("service-point"),
-  CLASSIFICATION_TYPE("classification-type");
+  CLASSIFICATION_TYPE("classification-type"),
+  LOCATION("location");
 
   private static final String DEFAULT_NUM_PARTITIONS_PROPERTY = "KAFKA_DOMAIN_TOPIC_NUM_PARTITIONS";
   private static final String DEFAULT_NUM_PARTITIONS_VALUE = "50";
-  private static final String CLASSIFICATION_TYPE_NUM_PARTITIONS_PROPERTY =
-    "KAFKA_CLASSIFICATION_TYPE_TOPIC_NUM_PARTITIONS";
-  private static final String CLASSIFICATION_TYPE_NUM_PARTITIONS_VALUE = "1";
+
+  /**
+   * Map where a key is {@link InventoryKafkaTopic} and value is a {@link Pair} of
+   * environment variable name that specifies number of partitions for the topic and default value is not specified.
+   */
+  private static final Map<InventoryKafkaTopic, Pair<String, String>> TOPIC_PARTITION_MAP = Map.of(
+    CLASSIFICATION_TYPE, Pair.of("KAFKA_CLASSIFICATION_TYPE_TOPIC_NUM_PARTITIONS", "1"),
+    LOCATION, Pair.of("KAFKA_LOCATION_TOPIC_NUM_PARTITIONS", "1")
+  );
+
   private final String topic;
 
   InventoryKafkaTopic(String topic) {
@@ -36,11 +48,9 @@ public enum InventoryKafkaTopic implements KafkaTopic {
 
   @Override
   public int numPartitions() {
-    if (this == CLASSIFICATION_TYPE) {
-      return getNumberOfPartitions(CLASSIFICATION_TYPE_NUM_PARTITIONS_PROPERTY,
-        CLASSIFICATION_TYPE_NUM_PARTITIONS_VALUE);
-    }
-    return getNumberOfPartitions(DEFAULT_NUM_PARTITIONS_PROPERTY, DEFAULT_NUM_PARTITIONS_VALUE);
+    return Optional.ofNullable(TOPIC_PARTITION_MAP.get(this))
+      .map(pair -> getNumberOfPartitions(pair.getKey(), pair.getValue()))
+      .orElse(getNumberOfPartitions(DEFAULT_NUM_PARTITIONS_PROPERTY, DEFAULT_NUM_PARTITIONS_VALUE));
   }
 
   private int getNumberOfPartitions(String propertyName, String defaultNumPartitions) {
