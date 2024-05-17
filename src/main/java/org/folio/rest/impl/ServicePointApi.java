04cc0c0f14114ad63d3d7a61bd8f20faf7e41db4
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-05-17 11:07:19 +0300

    MODINVSTOR-1219 Fix checkstyle violations

--- src/main/java/org/folio/rest/impl/ServicePointApi.java
+++ src/main/java/org/folio/rest/impl/ServicePointApi.java
@@ -11,7 +11,6 @@ import io.vertx.core.Handler;
 import java.util.Map;
 import java.util.UUID;
 import javax.ws.rs.core.Response;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
