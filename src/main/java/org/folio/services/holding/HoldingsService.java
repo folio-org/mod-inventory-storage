61e760445c00e719fcf5dd3f76d54c990439e8a0
Viacheslav_Poliakov <Viacheslav_Poliakov@epam.com>
2024-05-21 15:05:07 +0300

    feat(holdings-record): Restrict updating without sourceId
    - Delete validation from service
    
    Closes: MODINVSTOR-1161

--- src/main/java/org/folio/services/holding/HoldingsService.java
+++ src/main/java/org/folio/services/holding/HoldingsService.java
@@ -1,6 +1,5 @@
 package org.folio.services.holding;
 
-import static io.vertx.core.Future.failedFuture;
 import static io.vertx.core.Promise.promise;
 import static org.apache.logging.log4j.LogManager.getLogger;
 import static org.folio.rest.impl.HoldingsStorageApi.HOLDINGS_RECORD_TABLE;
@@ -14,7 +13,6 @@ import static org.folio.rest.persist.PgUtil.deleteById;
 import static org.folio.rest.persist.PgUtil.post;
 import static org.folio.rest.persist.PgUtil.postSync;
 import static org.folio.rest.persist.PgUtil.postgresClient;
-import static org.folio.rest.tools.utils.ValidationHelper.createValidationErrorMessage;
 import static org.folio.services.batch.BatchOperationContextFactory.buildBatchOperationContext;
 import static org.folio.validator.HridValidators.refuseWhenHridChanged;
 
@@ -33,7 +31,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.folio.persist.HoldingsRepository;
 import org.folio.persist.InstanceRepository;
-import org.folio.rest.exceptions.ValidationException;
 import org.folio.rest.jaxrs.model.HoldingsRecord;
 import org.folio.rest.jaxrs.model.Item;
 import org.folio.rest.persist.PostgresClient;
@@ -91,11 +88,6 @@ public class HoldingsService {
   }
 
   public Future<Response> updateHoldingRecord(String holdingId, HoldingsRecord holdingsRecord) {
-    if (holdingsRecord.getSourceId() == null) {
-      return failedFuture(new ValidationException(createValidationErrorMessage("sourceId",
-        holdingsRecord.getSourceId(), "The sourceId field required: cannot be null or deleted")));
-    }
-
     return holdingsRepository.getById(holdingId)
       .compose(existingHoldingsRecord -> {
         if (holdingsRecordFound(existingHoldingsRecord)) {
