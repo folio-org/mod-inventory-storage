eb77a31ee20a44e068d4ce163f3d9b9218aab89c
BKadirkhodjaev <b.kadirkhodjaev@gmail.com>
2024-06-04 02:06:50 +0500

    [MODINVSTOR-1218]. Add inventory.institution topic, add event publisher, update README.MD & ModuleDescriptor-template.json, add ITs

---
+++ src/main/java/org/folio/services/locationunit/InstitutionService.java
@@ -0,0 +1,118 @@
+package org.folio.services.locationunit;
+
+import static io.vertx.core.Future.succeededFuture;
+import static org.folio.rest.impl.LocationUnitApi.URL_PREFIX;
+import static org.folio.rest.tools.utils.ValidationHelper.createValidationErrorMessage;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import lombok.extern.log4j.Log4j2;
+import org.folio.persist.InstitutionRepository;
+import org.folio.rest.jaxrs.model.Locinst;
+
+import javax.ws.rs.core.Response;
+import org.folio.rest.jaxrs.model.Locinsts;
+import org.folio.rest.jaxrs.resource.LocationUnits.GetLocationUnitsInstitutionsResponse;
+import org.folio.rest.jaxrs.resource.LocationUnits.GetLocationUnitsInstitutionsByIdResponse;
+import org.folio.rest.jaxrs.resource.LocationUnits.PostLocationUnitsInstitutionsResponse;
+import org.folio.rest.jaxrs.resource.LocationUnits.PutLocationUnitsInstitutionsByIdResponse;
+import org.folio.rest.jaxrs.resource.LocationUnits.DeleteLocationUnitsCampusesByIdResponse;
+import org.folio.rest.jaxrs.resource.LocationUnits.DeleteLocationUnitsInstitutionsResponse;
+import org.folio.rest.persist.PgUtil;
+import org.folio.services.domainevent.InstitutionDomainEventPublisher;
+
+@Log4j2
+public class InstitutionService {
+
+  public static final String INSTITUTION_TABLE = "locinstitution";
+  private static final String MSG_ID_NOT_MATCH =
+    "Illegal operation: Institution ID cannot be changed";
+
+  private final Context vertxContext;
+  private final Map<String, String> okapiHeaders;
+  private final InstitutionRepository repository;
+  private final InstitutionDomainEventPublisher domainEventService;
+
+  public InstitutionService(Context vertxContext,
+                            Map<String, String> okapiHeaders) {
+    this.vertxContext = vertxContext;
+    this.okapiHeaders = okapiHeaders;
+    this.repository = new InstitutionRepository(vertxContext, okapiHeaders);
+    this.domainEventService =
+      new InstitutionDomainEventPublisher(vertxContext, okapiHeaders);
+  }
+
+  public Future<Response> getByQuery(String cql, int offset, int limit) {
+    return PgUtil.get(INSTITUTION_TABLE, Locinst.class, Locinsts.class, cql,
+      offset, limit, okapiHeaders, vertxContext,
+      GetLocationUnitsInstitutionsResponse.class);
+  }
+
+  public Future<Response> getById(String id) {
+    return PgUtil.getById(INSTITUTION_TABLE, Locinst.class, id, okapiHeaders,
+      vertxContext, GetLocationUnitsInstitutionsByIdResponse.class);
+  }
+
+  public Future<Response> create(Locinst institution) {
+    return PgUtil.post(INSTITUTION_TABLE, institution, okapiHeaders,
+        vertxContext, PostLocationUnitsInstitutionsResponse.class)
+      .onSuccess(response -> {
+        var headers = PostLocationUnitsInstitutionsResponse.headersFor201()
+          .withLocation(URL_PREFIX + response);
+
+        PostLocationUnitsInstitutionsResponse.respond201WithApplicationJson(
+          response, headers);
+      });
+  }
+
+  public Future<Response> update(String id, Locinst institution) {
+    if (Objects.nonNull(institution.getId()) && !institution.getId().equals(id)) {
+      return succeededFuture(
+        PutLocationUnitsInstitutionsByIdResponse.respond400WithTextPlain(
+          createValidationErrorMessage("locinst", institution.getId(),
+            MSG_ID_NOT_MATCH)));
+    }
+
+    if (Objects.isNull(institution.getId())) {
+      institution.setId(id);
+    }
+
+    return repository.getById(id)
+      .compose(oldInstitution ->
+        PgUtil.put(INSTITUTION_TABLE, institution, id,
+          okapiHeaders, vertxContext,
+          PutLocationUnitsInstitutionsByIdResponse.class)
+        .onSuccess(domainEventService.publishUpdated(oldInstitution)));
+  }
+
+  public Future<Response> delete(String id) {
+    return repository.getById(id)
+      .compose(oldInstitution ->
+        PgUtil.deleteById(INSTITUTION_TABLE, id, okapiHeaders,
+          vertxContext, DeleteLocationUnitsCampusesByIdResponse.class)
+        .onSuccess(domainEventService.publishRemoved(oldInstitution)));
+  }
+
+  public Future<Response> deleteAll() {
+    return repository.deleteAll().transform(prepareDeleteAllResponse())
+      .onSuccess(response -> domainEventService.publishAllRemoved());
+  }
+
+  private Function<AsyncResult<RowSet<Row>>, Future<Response>> prepareDeleteAllResponse() {
+    return reply -> {
+      var respond204 =
+        DeleteLocationUnitsInstitutionsResponse.respond204();
+      var respond500 =
+        DeleteLocationUnitsInstitutionsResponse.respond500WithTextPlain(
+          reply.cause().getMessage());
+
+      return succeededFuture(reply.succeeded() ? respond204 :respond500);
+    };
+  }
+}
---
+++ src/test/java/org/folio/rest/impl/LocationUnitInstitutionIT.java
@@ -0,0 +1,239 @@
+package org.folio.rest.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.folio.HttpStatus.HTTP_CREATED;
+import static org.folio.HttpStatus.HTTP_NOT_FOUND;
+import static org.folio.HttpStatus.HTTP_UNPROCESSABLE_ENTITY;
+import static org.folio.services.locationunit.InstitutionService.INSTITUTION_TABLE;
+import static org.folio.utility.RestUtility.TENANT_ID;
+import static org.folio.HttpStatus.HTTP_BAD_REQUEST;
+
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import org.folio.rest.jaxrs.model.Error;
+import org.folio.rest.jaxrs.model.Errors;
+import org.folio.rest.jaxrs.model.Locinst;
+import org.folio.rest.jaxrs.model.Locinsts;
+import org.folio.rest.jaxrs.model.Metadata;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.persist.cql.CQLWrapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(VertxExtension.class)
+public class LocationUnitInstitutionIT
+  extends BaseReferenceDataIntegrationTest<Locinst, Locinsts> {
+
+  @Override
+  protected String referenceTable() {
+    return INSTITUTION_TABLE;
+  }
+
+  @Override
+  protected String resourceUrl() {
+    return "/location-units/institutions";
+  }
+
+  @Override
+  protected Class<Locinst> targetClass() {
+    return Locinst.class;
+  }
+
+  @Override
+  protected Class<Locinsts> collectionClass() {
+    return Locinsts.class;
+  }
+
+  @Override
+  protected Locinst sampleRecord() {
+    return new Locinst().withName("test-institution").withCode("code");
+  }
+
+  @Override
+  protected Function<Locinsts, List<Locinst>> collectionRecordsExtractor() {
+    return Locinsts::getLocinsts;
+  }
+
+  @Override
+  protected List<Function<Locinst, Object>> recordFieldExtractors() {
+    return List.of(Locinst::getName);
+  }
+
+  @Override
+  protected Function<Locinst, String> idExtractor() {
+    return Locinst::getId;
+  }
+
+  @Override
+  protected Function<Locinst, Metadata> metadataExtractor() {
+    return Locinst::getMetadata;
+  }
+
+  @Override
+  protected UnaryOperator<Locinst> recordModifyingFunction() {
+    return classificationType -> classificationType.withName("name-updated");
+  }
+
+  @Override
+  protected List<String> queries() {
+    return List.of("name==test-institution", "code=code");
+  }
+
+  @BeforeEach
+  void beforeEach(Vertx vertx, VertxTestContext ctx) {
+    var postgresClient = PostgresClient.getInstance(vertx, TENANT_ID);
+    var institution = new Locinst().withName("institution").withCode("ic");
+
+    postgresClient.save(INSTITUTION_TABLE, institution)
+      .onFailure(ctx::failNow)
+      .onSuccess(id -> ctx.completeNow());
+  }
+
+  @AfterEach
+  void afterEach(Vertx vertx, VertxTestContext ctx) {
+    var postgresClient = PostgresClient.getInstance(vertx, TENANT_ID);
+
+    postgresClient.delete(referenceTable(), (CQLWrapper) null)
+      .compose(
+        rows -> postgresClient.delete(INSTITUTION_TABLE, (CQLWrapper) null))
+      .onFailure(ctx::failNow).onComplete(event -> ctx.completeNow());
+  }
+
+  @Test
+  void post_shouldReturn422_whenObjectIsDuplicate(Vertx vertx,
+                                                  VertxTestContext ctx) {
+    var client = vertx.createHttpClient();
+    var id = UUID.randomUUID().toString();
+    var record = sampleRecord().withId(id);
+    var message =
+      String.format("id value already exists in table locinstitution: %s", id);
+
+    var successfulRecord =
+      doPost(client, resourceUrl(), pojo2JsonObject(record))
+        .onComplete(verifyStatus(ctx, HTTP_CREATED));
+    var failedRecord = doPost(client, resourceUrl(), pojo2JsonObject(record))
+      .onComplete(verifyStatus(ctx, HTTP_UNPROCESSABLE_ENTITY));
+
+    Future.all(successfulRecord, failedRecord)
+      .map(CompositeFuture::list)
+      .map(results -> results.stream()
+        .filter(Objects::nonNull)
+        .peek(System.out::println)
+        .map(v -> (TestResponse) v)
+        .filter(v -> v.status() == HTTP_UNPROCESSABLE_ENTITY.toInt())
+        .findAny().orElse(null))
+      .onComplete(ctx.succeeding(duplicateResponse -> ctx.verify(() -> {
+        var actual = duplicateResponse.bodyAsClass(Errors.class);
+
+        assertThat(actual.getErrors()).hasSize(1)
+          .extracting(Error::getMessage)
+          .containsExactly(message);
+
+        ctx.completeNow();
+      })));
+  }
+
+  @Test
+  void post_shouldReturn422_whenCodeIsBlank(Vertx vertx,
+                                            VertxTestContext ctx) {
+    var client = vertx.createHttpClient();
+    var id = UUID.randomUUID().toString();
+    var record = sampleRecord().withId(id).withCode(null);
+
+    doPost(client, resourceUrl(), pojo2JsonObject(record))
+      .onComplete(verifyStatus(ctx, HTTP_UNPROCESSABLE_ENTITY))
+      .onComplete(ctx.succeeding(duplicateResponse -> ctx.verify(() -> {
+        var actual = duplicateResponse.bodyAsClass(Errors.class);
+        var message = "must not be null";
+
+        assertThat(actual.getErrors()).hasSize(1)
+          .extracting(Error::getMessage)
+          .containsExactly(message);
+
+        ctx.completeNow();
+      })));
+  }
+
+  @Test
+  void post_shouldReturn422_whenNameIsBlank(Vertx vertx,
+                                            VertxTestContext ctx) {
+    var client = vertx.createHttpClient();
+    var id = UUID.randomUUID().toString();
+    var record = sampleRecord().withId(id).withName(null);
+
+    doPost(client, resourceUrl(), pojo2JsonObject(record))
+      .onComplete(verifyStatus(ctx, HTTP_UNPROCESSABLE_ENTITY))
+      .onComplete(ctx.succeeding(duplicateResponse -> ctx.verify(() -> {
+        var actual = duplicateResponse.bodyAsClass(Errors.class);
+        var message = "must not be null";
+
+        assertThat(actual.getErrors()).hasSize(1)
+          .extracting(Error::getMessage)
+          .containsExactly(message);
+
+        ctx.completeNow();
+      })));
+  }
+
+  @Test
+  void put_shouldReturn422_whenIdIsNotMatchWithPayload(Vertx vertx,
+                                                       VertxTestContext ctx) {
+    var client = vertx.createHttpClient();
+    var invalidId = UUID.randomUUID().toString();
+    var record = sampleRecord().withId(UUID.randomUUID().toString());
+
+    doPut(client, resourceUrlById(invalidId), pojo2JsonObject(record))
+      .onComplete(verifyStatus(ctx, HTTP_BAD_REQUEST))
+      .onComplete(ctx.succeeding(response -> ctx.verify(() -> {
+        var actual = response.bodyAsClass(Errors.class);
+        var message = "Illegal operation: Institution ID cannot be changed";
+
+        assertThat(actual.getErrors()).hasSize(1)
+          .extracting(Error::getMessage)
+          .containsExactly(message);
+
+        ctx.completeNow();
+      })));
+  }
+
+  @Test
+  void put_shouldReturn422_whenIdIsNullWithPayload(Vertx vertx,
+                                                   VertxTestContext ctx) {
+    var client = vertx.createHttpClient();
+    var record = sampleRecord().withId(UUID.randomUUID().toString());
+    var body = pojo2JsonObject(record);
+
+    doPut(client, resourceUrlById(null), body)
+      .onComplete(verifyStatus(ctx, HTTP_BAD_REQUEST))
+      .onComplete(ctx.succeeding(response -> ctx.verify(() -> {
+        var actual = response.bodyAsClass(Errors.class);
+        var message = "Illegal operation: Institution ID cannot be changed";
+
+        assertThat(actual.getErrors()).hasSize(1)
+          .extracting(Error::getMessage)
+          .containsExactly(message);
+
+        ctx.completeNow();
+      })));
+  }
+
+  @Test
+  void delete_shouldReturn404_notConfigured(Vertx vertx,
+                                            VertxTestContext ctx) {
+    var client = vertx.createHttpClient();
+
+    doDelete(client, "")
+      .onComplete(verifyStatus(ctx, HTTP_NOT_FOUND))
+      .onComplete(ctx.succeeding(response -> ctx.verify(ctx::completeNow)));
+  }
+}
