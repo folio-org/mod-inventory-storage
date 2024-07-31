c6ae17e218c68ce2cbc7b851164eed30530105e7
Ruslan Lavrov <ruslan_lavrov@epam.com>
2024-07-31 14:08:28 +0300

    Implemented instance-bulk api

--- src/main/java/org/folio/rest/impl/InstanceStorageInstancesBulkApi.java
+++ src/main/java/org/folio/rest/impl/InstanceStorageInstancesBulkApi.java
@@ -9,6 +9,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.rest.jaxrs.model.InstanceBulkRequest;
 import org.folio.rest.jaxrs.resource.InstanceStorageInstancesBulk;
+import org.folio.rest.support.EndpointFailureHandler;
+import org.folio.services.instance.InstanceS3Service;
+import org.folio.services.s3storage.FolioS3ClientFactory;
 
 public class InstanceStorageInstancesBulkApi implements InstanceStorageInstancesBulk {
 
@@ -18,7 +21,12 @@ public class InstanceStorageInstancesBulkApi implements InstanceStorageInstances
   public void postInstanceStorageInstancesBulk(InstanceBulkRequest bulkRequest, Map<String, String> okapiHeaders,
                                                Handler<AsyncResult<Response>> asyncResultHandler,
                                                Context vertxContext) {
-
+    new InstanceS3Service(new FolioS3ClientFactory(), vertxContext.owner(), okapiHeaders)
+      .processInstances(bulkRequest)
+      .map(PostInstanceStorageInstancesBulkResponse::respond201WithApplicationJson)
+      .map(Response.class::cast)
+      .onFailure(EndpointFailureHandler::failureResponse)
+      .onComplete(asyncResultHandler);
   }
 
 }
