3bf20561aa35fd24648b1376d7587a9cd81efcb5
Dmytro Krutii <dmytro_krutii@epam.com>
2024-09-05 12:51:59 +0300

    MODINVSTOR-1205 Fix checkstyle warnings

--- src/main/java/org/folio/validator/SubjectsValidators.java
+++ src/main/java/org/folio/validator/SubjectsValidators.java
@@ -1,54 +1,61 @@
 package org.folio.validator;
 
-import io.vertx.core.Context;
-import io.vertx.core.Future;
-import org.folio.HttpStatus;
-import org.folio.okapi.common.GenericCompositeFuture;
-import org.folio.rest.exceptions.NotFoundException;
-import org.folio.rest.jaxrs.model.Instance;
-import org.folio.services.subjectsource.SubjectSourceService;
-import org.folio.services.subjecttype.SubjectTypeService;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+
+import org.folio.HttpStatus;
+import org.folio.rest.jaxrs.model.Instance;
+import org.folio.rest.exceptions.NotFoundException;
+import org.folio.okapi.common.GenericCompositeFuture;
+import org.folio.services.subjectsource.SubjectSourceService;
+import org.folio.services.subjecttype.SubjectTypeService;
 
 import static io.vertx.core.Future.failedFuture;
 import static io.vertx.core.Future.succeededFuture;
 
 public final class SubjectsValidators {
 
-  private SubjectsValidators(){}
+  private SubjectsValidators() {}
 
-  public static Future<Instance> refuseIfSubjectIdsNotFound(Instance instance, Context context, Map<String, String> okapiHeaders) {
+  public static Future<Instance> refuseIfSubjectIdsNotFound(Instance instance, Context context,
+                                                            Map<String, String> okapiHeaders) {
     List<Future<Void>> futures = new ArrayList<>();
     instance.getSubjects().stream()
       .forEach(subject -> {
-        performValidation(subject.getSubjectTypeId(), futures, typeId -> refuseIfSubjectTypeNotFound(typeId, context, okapiHeaders));
-        performValidation(subject.getSubjectSourceId(), futures, sourceId -> refuseIfSubjectSourceNotFound(sourceId, context, okapiHeaders));
+        performValidation(subject.getSubjectTypeId(), futures,
+          typeId -> refuseIfSubjectTypeNotFound(typeId, context, okapiHeaders));
+        performValidation(subject.getSubjectSourceId(), futures,
+          sourceId -> refuseIfSubjectSourceNotFound(sourceId, context, okapiHeaders));
       });
     return GenericCompositeFuture.all(futures)
       .map(instance)
-      .recover(throwable -> failedFuture(throwable));
+      .recover(Future::failedFuture);
   }
 
-  public static Future<List<Instance>> refuseIfSubjectIdsNotFound(List<Instance> instances, Context context, Map<String, String> okapiHeaders) {
+  public static Future<List<Instance>> refuseIfSubjectIdsNotFound(List<Instance> instances, Context context,
+                                                                  Map<String, String> okapiHeaders) {
     var futures = instances.stream()
       .map(instance -> refuseIfSubjectIdsNotFound(instance, context, okapiHeaders))
       .toList();
     return GenericCompositeFuture.all(futures)
       .map(v -> instances)
-      .recover(throwable -> failedFuture(throwable));
+      .recover(Future::failedFuture);
   }
 
-  private static void performValidation(String id, List<Future<Void>> futures, Function<String, Future<Void>> validationFunction) {
+  private static void performValidation(String id, List<Future<Void>> futures,
+                                        Function<String, Future<Void>> validationFunction) {
     if (id != null) {
       futures.add(validationFunction.apply(id));
     }
   }
 
-  private static Future<Void> refuseIfSubjectTypeNotFound(String id, Context context, Map<String, String> okapiHeaders) {
+  private static Future<Void> refuseIfSubjectTypeNotFound(String id, Context context,
+                                                          Map<String, String> okapiHeaders) {
     return new SubjectTypeService(context, okapiHeaders)
       .getById(id)
       .compose(subjectType -> subjectType.getStatus() == HttpStatus.HTTP_NOT_FOUND.toInt()
@@ -56,7 +63,8 @@ public final class SubjectsValidators {
         : succeededFuture());
   }
 
-  private static Future<Void> refuseIfSubjectSourceNotFound(String id, Context context, Map<String, String> okapiHeaders) {
+  private static Future<Void> refuseIfSubjectSourceNotFound(String id, Context context,
+                                                            Map<String, String> okapiHeaders) {
     return new SubjectSourceService(context, okapiHeaders)
       .getById(id)
       .compose(subjectType -> subjectType.getStatus() == HttpStatus.HTTP_NOT_FOUND.toInt()
