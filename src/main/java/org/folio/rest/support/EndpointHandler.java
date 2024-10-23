c4895bbb61550065f3452fe17df86f10cdb95550
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2024-10-23 07:55:53 +0300

    fix(pub-period-migration) fix ItemStorageDereferencedApi

--- src/main/java/org/folio/rest/support/EndpointHandler.java
+++ src/main/java/org/folio/rest/support/EndpointHandler.java
@@ -5,7 +5,7 @@ import static io.vertx.core.Future.succeededFuture;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import javax.ws.rs.core.Response;
-import org.folio.rest.jaxrs.model.Instances;
+import org.folio.HttpStatus;
 import org.folio.rest.jaxrs.model.InstancesWithoutPubPeriod;
 import org.folio.rest.jaxrs.resource.InstanceStorage;
 import org.folio.utils.ObjectConverterUtils;
@@ -30,17 +30,22 @@ public final class EndpointHandler {
 
   public static Handler<AsyncResult<Response>> handleInstances(Handler<AsyncResult<Response>> asyncResultHandler) {
     return result -> {
-      if (result.succeeded() && result.result().getStatus() == 200) {
-        Instances instances = (Instances) result.result().getEntity();
-        var instancesWithoutPubPeriod = ObjectConverterUtils.convertObject(instances, InstancesWithoutPubPeriod.class);
-
-        asyncResultHandler.handle(succeededFuture(
-          InstanceStorage.GetInstanceStorageInstancesResponse.respond200WithApplicationJson(
-            instancesWithoutPubPeriod)));
+      if (result.succeeded()) {
+        handleSuccess(asyncResultHandler, result.result());
       } else {
-        asyncResultHandler.handle(succeededFuture(
-          InstanceStorage.GetInstanceStorageInstancesResponse.respond500WithTextPlain(result.cause())));
+        EndpointFailureHandler.handleFailure(asyncResultHandler).handle(result.cause());
       }
     };
+  }
+
+  private static void handleSuccess(Handler<AsyncResult<Response>> asyncResultHandler, Response result) {
+    if (result.getStatus() == HttpStatus.HTTP_OK.toInt()) {
+      var instances = result.getEntity();
+      var instancesWithoutPubPeriod = ObjectConverterUtils.convertObject(instances, InstancesWithoutPubPeriod.class);
+      asyncResultHandler.handle(succeededFuture(
+        InstanceStorage.GetInstanceStorageInstancesResponse.respond200WithApplicationJson(instancesWithoutPubPeriod)));
+    } else {
+      asyncResultHandler.handle(succeededFuture(result));
+    }
   }
 }
