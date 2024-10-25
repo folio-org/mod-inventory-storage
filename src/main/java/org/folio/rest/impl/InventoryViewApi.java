d598acd9bb537f977f24e7ae25bb1a0d781d41be
Illia Borysenko <Illia_Borysenko@epam.com>
2024-10-25 11:58:46 +0300

    MODINVSTOR-1266 Append bounded items to invetory-view instances response

--- src/main/java/org/folio/rest/impl/InventoryViewApi.java
+++ src/main/java/org/folio/rest/impl/InventoryViewApi.java
@@ -8,17 +8,26 @@ import java.util.Map;
 import javax.ws.rs.core.Response;
 import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.resource.InventoryViewInstances;
+import org.folio.rest.support.EndpointHandler;
 import org.folio.services.instance.InstanceService;
 
 public class InventoryViewApi implements InventoryViewInstances {
   @Validate
   @Override
-  public void getInventoryViewInstances(String totalRecords, int offset, int limit, String query,
-                                        RoutingContext routingContext, Map<String, String> okapiHeaders,
-                                        Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-
-    new InstanceService(vertxContext, okapiHeaders)
-      .streamGetInventoryViewInstances("instance_holdings_item_view", query,
-        offset, limit, null, "instances", 0, routingContext);
+  public void getInventoryViewInstances(boolean withBoundedItems,
+    String totalRecords, int offset, int limit,
+    String query, RoutingContext routingContext,
+    Map<String, String> okapiHeaders,
+    Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    var instanceService = new InstanceService(vertxContext, okapiHeaders);
+    if (withBoundedItems) {
+      instanceService
+        .getInventoryViewInstancesWithBoundedItems(offset, limit, query)
+        .onComplete(EndpointHandler.handle(asyncResultHandler));
+    } else {
+      instanceService
+        .streamGetInventoryViewInstances("instance_holdings_item_view", query,
+          offset, limit, null, "instances", 0, routingContext);
+    }
   }
 }
