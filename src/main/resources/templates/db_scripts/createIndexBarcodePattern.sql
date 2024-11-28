7be3028a8c9ee5d51aaef1bcf702c0298e6133a7
Julian Ladisch <julianladisch@users.noreply.github.com>
2024-11-28 12:46:46 +0100

    Remove documentation from .sql file, it exists in PR and Jira
    (cherry picked from commit 8273672c324e926e36441c3eddacb4d9398cc701)

--- src/main/resources/templates/db_scripts/createIndexBarcodePattern.sql
+++ src/main/resources/templates/db_scripts/createIndexBarcodePattern.sql
@@ -1,11 +1,2 @@
-<#--
-  Index for item barcode right truncation used by checkin and checkout:
-  https://github.com/folio-org/ui-checkout/blob/v11.0.0/src/ScanItems.js#L163-L164
-  https://github.com/folio-org/ui-checkin/blob/v9.2.0/src/Scan.js#L318-L319
-  The existing btree index doesn’t support right truncation because barcode is
-  Unicode and not "C" locale, for details see
-  https://www.postgresql.org/docs/current/indexes-opclass.html
-  https://www.cybertec-postgresql.com/en/indexing-like-postgresql-oracle/
--->
 CREATE INDEX IF NOT EXISTS item_barcode_idx_pattern
   ON ${myuniversity}_${mymodule}.item (lower(jsonb->>'barcode') text_pattern_ops);
