f9c45f92687318bc88da4c2000ab1a19733bd2ea
Javokhir_Abdullaev <javokhir_abdullaev@epam.com>
2024-12-04 11:59:38 +0500

    add indexes

--- src/main/resources/templates/db_scripts/createInstanceSubjectSourceTable.sql
+++ src/main/resources/templates/db_scripts/createInstanceSubjectSourceTable.sql
@@ -1,11 +1,10 @@
 
 CREATE TABLE IF NOT EXISTS ${myuniversity}_${mymodule}.instance_subject_source (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   instance_id UUID,
   source_id UUID,
   CONSTRAINT fk_instance_id FOREIGN KEY(instance_id) REFERENCES ${myuniversity}_${mymodule}.instance(id) ON DELETE CASCADE,
   CONSTRAINT fk_source_id FOREIGN KEY(source_id) REFERENCES ${myuniversity}_${mymodule}.subject_source(id)
 );
 
-CREATE INDEX IF NOT EXISTS instance_subject_source_instance_idx
-  ON ${myuniversity}_${mymodule}.instance_subject_source(instance_id);
+CREATE INDEX IF NOT EXISTS instance_subject_source_source_idx
+  ON ${myuniversity}_${mymodule}.instance_subject_source(source_id);
--- src/main/resources/templates/db_scripts/createInstanceSubjectTypeTable.sql
+++ src/main/resources/templates/db_scripts/createInstanceSubjectTypeTable.sql
@@ -1,11 +1,10 @@
 
 CREATE TABLE IF NOT EXISTS ${myuniversity}_${mymodule}.instance_subject_type (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   instance_id UUID,
   type_id UUID,
   CONSTRAINT fk_instance_id FOREIGN KEY(instance_id) REFERENCES ${myuniversity}_${mymodule}.instance(id) ON DELETE CASCADE,
   CONSTRAINT fk_type_id FOREIGN KEY(type_id) REFERENCES ${myuniversity}_${mymodule}.subject_type(id)
 );
 
-CREATE INDEX IF NOT EXISTS instance_subject_type_instance_idx
-  ON ${myuniversity}_${mymodule}.instance_subject_type(instance_id);
+CREATE INDEX IF NOT EXISTS instance_subject_type_type_idx
+  ON ${myuniversity}_${mymodule}.instance_subject_type(type_id);
