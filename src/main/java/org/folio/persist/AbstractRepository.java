c00f1b1e52fb710fbdfa0c02772453b542e9e10b
Kateryna_Senchenko <kateryna_senchenko@epam.com>
2025-01-25 13:14:16 +0200

    MODINVSTOR-1354 Optimize check for existing shadow Instance before creating it (#1141)
    * MODINVSTOR-1354: Optimize check for existing shadow Instance before creating it
    
    * MODINVSTOR-1354: Fix checkstyle
    
    * update news
    
    (cherry picked from commit 656c50581660afe32352ea606c4d9f4e043df302)

--- src/main/java/org/folio/persist/AbstractRepository.java
+++ src/main/java/org/folio/persist/AbstractRepository.java
@@ -15,6 +15,7 @@ import io.vertx.core.json.Json;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
+import io.vertx.sqlclient.Tuple;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -91,6 +92,13 @@ public abstract class AbstractRepository<T> {
       .collect(Collectors.toSet());
 
     return getById(ids);
+  }
+
+  public Future<Boolean> exists(String id) {
+    return postgresClient.execute(
+        "select 1 from " + postgresClient.getSchemaName() + "." + tableName + " where id = $1 limit 1",
+        Tuple.of(id))
+      .map(ar -> ar != null && ar.rowCount() == 1);
   }
 
   public Future<RowSet<Row>> update(AsyncResult<SQLConnection> connection, String id, T entity) {
