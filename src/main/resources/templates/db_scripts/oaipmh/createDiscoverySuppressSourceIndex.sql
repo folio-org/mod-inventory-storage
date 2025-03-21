31bec7e2170159fd0c0c146acf26fad63773a6a0
obozhko-folio <Oleksandr_Bozhko@epam.com>
2025-03-21 11:24:50 +0200

    MODINVSTOR-1377 Added index

--- src/main/resources/templates/db_scripts/oaipmh/createDiscoverySuppressSourceIndex.sql
+++ src/main/resources/templates/db_scripts/oaipmh/createDiscoverySuppressSourceIndex.sql
@@ -1,2 +1,2 @@
-CREATE INDEX instance_discoverysuppress_source_idx
+CREATE INDEX IF NOT EXISTS instance_discoverysuppress_source_idx
 ON ${myuniversity}_${mymodule}.instance (((jsonb ->> 'discoverySuppress'::text)), ((jsonb ->> 'source'::text)));
