f09d4c8b7fd2fa1e207218002fed07ff32e0b80c
psmagin <pavlo_smahin@epam.com>
2025-07-01 11:36:50 +0300

    fix(migration): updateIdentifierTypeCanceledLCCN migration script to modify unexpected records
    Closes: MODINVSTOR-1361

--- src/main/resources/templates/db_scripts/updateIdentifierTypeCanceledLCCN.sql
+++ src/main/resources/templates/db_scripts/updateIdentifierTypeCanceledLCCN.sql
@@ -1,3 +1,8 @@
+UPDATE ${myuniversity}_${mymodule}.identifier_type
+SET jsonb = jsonb_set(jsonb, '{name}', to_jsonb(CONCAT(jsonb ->> 'name', '_custom')))
+WHERE jsonb ->> 'name' = 'Canceled LCCN'
+  AND id <> 'c858e4f2-2b6b-4385-842b-60532ee34abb';
+
 INSERT INTO ${myuniversity}_${mymodule}.identifier_type (id, jsonb)
 VALUES ('c858e4f2-2b6b-4385-842b-60532ee34abb',
         json_build_object('id','c858e4f2-2b6b-4385-842b-60532ee34abb', 'name', 'Canceled LCCN',  'source', 'folio'))
