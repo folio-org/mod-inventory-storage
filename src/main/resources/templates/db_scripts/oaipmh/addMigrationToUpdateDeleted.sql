862a8875cd7f90f7c9bcc5d93c5510242c23a0a5
obozhko-folio <83696213+obozhko-folio@users.noreply.github.com>
2025-07-16 14:12:34 +0300

    MODINVSTOR-1449 Rollback migration for deleted (#1238)
    * MODINVSTOR-1449 Rollback migration for deleted

--- src/main/resources/templates/db_scripts/oaipmh/addMigrationToUpdateDeleted.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE ${myuniversity}_${mymodule}.instance
-DISABLE TRIGGER check_subject_references_on_insert_or_update;
-
-UPDATE ${myuniversity}_${mymodule}.instance
-SET jsonb = jsonb || '{"deleted": "false"}'
-WHERE jsonb ->> 'deleted' IS NULL;
-
-ALTER TABLE ${myuniversity}_${mymodule}.instance
-ENABLE TRIGGER check_subject_references_on_insert_or_update;
