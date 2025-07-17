a0f3d855758ce2924b43abb06121c3ecf7dae3a0
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2025-07-17 17:42:37 +0300

    feat(s3-env-variables) remove default s3 bucket name

--- src/main/java/org/folio/services/s3storage/FolioS3ClientFactory.java
+++ src/main/java/org/folio/services/s3storage/FolioS3ClientFactory.java
@@ -13,7 +13,6 @@ public class FolioS3ClientFactory {
   public static final String S3_SECRET_ACCESS_KEY_CONFIG = "S3_SECRET_ACCESS_KEY";
   public static final String S3_IS_AWS_CONFIG = "S3_IS_AWS";
   private static final String S3_URL_DEFAULT = "https://s3.amazonaws.com";
-  private static final String S3_BUCKET_DEFAULT = "mod-inventory-storage";
   private static final String S3_IS_AWS_DEFAULT = "false";
 
   public FolioS3Client getFolioS3Client() {
@@ -25,7 +24,7 @@ public class FolioS3ClientFactory {
       .builder()
       .endpoint(getValue(S3_URL_CONFIG, S3_URL_DEFAULT))
       .region(getValue(S3_REGION_CONFIG))
-      .bucket(getValue(S3_BUCKET_CONFIG, S3_BUCKET_DEFAULT))
+      .bucket(getValue(S3_BUCKET_CONFIG))
       .accessKey(getValue(S3_ACCESS_KEY_ID_CONFIG))
       .secretKey(getValue(S3_SECRET_ACCESS_KEY_CONFIG))
       .awsSdk(Boolean.parseBoolean(getValue(S3_IS_AWS_CONFIG, S3_IS_AWS_DEFAULT)))
