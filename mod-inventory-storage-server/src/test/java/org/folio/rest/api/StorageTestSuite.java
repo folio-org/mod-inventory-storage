f80b9582286f467e5b017e90ec0d81a1f5b37685
Alexander Sambale <Alexander.Sambale@samxela.eu>
2025-08-19 15:01:58 +0200

    MODINVSTOR-1445 chore: :white_check_mark: Add CustomFieldsApiTest to StorageTestSuite

--- mod-inventory-storage-server/src/test/java/org/folio/rest/api/StorageTestSuite.java
+++ mod-inventory-storage-server/src/test/java/org/folio/rest/api/StorageTestSuite.java
@@ -41,6 +41,7 @@ import org.junit.runners.Suite;
   AsyncMigrationTest.class,
   AuditDeleteTest.class,
   BoundWithStorageTest.class,
+  CustomFieldsApiTest.class,
   DereferencedItemStorageTest.class,
   EffectiveLocationMigrationTest.class,
   HoldingsStorageTest.class,
