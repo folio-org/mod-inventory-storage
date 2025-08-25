75ae274c7b2998e2227adde523696519c6d76ce5
Alexander Sambale <Alexander.Sambale@samxela.eu>
2025-08-25 13:09:37 +0200

    MODINVSTOR-1445 chore: :white_check_mark: Update testPutCustomField and testPutCustomFieldCollection

--- src/test/java/org/folio/rest/api/CustomFieldsApiTest.java
+++ src/test/java/org/folio/rest/api/CustomFieldsApiTest.java
@@ -39,6 +39,8 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
   private static final String ACTIVE = "active";
   private static final String CREATION_DATE = "creation_date";
   private static final String CUSTOM_FIELDS = "customFields";
+  private static final String CUSTOM_FIELDS_OPTIONS_STATS_ENDPOINT = "/{id}/options/{optId}/stats";
+  private static final String CUSTOM_FIELDS_STATS_ENDPOINT = "/{id}/stats";
   private static final String CUSTOM_FIELDS_URL = "/custom-fields";
   private static final String EMAIL = "email";
   private static final String ENTITY_TYPE = "entityType";
@@ -53,6 +55,9 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
   private static final String META = "meta";
   private static final String MULTI_SELECT = "multiSelect";
   private static final String NAME = "name";
+  private static final String NAME_MULTISELECT = "multiselect";
+  private static final String NAME_SINGLE_SELECT = "singleselect";
+  private static final String NAME_TEXTBOX = "textbox";
   private static final String OPTIONS = "options";
   private static final String PATRON_GROUP = "patronGroup";
   private static final String PERMANENT_LOAN_TYPE_ID = "permanentLoanTypeId";
@@ -66,30 +71,29 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
   private static final String USERS_URL = "/users/";
   private static final String VALUE = "value";
   private static final String VALUES = "values";
-  private static final String CUSTOM_FIELDS_STATS_ENDPOINT = "/{id}/stats";
-  private static final String CUSTOM_FIELDS_OPTIONS_STATS_ENDPOINT = "/{id}/options/{optId}/stats";
 
-  private final UUID userId = UUID.randomUUID();
-  private final JsonObject user = createSimpleUser(userId);
-  private UUID holdingId;
+  private final List<JsonObject> customFields = createCustomFields(ITEM);
+  private UUID holdingId = createInstanceAndHolding(MAIN_LIBRARY_LOCATION_ID);
   private final List<JsonObject> simpleItems = List
       .of(simpleItem(), simpleItem(), simpleItem());
-  private final List<JsonObject> customFields = createCustomFields(ITEM);
+  private final UUID userId = UUID.randomUUID();
+  private final JsonObject user = createSimpleUser(userId);
 
   @Before
   public void beforeEach() {
     StorageTestSuite.deleteAll(itemsStorageUrl(""));
+    updateCustomFields(List.of()); // there is no deleteAll
     removeAllEvents();
     // folio-custom-field depends on UserService information for saving custom
     // fields
     WireMock.stubFor(WireMock.get(USERS_URL + userId)
         .willReturn(WireMock.okJson(user.encode())));
-    holdingId = createInstanceAndHolding(MAIN_LIBRARY_LOCATION_ID);
   }
 
   @After
   public void afterEach() {
     StorageTestSuite.deleteAll(itemsStorageUrl(""));
+    updateCustomFields(List.of());
     removeAllEvents();
   }
 
@@ -126,9 +130,10 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
   }
 
   /**
-   *
+   * Should automatically update the item, when a related custom field is deleted.
    * POST /custom-fields
    * GET /custom-fields
+   * DELETE /custom-fields/{id}
    * GET /custom-fields/{id}/stats
    */
   @Test
@@ -136,79 +141,105 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
     // given
     // simple item and default custom field as JsonObject
     // (textbox)
-    // when
-    // saving the custom field in the database
+    // saved in the database
     assertEquals(0, getCustomFieldsCount());
-    saveCustomField(customFields.get(0));
+    int customFieldIndex = 0;
+    saveCustomField(customFields.get(customFieldIndex));
     assertEquals(1, getCustomFieldsCount());
-    assertEquals(0, getCustomFieldStatisticCount(customFieldsId(0)));
-    // adding references to the custom field and
-    // corresponding value
-    // and saving the items with custom field values in the database
-    IndividualResource item = saveItem(itemAddCustomFields(0, List.of(0)));
-    assertEquals(true, itemContainsCustomField(item, "textbox"));
-    assertEquals(1, getCustomFieldStatisticCount(customFieldsId(0)));
-    deleteCustomField(customFieldsId(0));
+    assertEquals(0, getCustomFieldStatisticCount(customFieldsId(customFieldIndex)));
+    IndividualResource item = saveItem(itemAddCustomFields(0, List.of(customFieldIndex)));
+    assertEquals(true, itemContainsCustomField(item, NAME_TEXTBOX));
+    assertEquals(1, getCustomFieldStatisticCount(customFieldsId(customFieldIndex)));
+    // when
+    // deleting the custom field in the database
+    deleteCustomField(customFieldsId(customFieldIndex));
     // then
-    // the items should be created
     assertEquals(0, getCustomFieldsCount());
     // check that item has no custom field anymore
     IndividualResource updatedItem = getItem(item.getId());
-    assertEquals(false, itemContainsCustomField(updatedItem, "textbox"));
+    assertEquals(false, itemContainsCustomField(updatedItem, NAME_TEXTBOX));
   }
 
   /**
-   * remove single option from custom field
-   * customFieldsPO.get(1).getSelectField().getOptions().getValues().removeFirst();
-   * putCustomField(customFieldsPO.get(1));
-   * 
-   * assertEquals(2,
-   * purchaseOrder1.getCustomFields().getAdditionalProperties().size());
-   * assertNull(purchaseOrder1.getCustomFields().getAdditionalProperties().get("singleselect"));
-   * assertThat(purchaseOrder2, samePropertyValuesAs(purchaseOrders.get(1)));
-   * 
+   * Should automatically update the item, when a related custom field is
+   * updated.
    * POST /custom-fields
    * PUT /custom-fields/{id}
    */
   @Test
-  public void testCustomFieldPut() {
+  public void testPutCustomField() {
     // given
     // simple item and default custom field as JsonObject
-    // (singleselect)
+    // (multiselect)
+    // the simple item with custom field values in the database
+    saveCustomField(customFields.get(2));
+    IndividualResource item = saveItem(itemAddCustomFields(0, List.of(2)));
+    assertEquals(2, getItemMultiselectSize(item));
     // when
-    // saving the custom field in the database
-    saveCustomField(customFields.get(1));
-    // adding references to the custom field and
-    // corresponding value
-    // and saving the items with custom field values in the database
-    IndividualResource item = saveItem(itemAddCustomFields(0, List.of(1)));
-
+    // updating the custom field
+    // (from 4 options to only 2 options)
+    customFields.get(2)
+        .put(SELECT_FIELD, createSelectField(2, true));
+    updateCustomField(customFields.get(2));
+    // then
+    // the reference in the item for custom fields should also be updated
+    // before opt1, opt2; now only opt1, because opt2 was deleted
+    IndividualResource updatedItem = getItem(item.getId());
+    assertEquals(1, getItemMultiselectSize(updatedItem));
   }
 
+  /**
+   * Should automatically update items, when all custom fields are updated and the
+   * rest is deleted.
+   * POST /custom-fields
+   * PUT /custom-fields
+   */
   @Test
-  void testPutPutCustomFieldCollection() {
-    // // populate with sample data
-    // populateSampleData();
+  public void testPutCustomFieldCollection() {
+    // given
+    // 2 simple items and default custom fields as JsonObjects
+    // (textbox, singleselect, multiselect)
+    // when
+    // saving the custom fields in the database
+    saveCustomField(customFields.get(0));
+    saveCustomField(customFields.get(1));
+    saveCustomField(customFields.get(2));
+    // adding references to the 3 custom fields and
+    // corresponding values for each of the items
+    // and saving the items with custom field values in the database
+    JsonObject itemWithCustomField0 = itemAddCustomFields(0);
+    IndividualResource item1 = saveItem(itemWithCustomField0);
+    assertEquals(true, itemCustomFieldsContainsKey(item1, NAME_SINGLE_SELECT));
+    assertEquals(true, itemCustomFieldsContainsKey(item1, NAME_TEXTBOX));
+    assertEquals(true, itemCustomFieldsContainsKey(item1, NAME_MULTISELECT));
+    JsonObject itemWithCustomField1 = itemAddCustomFields(1);
+    IndividualResource item2 = saveItem(itemWithCustomField1);
+    assertEquals(true, itemCustomFieldsContainsKey(item2, NAME_SINGLE_SELECT));
+    assertEquals(true, itemCustomFieldsContainsKey(item2, NAME_TEXTBOX));
+    assertEquals(true, itemCustomFieldsContainsKey(item2, NAME_MULTISELECT));
+    // new custom fields
+    JsonObject newCustomField = customFields.get(2).put(NAME, "newMultiselect").put(ID, UUID.randomUUID().toString());
+    List<JsonObject> customFieldsToUpdate = List.of(newCustomField, customFields.get(0), customFields.get(1));
+    // when
+    // updating custom fields
+    updateCustomFields(customFieldsToUpdate);
+    // then
+    IndividualResource updatedItem1 = getItem(item1.getId());
+    assertEquals(true, itemCustomFieldsContainsKey(updatedItem1, NAME_SINGLE_SELECT));
+    assertEquals(true, itemCustomFieldsContainsKey(updatedItem1, NAME_TEXTBOX));
+    assertEquals(false, itemCustomFieldsContainsKey(updatedItem1, NAME_MULTISELECT));
+    IndividualResource updatedItem2 = getItem(item2.getId());
+    assertEquals(true, itemCustomFieldsContainsKey(updatedItem2, NAME_SINGLE_SELECT));
+    assertEquals(true, itemCustomFieldsContainsKey(updatedItem2, NAME_TEXTBOX));
+    assertEquals(false, itemCustomFieldsContainsKey(updatedItem2, NAME_MULTISELECT));
+  }
 
-    // // remove opt from custom field using PutCustomFieldsCollection
-    // customFieldsPO.get(1).getSelectField().getOptions().getValues().removeFirst();
-    // putCustomFieldCollection(
-    // new PutCustomFieldCollection()
-    // .withCustomFields(customFieldsPO)
-    // .withEntityType("purchase_order"));
+  private boolean itemCustomFieldsContainsKey(IndividualResource item, String key) {
+    return ((JsonObject) item.getJson().getValue(CUSTOM_FIELDS)).containsKey(key);
+  }
 
-    // // POs should be updated
-    // Map<String, PurchaseOrder> allPurchaseOrders = getAllPurchaseOrders();
-    // PurchaseOrder purchaseOrder1 =
-    // allPurchaseOrders.get(purchaseOrders.get(0).getId());
-    // PurchaseOrder purchaseOrder2 =
-    // allPurchaseOrders.get(purchaseOrders.get(1).getId());
-    // assertEquals(2,
-    // purchaseOrder1.getCustomFields().getAdditionalProperties().size());
-    // assertNull(purchaseOrder1.getCustomFields().getAdditionalProperties().get("singleselect"));
-    // assertEquals(3,
-    // purchaseOrder2.getCustomFields().getAdditionalProperties().size());
-    // assertThat(purchaseOrder2, samePropertyValuesAs(purchaseOrders.get(1)));
+  private int getItemMultiselectSize(IndividualResource item) {
+    return ((JsonArray) ((JsonObject) item.getJson().getValue(CUSTOM_FIELDS)).getValue(NAME_MULTISELECT)).size();
   }
 
   private boolean itemContainsCustomField(IndividualResource item, String name) {
@@ -252,37 +283,52 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
   private List<JsonObject> createCustomFields(String entityType) {
     JsonObject textbox = new JsonObject()
         .put(ID, UUID.randomUUID().toString())
-        .put(NAME, "textbox")
+        .put(NAME, NAME_TEXTBOX)
         .put(TYPE, Type.TEXTBOX_SHORT)
         .put(ENTITY_TYPE, entityType);
 
     JsonObject singleselect = new JsonObject()
         .put(ID, UUID.randomUUID().toString())
-        .put(NAME, "singleselect")
+        .put(NAME, NAME_SINGLE_SELECT)
         .put(TYPE, Type.SINGLE_SELECT_DROPDOWN)
         .put(ENTITY_TYPE, entityType)
         .put(SELECT_FIELD, createSelectField(3, false));
 
     JsonObject multiselect = new JsonObject()
         .put(ID, UUID.randomUUID().toString())
-        .put(NAME, "multiselect")
+        .put(NAME, NAME_MULTISELECT)
         .put(TYPE, Type.MULTI_SELECT_DROPDOWN)
         .put(ENTITY_TYPE, entityType)
         .put(SELECT_FIELD, createSelectField(4, true));
     return List.of(textbox, singleselect, multiselect);
   }
 
+  /**
+   * Create custom field values which can be inserted in items.
+   *
+   * @param customFieldIndices indices of the example custom field values to
+   *                           insert
+   * @return JsonObject representing the custom field values
+   */
   private JsonObject createCustomFieldValues(List<Integer> customFieldIndices) {
     JsonObject customFieldValues = new JsonObject();
-    if (customFieldIndices.contains(0))
-      customFieldValues.put("textbox", "text1");
-    if (customFieldIndices.contains(1))
-      customFieldValues.put("singleselect", "opt_0");
-    if (customFieldIndices.contains(2))
-      customFieldValues.put("multiselect", new JsonArray().add("opt_1").add("opt_2"));
+    if (customFieldIndices.contains(0)) {
+      customFieldValues.put(NAME_TEXTBOX, "text1");
+    }
+    if (customFieldIndices.contains(1)) {
+      customFieldValues.put(NAME_SINGLE_SELECT, "opt_0");
+    }
+    if (customFieldIndices.contains(2)) {
+      customFieldValues.put(NAME_MULTISELECT, new JsonArray().add("opt_1").add("opt_2"));
+    }
     return customFieldValues;
   }
 
+  /**
+   * Create a minimal item.
+   *
+   * @return a simple item as JsonObject
+   */
   private JsonObject simpleItem() {
     return new JsonObject()
         .put(ID, UUID.randomUUID().toString())
@@ -302,6 +348,14 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
     return itemAddCustomFields(itemToCreateIndex, List.of(0, 1, 2));
   }
 
+  /**
+   * Add custom field values to the simple item referenced by itemToCreateIndex.
+   *
+   * @param itemToCreateIndex  index of simpleItems
+   * @param customFieldIndices indices of the custom fields which should get added
+   *                           to the item
+   * @return the simple item with added custom fields
+   */
   private JsonObject itemAddCustomFields(int itemToCreateIndex, List<Integer> customFieldIndices) {
     return simpleItems
         .get(itemToCreateIndex)
@@ -315,8 +369,8 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
   }
 
   /**
-   * Save the customfield with the API.
-   * Assert the customfields got created by checking the status code.
+   * Save the custom field with the API.
+   * Assert the custom fields got created by checking the status code.
    * Here we also need to add the mock to a user interface because custom fields
    * still have dependencies on a user
    *
@@ -340,15 +394,73 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
     return response;
   }
 
-  private Response deleteCustomField(String id) {
-    var createCompleted = new CompletableFuture<Response>();
+  /**
+   * Update the custom field with the API.
+   * Assert the custom fields got update by checking the status code.
+   * Here we also need to add the mock to a user interface because custom fields
+   * still have dependencies on a user
+   *
+   * @param customFieldToUpdate The JsonObject representing the custom field
+   * @return Response of the update request
+   */
+  private Response updateCustomField(JsonObject customFieldToUpdate) {
+    var updateCompleted = new CompletableFuture<Response>();
     String usersUrl = HTTP_LOCALHOST + mockServer.port();
     Map<String, String> headers = Map
         .of(XOkapiHeaders.USER_ID, userId.toString(), XOkapiHeaders.URL, usersUrl,
             XOkapiHeaders.URL_TO, usersUrl);
+    getClient().put(
+        customFieldsUrl("/" + customFieldToUpdate.getString(ID)),
+        customFieldToUpdate,
+        headers,
+        TENANT_ID,
+        ResponseHandler.any(updateCompleted));
+    Response response = get(updateCompleted);
+    assertThat(response.getStatusCode(), is(HttpURLConnection.HTTP_NO_CONTENT));
+    return response;
+  }
+
+  /**
+   * Update the custom fields and delete the rest with the API.
+   * Assert the custom fields got update by checking the status code.
+   * Here we also need to add the mock to a user interface because custom fields
+   * still have dependencies on a user
+   *
+   * @param customFieldsToUpdate List of JsonObjects representing the custom
+   *                             fields
+   * @return Response of the update request
+   */
+  private Response updateCustomFields(List<JsonObject> customFieldsToUpdate) {
+    var updateCompleted = new CompletableFuture<Response>();
+    String usersUrl = HTTP_LOCALHOST + mockServer.port();
+    Map<String, String> headers = Map
+        .of(XOkapiHeaders.USER_ID, userId.toString(), XOkapiHeaders.URL, usersUrl,
+            XOkapiHeaders.URL_TO, usersUrl);
+    JsonObject customFieldsObject = new JsonObject()
+        .put(CUSTOM_FIELDS, customFieldsToUpdate)
+        .put(ENTITY_TYPE, ITEM);
+    getClient().put(
+        customFieldsUrl(""),
+        customFieldsObject,
+        headers,
+        TENANT_ID,
+        ResponseHandler.any(updateCompleted));
+    Response response = get(updateCompleted);
+    assertThat(response.getStatusCode(), is(HttpURLConnection.HTTP_NO_CONTENT));
+    return response;
+  }
+
+  /**
+   * Delete the custom field by id with the API.
+   * Assert the custom fields got deleted by checking the status code.
+   *
+   * @param id of the custom field
+   * @return Response of the delete request
+   */
+  private Response deleteCustomField(String id) {
+    var createCompleted = new CompletableFuture<Response>();
     getClient().delete(
         customFieldsUrl("/" + id),
-        headers,
         TENANT_ID,
         ResponseHandler.any(createCompleted));
     Response response = get(createCompleted);
@@ -366,12 +478,24 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
     return itemsClient.create(itemToCreate);
   }
 
+  /**
+   * Get item by UUID.
+   *
+   * @param id UUID of the item you want to get
+   * @return The IndividualResource of the json response of the get request
+   */
   private IndividualResource getItem(UUID id) {
     Response response = itemsClient.getById(id);
     assertThat(response.getStatusCode(), is(HttpURLConnection.HTTP_OK));
     return new IndividualResource(response);
   }
 
+  /**
+   * Create a simple user with the specified UUID.
+   *
+   * @param newUserId UUID for the user
+   * @return a JsonObject of the simple user
+   */
   private JsonObject createSimpleUser(UUID newUserId) {
     JsonObject meta = new JsonObject()
         .put(CREATION_DATE, "2016-11-05T07:23")
@@ -394,6 +518,13 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
         .put(PERSONAL, personal);
   }
 
+  /**
+   * Get the custom field statistic count with the API.
+   * Asserts the correct response status
+   *
+   * @param customFieldId id of the custom field
+   * @return count of the custom field statistic as int
+   */
   private int getCustomFieldStatisticCount(String customFieldId) {
     return given()
         .header(new Header(TENANT, TENANT_ID))
@@ -406,6 +537,14 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
         .getCount();
   }
 
+  /**
+   * Get the custom field option statistic count with the API.
+   * Asserts the correct response status
+   *
+   * @param customFieldId id of the custom field
+   * @param optionId      id of the option
+   * @return count of the custom field option statistic as int
+   */
   private int getCustomFieldOptionStatisticCount(String customFieldId, String optionId) {
     return given()
         .header(new Header(TENANT, TENANT_ID))
@@ -418,6 +557,12 @@ public class CustomFieldsApiTest extends TestBaseWithInventoryUtil {
         .getCount();
   }
 
+  /**
+   * Get the number of custom fields in the database with the API.
+   * Asserts the correct response status
+   *
+   * @return the number of custom fields as int
+   */
   private int getCustomFieldsCount() {
     var createCompleted = new CompletableFuture<Response>();
     getClient().get(
