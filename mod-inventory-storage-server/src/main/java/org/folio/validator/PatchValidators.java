5160d9fc03f2ab7699fb6120ae61db6fbfb74d3d
Siarhei Charniak <siarhei_charniak@epam.com>
2026-01-29 15:28:59 +0300

    MODINSVSTOR-1456

--- mod-inventory-storage-server/src/main/java/org/folio/validator/PatchValidators.java
+++ mod-inventory-storage-server/src/main/java/org/folio/validator/PatchValidators.java
@@ -1,8 +1,11 @@
 package org.folio.validator;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.vertx.core.Future;
 import io.vertx.core.json.JsonObject;
+import java.util.Arrays;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.folio.rest.exceptions.BadRequestException;
 import org.folio.rest.jaxrs.model.Instance;
 
@@ -11,13 +14,15 @@ public final class PatchValidators {
   }
 
   public static Future<JsonObject> checkInstanceFields(JsonObject patchJson) {
-    var instanceFields = JsonObject.mapFrom(new Instance()).fieldNames();
+    var instanceFields = Arrays.stream(FieldUtils.getFieldsWithAnnotation(Instance.class, JsonProperty.class))
+      .map(field -> field.getAnnotation(JsonProperty.class).value())
+      .collect(Collectors.toSet());
     var rejectedFields = patchJson.fieldNames().stream()
       .filter(fieldName -> !instanceFields.contains(fieldName))
-      .collect(Collectors.joining(", ", "'", "'"));
+      .collect(Collectors.joining(", "));
     if (!rejectedFields.isEmpty()) {
       return Future.failedFuture(
-        new BadRequestException("Field(s) " + rejectedFields + " not allowed in Instance patch"));
+        new BadRequestException("Field(s) '" + rejectedFields + "' not allowed in Instance patch"));
     }
     return Future.succeededFuture(patchJson);
   }
