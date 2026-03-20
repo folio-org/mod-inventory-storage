6a2a7385cd2a3e2af2939c102c7d1ed5da69d3ad
Siarhei Charniak <siarhei_charniak@epam.com>
2026-03-20 09:05:41 +0300

    MODINVSTOR-1529

--- mod-inventory-storage-server/src/main/java/org/folio/services/instance/InstanceService.java
+++ mod-inventory-storage-server/src/main/java/org/folio/services/instance/InstanceService.java
@@ -43,7 +43,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.folio.dbschema.ObjectMapperTool;
 import org.folio.okapi.common.XOkapiHeaders;
 import org.folio.persist.InstanceMarcRepository;
 import org.folio.persist.InstanceRelationshipRepository;
