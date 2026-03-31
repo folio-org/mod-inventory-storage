6d36b0e3667bb635377233ffcb035af96ed55e9b
Matt Weaver <mweaver@ebsco.com>
2026-03-31 07:36:07 -0500

    MODFQMMGR-1097 Remove the instance admin notes index

--- mod-inventory-storage-server/src/main/resources/templates/db_scripts/instance/createInstanceAdministrativeNotesIndex.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX IF NOT EXISTS instance_administrativenotes_idx ON ${myuniversity}_${mymodule}.instance
-  USING gin (
-             ${myuniversity}_${mymodule}.normalize_jsonb_array(jsonb -> 'administrativeNotes')
-             jsonb_path_ops
-    );
