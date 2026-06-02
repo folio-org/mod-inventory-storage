4386065ada44ccd74a680241cdad84d6ad913f53
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2026-06-02 13:33:58 +0200

    fix: Fix Instace complete_updated_date update performance on item create/update (#1365)
    * fix: Fix Instace complete_updated_date update performance on item create/update
    
    Closes: MODINVSTOR-1569
    
    (cherry picked from commit cc3dacfff987a4708a63dd4c38f243261e6aef6e)

--- src/main/resources/templates/db_scripts/oaipmh/updateCompleteUpdatedDateFunctionsToHandleMove.sql
+++ src/main/resources/templates/db_scripts/oaipmh/updateCompleteUpdatedDateFunctionsToHandleMove.sql
@@ -38,16 +38,36 @@
     VOLATILE NOT LEAKPROOF
  AS $BODY$
  BEGIN
+     -- Update instance for the new (or current) holdings record.
+     -- Uses index on holdings_record.id for fast lookup.
      UPDATE ${myuniversity}_${mymodule}.instance inst SET complete_updated_date = NOW()
      WHERE inst.id IN (
          SELECT instanceid
          FROM ${myuniversity}_${mymodule}.holdings_record hold_rec
-         WHERE hold_rec.id = NEW.holdingsrecordid
-            OR (TG_OP = 'UPDATE' AND hold_rec.id = OLD.holdingsrecordid AND OLD.holdingsrecordid <> NEW.holdingsrecordid)
-            OR hold_rec.id IN (
-                SELECT holdingsrecordid
-                FROM ${myuniversity}_${mymodule}.bound_with_part bwp
-                WHERE bwp.itemid = NEW.id));
+         WHERE hold_rec.id = NEW.holdingsrecordid);
+
+     -- When an item is moved to a different holdings record, also update the
+     -- source instance. Kept as a separate statement to preserve index usage on
+     -- holdings_record.id (combining with OR + subquery causes sequential scans).
+     IF TG_OP = 'UPDATE' AND OLD.holdingsrecordid <> NEW.holdingsrecordid THEN
+         UPDATE ${myuniversity}_${mymodule}.instance inst SET complete_updated_date = NOW()
+         WHERE inst.id IN (
+             SELECT instanceid
+             FROM ${myuniversity}_${mymodule}.holdings_record hold_rec
+             WHERE hold_rec.id = OLD.holdingsrecordid);
+     END IF;
+
+     -- Update instances reachable via bound-with parts.
+     -- Uses index on bound_with_part.itemid and holdings_record.id for fast lookup.
+     UPDATE ${myuniversity}_${mymodule}.instance inst SET complete_updated_date = NOW()
+     WHERE inst.id IN (
+         SELECT instanceid
+         FROM ${myuniversity}_${mymodule}.holdings_record hold_rec
+         WHERE hold_rec.id IN (
+             SELECT holdingsrecordid
+             FROM ${myuniversity}_${mymodule}.bound_with_part bwp
+             WHERE bwp.itemid = NEW.id));
+
      RETURN NEW;
  END;
  $BODY$;
