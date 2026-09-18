277001b202a55ff4e9230e46062dc08b51236707
Wayne Schneider <wayne@indexdata.com>
2026-09-18 12:44:29 -0500

    Fix view definition to use proper variables

--- src/main/resources/templates/db_scripts/instance-hr-item/instance-hr-item-view.sql
+++ src/main/resources/templates/db_scripts/instance-hr-item/instance-hr-item-view.sql
@@ -14,5 +14,5 @@ SELECT instance.id as id, JSONB_BUILD_OBJECT(
                         JOIN ${myuniversity}_${mymodule}.holdings_record as hr
                           ON hr.id = bw.holdingsrecordid
                         WHERE hr.instanceId = instance.id LIMIT 1))
-	) AS jsonb, lower(cu_mod_inventory_storage.f_unaccent(instance.jsonb->>'hrid')) as instance_hrid
+	) AS jsonb, lower(${myuniversity}_${mymodule}.f_unaccent(instance.jsonb->>'hrid')) as instance_hrid
   FROM ${myuniversity}_${mymodule}.instance;
